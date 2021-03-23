f3be6c31cd2f566c82f2d0e69d653e8821bfae6c
codysmith287 <csmith@shift3tech.com>
2021-03-23 15:59:08 -0700

    fix(App.tsx): fix App.tsx component to trigger correct fallback behavior for the Sentry ErrroBoundary component

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,42 @@ import React, { useState } from 'react'
 import * as Sentry from '@sentry/react'
 
 function App(): JSX.Element {
+  /* TODO: Delete message and setMessage useState hook, fallbackComponent, 
+  ExampleLandingPage once we actually set the app with actual pages 
+  and routing. These are just for testing Sentry's ErrorBoundary component
+  fallback behavior.  
+  */
+  const [message, setMessage] = useState('Hello World!')
+
+  const fallbackComponent = () => (
+    <div>
+      <h1>Sorry, an error has occurred. Please refresh the page.</h1>
+    </div>
+  )
+  const ExampleLandingPage = () => {
+    const ErrorTestFunction = (str1: string, str2: any) => {
+      const newStr = str1 + str2
+      setMessage(str2)
+    }
+    return (
+      <div>
+        <h1>React Boilerplate</h1>
+        <h2>
+          {message} - {message.length}
+        </h2>
+        <button onClick={() => ErrorTestFunction('hello', undefined)} type="submit">
+          Break the world
+        </button>
+      </div>
+    )
+  }
   /* TODO: As set up actual pages and routing we need determine 
   the specific behavior and props we like from Sentry ErrorBoundary(ies)
   https://docs.sentry.io/platforms/javascript/guides/react/components/errorboundary/
   */
-  const [message, setMessage] = useState('Hello World!')
-
-  const fallBackComponent = () => <div>An error has occurred.</div>
-  const ErrorTestFunction = (str1: string, str2: any) => {
-    const newStr = str1 + str2
-    setMessage(str2)
-  }
-
   return (
-    <Sentry.ErrorBoundary fallback={fallBackComponent}>
-      <h1>React Boilerplate</h1>
-      <h2>
-        {message} - {message.length}
-      </h2>
-      <button onClick={() => ErrorTestFunction('hello', undefined)} type="submit">
-        Break the world
-      </button>
+    <Sentry.ErrorBoundary fallback={fallbackComponent}>
+      <ExampleLandingPage />
     </Sentry.ErrorBoundary>
   )
 }
