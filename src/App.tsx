606fb8dafe249845d64fa0911c71b5e2635244e7
codysmith287 <csmith@shift3tech.com>
2021-03-23 12:33:45 -0700

    feat(App.tsx): add Sentry.ErrorBoundary component to App.tsx for ensure errors are being captured and sent

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react'
+import * as Sentry from '@sentry/react'
 
-function App() {
+function App(): JSX.Element {
+  /* TODO: As set up actual pages and routing we need determine 
+  the specific behavior and props we like from Sentry ErrorBoundary(ies)
+  https://docs.sentry.io/platforms/javascript/guides/react/components/errorboundary/
+  */
+  const [message, setMessage] = useState('Hello World!')
+
+  const fallBackComponent = () => <div>An error has occurred.</div>
+  const ErrorTestFunction = (str1: string, str2: any) => {
+    const newStr = str1 + str2
+    setMessage(str2)
+  }
+
   return (
-    <>
+    <Sentry.ErrorBoundary fallback={fallBackComponent}>
       <h1>React Boilerplate</h1>
-    </>
-  );
+      <h2>
+        {message} - {message.length}
+      </h2>
+      <button onClick={() => ErrorTestFunction('hello', undefined)} type="submit">
+        Break the world
+      </button>
+    </Sentry.ErrorBoundary>
+  )
 }
 
-export default App;
+export default App
