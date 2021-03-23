5ec97302f7540117c65ebf79512c9587aaf7c77c
codysmith287 <csmith@shift3tech.com>
2021-03-23 12:16:18 -0700

    feat(index.tsx): add sentry dsn to index.tsx per docs

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import * as Sentry from '@sentry/react'
+import reportWebVitals from './reportWebVitals'
+import App from './App'
+
+Sentry.init({
+  dsn: 'https://943bdfd77eed4be6b91a42aa0ce6a29c@o68356.ingest.sentry.io/5622322',
+})
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
-);
+)
 
-reportWebVitals();
+reportWebVitals()
