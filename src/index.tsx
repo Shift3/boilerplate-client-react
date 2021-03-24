61a49a27c198a2f861b6c1243efa8d3160615439
codysmith287 <csmith@shift3tech.com>
2021-03-23 19:14:44 -0700

    feat(index.tsx): incorporate PR feedback and add code comment for Sentry.init

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,14 @@ import * as Sentry from '@sentry/react'
 import reportWebVitals from './reportWebVitals'
 import App from './App'
 
+/* TODO: - CMS 3/23/21
+- Add in configuration for toggling autoSesssionTracking
+- Discuss adding in a release option with the team
+- Discuss an environment folder with the team. 
+*/
 Sentry.init({
   dsn: 'https://943bdfd77eed4be6b91a42aa0ce6a29c@o68356.ingest.sentry.io/5622322',
+  autoSessionTracking: false,
 })
 
 ReactDOM.render(
