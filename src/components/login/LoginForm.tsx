387d20c3e72b7a2573b5adb5bb1521d49061af30
Elizabeth Eidelson <fluturecode@gmail.com>
2021-03-29 15:14:34 -0400

    feat(login):updated LoginForm.tsx;

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,46 +1,47 @@
-import { Field, Form, Formik } from 'formik'
 import React, { useState } from 'react'
 import Button from 'react-bootstrap/button'
+import Form from 'react-bootstrap/form'
 
 interface ILoginFormProps {
   onSubmit: (username: string, password: string) => void
 }
 
-type ILoginFormValues = {
+interface ILoginFormValues {
   username: string
   password: string
 }
 
 const LoginForm: React.FC<ILoginFormProps> = ({ onSubmit }: ILoginFormProps) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
   const initialValues: ILoginFormValues = {
     username: '',
     password: '',
   }
 
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-
-  const handleSubmit = (event: React.FormEvent) => {
+  const handleSubmit = (event: React.FormEvent): void => {
     event.preventDefault()
 
     if (!username || !password) {
       return
     }
+
     onSubmit(username, password)
   }
 
   return (
-    <>
-      <h1>Login</h1>
-      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-        <Form>
-          <Field name="username" type="text" label="Username" required />
-          <Field name="password" type="password" label="Password" required />
-          <Button type="submit">Submit</Button>
-        </Form>
-      </Formik>
-    </>
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="loginFormUsername">
+        <Form.Label>Username</Form.Label>
+        <Form.Control type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+      </Form.Group>
+      <Form.Group controlId="loginFormPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      </Form.Group>
+      <Button type="submit">Submit</Button>
+    </Form>
   )
 }
-
 export default LoginForm
