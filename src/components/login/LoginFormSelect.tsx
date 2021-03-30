a32e0dc2395156ae79b000ecc9e8263d6a65502e
Elizabeth Eidelson <fluturecode@gmail.com>
2021-03-29 21:16:50 -0400

    feat*(login):updated LoginFormSelec.tsx

--- src/components/login/LoginFormSelect.tsx
+++ src/components/login/LoginFormSelect.tsx
@@ -8,14 +8,16 @@ export interface FormikSelectItem {
 
 interface FormikSelectProps {
   username: string
+  password: string
   items: FormikSelectItem[]
   label: string
   required?: boolean
 }
 
-const FormikSelect: React.FC<FormikSelectProps> = ({ username, items, label, required = true }) => (
+const FormikSelect: React.FC<FormikSelectProps> = ({ username, label, password, required = true }) => (
   <>
-    <Field name={username} label={label} />
+    <Field name={username} label={label} required={required} />
+    <Field name={password} label={label} required={required} />
   </>
 )
 
