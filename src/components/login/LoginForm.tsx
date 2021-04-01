c32070676bfa26763571556a5776acd42e24c193
Elizabeth Eidelson <fluturecode@gmail.com>
2021-03-31 21:38:12 -0400

    feat(login):added Yup detailed validation to LoginForm.tsx

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -7,9 +7,25 @@ const LoginKey: { [P in keyof ILogin]: P } = {
   password: 'password',
 }
 
+const lowercaseRegex = /(?=.*[a-z])/
+const uppercaseRegex = /(?=.*[A-Z])/
+const numericRegex = /(?=.*[0-9])/
+const specialCharRegex = /(?=.*\W)/
+
 const loginSchema: yup.SchemaOf<ILogin> = yup.object().shape({
-  username: yup.string().required(),
-  password: yup.string().required(),
+  username: yup.string().min(6, 'Username must be at least 6 characters.').required(),
+  // password will be verified from backend for login, but the lines below are to demonstrate how Yup validation is working, and will be removed in next PR.
+  password: yup
+    .string() // Must contain at least 1 uppercase character
+    .matches(uppercaseRegex, 'One upper case character required.')
+    // Must contain at least 1 lowercase character
+    .matches(lowercaseRegex, 'One lower case character required.')
+    // Must contain at least 1 numeric character
+    .matches(numericRegex, 'One number is required.')
+    // Must contain at least 1 special character
+    .matches(specialCharRegex, 'One special character is required.')
+    .min(8, 'Minimum 8 character required.')
+    .required('Password is required.'),
 })
 
 interface ILogin {
