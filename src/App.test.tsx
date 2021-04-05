2e8680cbdcc8daac79c857000654cf8c6854d384
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-04-05 11:14:29 -0700

    test(App): refactor test to check that App component renders

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import App from './App'
 
 test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/react boilerplate/i)
-  expect(linkElement).toBeInTheDocument()
+  const { container } = render(<App />)
+  expect(container).toBeInTheDocument()
 })
