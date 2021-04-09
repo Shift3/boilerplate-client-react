b04603adf6f64e3bc44387c635099738803c405b
Elizabeth Eidelson <fluturecode@gmail.com>
2021-04-08 21:38:24 -0400

    feat(login):added containers to loogin page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,17 +2,24 @@ import React from 'react'
 import styled from 'styled-components'
 import { LoginForm } from '../components/login/LoginForm'
 
-const Wrapper = styled.div`
+const LoginWrapper = styled.div`
   display: grid;
   grid-template: 15vh 70vh 15vh / 20% auto 20%;
 `
 const LoginGrid = styled.main`
   display: grid;
-  grid-template: 70vh / 33.3% 66.6%;
+  grid-template: 40vh / 33.3% 66.6%;
   grid-area: 2 / 2 / 2 / 2;
-  border-radius: 15.78px;
+  border-radius: 18px;
   overflow: hidden;
   box-shadow: 0 2px 16px 0 rgba(0, 0, 0, 0.28);
+  border: 2px solid #175f6e;
+`
+const LoginFormContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 2;
 `
 
 const HomePage: React.FC = () => {
@@ -20,11 +27,13 @@ const HomePage: React.FC = () => {
   const onLoginFormSubmit = (username: string, password: string) => {}
 
   return (
-    <Wrapper>
+    <LoginWrapper>
       <LoginGrid>
-        <LoginForm onSubmit={onLoginFormSubmit} />
+        <LoginFormContainer>
+          <LoginForm onSubmit={onLoginFormSubmit} />
+        </LoginFormContainer>
       </LoginGrid>
-    </Wrapper>
+    </LoginWrapper>
   )
 }
 
