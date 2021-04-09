09310787d6e90f01573276b709031baf45ed2b1d
Elizabeth Eidelson <fluturecode@gmail.com>
2021-04-09 08:30:03 -0400

    feat(login):added route for LoginPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import * as Sentry from '@sentry/react'
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { LoginPage } from './pages/LoginPage'
 
-const App = (): JSX.Element => (
+export const App: React.FC = () => (
   <Sentry.ErrorBoundary>
-    <LoginPage />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={LoginPage} />
+      </Switch>
+    </Router>
   </Sentry.ErrorBoundary>
 )
-
-export default App
