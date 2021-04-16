43862d5f20e631f6c2e5e5447a2b15fe2ffc8754
Adam Weaver <parkersalterego92@gmail.com>
2021-04-16 15:52:21 -0700

    fix(auth.actions): made it more clear that current payload being dispatched in loginUser action contains fake data

--- src/context/actions/auth.actions.ts
+++ src/context/actions/auth.actions.ts
@@ -1,14 +1,10 @@
 import { Dispatch } from 'react';
 import Types from '../action.types';
-import { LogoutUserAction, LoginUserAction, IAction } from './types'
+import { LogoutUserAction, LoginUserAction, IAction } from './types';
 import { ILoginFormData } from '../../components/login/types';
 
 export const logoutUser: LogoutUserAction = (dispatch: Dispatch<IAction>) => (payload: null) => dispatch({ type: Types.LOGOUT_USER, payload });
-export const loginUser: LoginUserAction = (dispatch: Dispatch<IAction>) => async (payload: ILoginFormData) => {
-    // eslint-disable-next-line
-    console.log(payload);
-    
-    // make call via http service to server and receive ILoginResponse
-
-    dispatch({ type: Types.LOGIN_USER, payload: { user: { username: "test", _id: "123456" }, token: "hfuewfue473827943$%#*@($" } });
+export const loginUser: LoginUserAction = (dispatch: Dispatch<IAction>) => async (payload: ILoginFormData) => {    
+    // @TODO make call via http service to server and receive userData and pass to dispatch below (mock data being used in the mean time)
+    dispatch({ type: Types.LOGIN_USER, payload: { user: { username: "fakeUser", _id: "fakeId123456" }, token: "fakeToken73827943" } });
 };
