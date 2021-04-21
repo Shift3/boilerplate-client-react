c7c070bf4aa571f42f67a1fd166a22f143f534a7
Elizabeth Eidelson <fluturecode@gmail.com>
2021-04-20 23:14:05 -0400

    feat(holy-grail):medited .prettierrc.js and styled.ts

--- src/components/holyGrailLayout/styled.ts
+++ src/components/holyGrailLayout/styled.ts
@@ -1,30 +1,28 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const HolyGrailWrapper = styled.div`
   display: flex;
+  min-height: 100vh;
   flex-direction: column;
-  align-items: stretch;
-  height: 100vh;
   @media all and (min-width: 768px) {
     flex-direction: row;
     flex-wrap: wrap;
   }
-`
+`;
 
 export const HolyGrailMain = styled.main`
-  flex: 1;
-  order: 2;
+  display: flex;
   @media all and (min-width: 768px) {
     flex: 2;
     order: 2;
     min-height: 80vh;
   }
-`
+`;
 
 export const HolyGrailNavLeft = styled.aside`
   flex: 1;
   order: 1;
-`
+`;
 
 export const HolyGrailNavRight = styled.aside`
   display: none;
@@ -32,10 +30,10 @@ export const HolyGrailNavRight = styled.aside`
     order: 3;
     flex: 1;
   }
-`
+`;
 
 export const HolyGrailFooter = styled.footer`
   flex: shrink;
   width: 100vw;
   order: 4;
-`
+`;
