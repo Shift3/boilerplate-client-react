63b25c70cb9c0aca145e3038e0db308a21e45d77
Adam Weaver <parkersalterego92@gmail.com>
2021-04-26 10:20:20 -0700

    feat(formBuilder types): types file added for formBuilder component

--- src/components/formBuilder/types.ts
+++ src/components/formBuilder/types.ts
@@ -1,22 +1,58 @@
-import { AnyObjectSchema } from 'yup';
+import { SetStateAction } from "react";
+import { SchemaOf } from 'yup';
 
-export interface IFormConfig {
-    name: string
-    fieldType: string,
-    label: string,
-    text: string,
-    placeholder: string,
-    fieldConfig: {
-      inputType: string,
-      autocomplete: AutocompleteType,
-    }
+export interface IRegisterFormData {
+  email: string,
+  confirmEmail: string,
+  firstName: string,
+  lastName: string
 }
+
+export interface ILoginFormData {
+  username: string,
+  password: string
+}
+
+export interface ISetPasswordFormData {
+  password: string,
+  confirmPassword: string
+}
+
+export interface IInputConfig {
+    name: string
+    inputType: "input",
+    type: string,
+    label: string | undefined,
+    text: string,
+    placeholder: string | undefined,
+    initialValue: null
+    required: boolean
+    autocomplete: AutocompleteType
+}
+
+export interface IInputData {
+    touched: boolean
+    value: any,
+    error: string,
+    required: boolean
+  }
 
 export interface IFormProps {
-    schema: AnyObjectSchema
-    config: [IFormConfig]
+    schemaGenerator: SchemaGenerator,
+    config: IInputConfig[],
     onSubmit: (formData: Record<string, unknown>) => void
+    title: string
 }
+
+export type SetFormState = SetStateAction<{ values: Record<string, any>; touched: boolean; errors: never[]; }>
+
+export type GenerateSetPasswordFormSchema = (stateRef: Record<string, any>) => SchemaOf<ISetPasswordFormData>;
+export type GenerateLoginFormSchema = () => SchemaOf<ILoginFormData>
+export type GenerateRegisterFormSchema = (stateRef: Record<string, any>) => SchemaOf<IRegisterFormData>;
+
+export type SchemaGenerator = GenerateLoginFormSchema | GenerateSetPasswordFormSchema | GenerateRegisterFormSchema;
+
+export type FormData = ILoginFormData | IRegisterFormData | ISetPasswordFormData;
 
 export type AutocompleteType =
   | 'off'
