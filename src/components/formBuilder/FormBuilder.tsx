9613717aeea89256abdcde45dd4d7a816d9b4fec
Adam Weaver <parkersalterego92@gmail.com>
2021-04-26 10:19:28 -0700

    feat(updated formBuilder): updated form builder to take in a config, schema, and onSubmit method in order to generate, validate, and submit the form

--- src/components/formBuilder/FormBuilder.tsx
+++ src/components/formBuilder/FormBuilder.tsx
@@ -1,44 +1,84 @@
-import { yupResolver } from '@hookform/resolvers/yup';
-import { FC } from 'react';
-import { useForm } from 'react-hook-form';
-import { Form, Button, Container} from 'react-bootstrap'
-import { v4 as uuidv4 } from 'uuid';
-import { IFormProps } from './types';
+import React, { useState, useEffect, SyntheticEvent } from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
+import * as _ from 'lodash';
+import * as yup from 'yup';
+import { IFormProps, IInputConfig, IInputData } from './types';
 
-export const FormBuilder: FC<any> = ({ onSubmit, schema, config }: IFormProps) => {
-  const { register, handleSubmit, errors } = useForm({ resolver: yupResolver(schema) })
+export const FormBuilder: (props: IFormProps) => JSX.Element = ({ config, onSubmit, schemaGenerator, title }: IFormProps) => {
+  const [formData, setFormData] = useState({});
 
-  const onFormSubmit = handleSubmit((formData) => onSubmit(formData));
+  const initialInputValue: IInputData = { touched: false, value: undefined, error: "", required: false };
+
+  useEffect(() => {
+    setFormData(config.reduce((initialFormData, { name, required }: IInputConfig) => (
+      { ...initialFormData, [name]: { ...initialInputValue, required } }
+    ), {}))
+  }, [])
+
+  const canSubmit = Object.keys(formData).filter((key) => {
+    const { touched, value, error, required } = _.get(formData, `[${key}]`, initialInputValue);
+    return !!error || ((!value || !touched) && required)
+  }).length === 0;
+
+  const formatFormData: () => Record<string, unknown> = () => (
+    Object.keys(formData).reduce((data: Record<string, unknown>, key: string) => (
+      { ...data, [key]: _.get(formData, `[${key}.value]`, undefined) }
+    ), {})
+  );
+
+  const onInputChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setFormData({
+      ...formData,
+      [name]: {
+        value,
+        touched: true,
+        error: await yup.reach(schemaGenerator(formatFormData()), name)
+        .validate(value)
+          .then(() => "")
+            .catch((err: yup.ValidationError) => err.message)  
+      }
+    });
+  }
+
+  const renderInputError = (name: string) => {
+    const error = _.get(formData, `[${name}].error`, undefined);
+
+    return error ? (<span role="alert">{ error }</span>) : null;
+  }
+
+  const renderInputs = () => (
+    config.map(({ name, label, type, inputType, text, autocomplete }: IInputConfig) => (
+      <Form.Group key={name} >
+        { label && <Form.Label>{ label }</Form.Label> } 
+        {
+          inputType === "input" &&
+            <Form.Control
+            name={ name }
+            type={ type }
+            autoComplete={ autocomplete }
+            onChange={ onInputChange }
+            />
+        }
+        { renderInputError(name) }
+        { text && <Form.Text>{ text }</Form.Text> }
+      </Form.Group>
+    ))
+  )
+
+  const onFormSubmit = (e: SyntheticEvent) => {
+    e.preventDefault();
+    onSubmit(formatFormData());
+  }
 
   return (
     <Container>
-      <Form onSubmit={onFormSubmit}>
-        { config.map((field) => (
-            <Form.Group controlId="formBasicEmail" key={ uuidv4() }>
-              { 
-                field.label 
-                  && <Form.Label>{ field.label }</Form.Label> 
-              }
-              <Form.Control
-                type={ field.fieldConfig.inputType }
-                autoComplete={ field.fieldConfig.autocomplete } 
-                placeholder={ field.placeholder } 
-                name={ field.name } 
-                ref={register}
-              />
-              {  
-                field.text 
-                  && <Form.Text className="text-muted">{ field.text }</Form.Text> 
-              }
-              { 
-                errors[field.name] 
-                  && <span role="alert" className="danger">{errors[field.name].message}</span>
-              }
-            </Form.Group>
-        ))}
-
-        <Button variant="primary" type="submit">Submit</Button>
-      </Form>
+      { title && <h2>{ title }</h2> }
+       <Form onSubmit={ onFormSubmit }>
+         { renderInputs() }
+         <Button type="submit" disabled={ !canSubmit }>Submit</Button>
+       </Form>
     </Container>
-  )
+  );
 }
