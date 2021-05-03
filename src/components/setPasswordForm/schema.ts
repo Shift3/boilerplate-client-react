9b3eb01c75f9b638c67a2bb1ff9f59d8abf185b3
Adam Weaver <parkersalterego92@gmail.com>
2021-05-03 11:11:49 -0700

    feat(setPassword Schema): SetPassword Schema updated to utilize errorMessages enum to display custom errorMessages and enable tests to hook in and make confirmations based on the errorMessages enum

--- src/components/setPasswordForm/schema.ts
+++ src/components/setPasswordForm/schema.ts
@@ -1,7 +1,19 @@
 import * as yup from 'yup';
 
+export enum errorMessages {
+    PASSWORD_REQUIRED = "Password is required.",
+    PASSWORD_LENGTH = "Password must be between 7 and 12 characters.",
+    PASSWORD_MATCH = "Passwords must match."
+}
+
 export const SetPasswordFormSchema =  yup.object().shape({
-    password: yup.string().min(7).max(2).required(),
-    confirmPassword: yup.string().min(7).max(2).required()
-        .oneOf([yup.ref("password"), null], 'Passwords must match.'),
+    password: yup.string()
+        .min(7, errorMessages.PASSWORD_LENGTH)
+        .max(12, errorMessages.PASSWORD_LENGTH)
+        .required(errorMessages.PASSWORD_REQUIRED),
+    confirmPassword: yup.string()
+        .min(7, errorMessages.PASSWORD_LENGTH)
+        .max(12, errorMessages.PASSWORD_LENGTH)
+        .required(errorMessages.PASSWORD_REQUIRED)
+        .oneOf([yup.ref("password"), null], errorMessages.PASSWORD_MATCH),
 });
