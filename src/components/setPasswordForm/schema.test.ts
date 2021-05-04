e6f2b18a3d3ab6b8dfcc879dc07c839a7f7ce261
Adam Weaver <parkersalterego92@gmail.com>
2021-05-04 09:16:19 -0700

    test(setPasswordForm): schema and tests modified and passing

--- src/components/setPasswordForm/schema.test.ts
+++ src/components/setPasswordForm/schema.test.ts
@@ -1,70 +1,42 @@
 import { SetPasswordFormSchema, errorMessages } from './schema';
 import * as yup from 'yup';
 
+const { PASSWORD_REQUIRED, PASSWORD_LENGTH, PASSWORD_MATCH } = errorMessages;
+
 describe('loginFormSchema', () => {
     const validPassword = 'Password123!';
     const mismatchPassword = 'Password123';
+    const blankPassword = '';
     const shortPassword= 'test';
     const longPassword = 'passwordtoolong';
 
+    const formData = { 
+        password: validPassword, 
+        confirmPassword: validPassword
+    };
+
+    const errorMessageCheck = async (field: string, value: string, message: string) => (
+        expect(await yup.reach(SetPasswordFormSchema, field).validate(value)
+                .catch((err: yup.ValidationError) => err.message)).toEqual(message)
+    );
+
+    const errorMessageConfirmCheck = async (formData: Record<string, any>, field: string, mismatch: string, message: string) => (
+        expect(await SetPasswordFormSchema.validate({ ...formData, [field]: mismatch }).catch(err => err.message)).toEqual(message)
+    );
+
     describe('Valid input data', () => {
-        it('Should pass validation', () => {
-        expect(SetPasswordFormSchema
-            .isValidSync({ password: validPassword, confirmPassword: validPassword }))
-                .toBeTruthy()
-        })
-    })
+        it('Should pass validation', () => expect(SetPasswordFormSchema.isValidSync(formData)).toBeTruthy());
+    });
 
     describe('Password', () => {
-        it('Should be required', () => {
-        expect(yup.reach(SetPasswordFormSchema, "password")
-            .validate('')
-                .catch((err: yup.ValidationError) => err.message))
-                    .toEqual(errorMessages.PASSWORD_REQUIRED);
-        })
-
-        it('Should be at least 7 characters', () => {
-        expect(yup.reach(SetPasswordFormSchema, "password")
-            .validate(shortPassword)
-                .catch((err: yup.ValidationError) => err.message))
-                    .toEqual(errorMessages.PASSWORD_LENGTH);
-        })
-
-        it('Should be at most 12 characters', () => {
-            expect(yup.reach(SetPasswordFormSchema, "password")
-            .validate(longPassword)
-                .catch((err: yup.ValidationError) => err.message))
-                    .toEqual(errorMessages.PASSWORD_LENGTH);
-        })
+        it('Should be required', async () => errorMessageCheck('password', '', PASSWORD_REQUIRED));
+        it('Should be at least 7 characters', () => errorMessageCheck('password', shortPassword, PASSWORD_LENGTH));
+        it('Should be at most 12 characters', () => errorMessageCheck('password', longPassword, PASSWORD_LENGTH));
     })
 
     describe('ConfirmPassword', () => {
-        it('Should be required', () => {
-            expect(yup.reach(SetPasswordFormSchema, "confirmPassword")
-            .validate('')
-                .catch((err: yup.ValidationError) => err.message))
-                    .toEqual(errorMessages.PASSWORD_REQUIRED);
-        })
-
-        it('Should be at least 7 characters', () => {
-            expect(yup.reach(SetPasswordFormSchema, "confirmPassword")
-                .validate(shortPassword)
-                    .catch((err: yup.ValidationError) => err.message))
-                        .toEqual(errorMessages.PASSWORD_LENGTH);
-            })
-
-        it('Should be at most 12 characters', () => {
-            expect(yup.reach(SetPasswordFormSchema, "confirmPassword")
-                .validate(longPassword)
-                    .catch((err: yup.ValidationError) => err.message))
-                        .toEqual(errorMessages.PASSWORD_LENGTH);
-        })
-
-        it('Should match password', () => {
-            expect(SetPasswordFormSchema
-                .validate({ password: validPassword, confirmPassword: mismatchPassword })
-                    .catch(err => err.message))
-                        .toEqual(errorMessages.PASSWORD_MATCH);
-        })
-    })
-})
+        const updatedFormData = { password: '', confirmPassword: '' };
+        it('Should be required', () => errorMessageConfirmCheck(updatedFormData, 'confirmPassword', blankPassword, PASSWORD_REQUIRED));
+        it('Should match password', () => errorMessageConfirmCheck(formData, "confirmPassword", mismatchPassword, PASSWORD_MATCH));
+    });
+});
--- src/components/setPasswordForm/schema.ts
+++ src/components/setPasswordForm/schema.ts
@@ -8,12 +8,10 @@ export enum errorMessages {
 
 export const SetPasswordFormSchema =  yup.object().shape({
     password: yup.string()
+        .required(errorMessages.PASSWORD_REQUIRED)
         .min(7, errorMessages.PASSWORD_LENGTH)
-        .max(12, errorMessages.PASSWORD_LENGTH)
-        .required(errorMessages.PASSWORD_REQUIRED),
+        .max(12, errorMessages.PASSWORD_LENGTH),
     confirmPassword: yup.string()
-        .min(7, errorMessages.PASSWORD_LENGTH)
-        .max(12, errorMessages.PASSWORD_LENGTH)
         .required(errorMessages.PASSWORD_REQUIRED)
         .oneOf([yup.ref("password"), null], errorMessages.PASSWORD_MATCH),
 });
