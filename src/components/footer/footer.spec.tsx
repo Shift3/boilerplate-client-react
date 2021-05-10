008e4db1423fde163ef70cb5021213a1a859b9e2
Elizabeth Eidelson <fluturecode@gmail.com>
2021-05-10 12:01:02 -0400

    test(footer):updated footer.spec.tsx

--- src/components/footer/footer.spec.tsx
+++ src/components/footer/footer.spec.tsx
@@ -1,10 +1,54 @@
-import { render, rerender, getByText, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { unmountComponentAtNode } from 'react-dom';
 import { Footer, copywriteDate, creationYear } from './index';
 import { Constants } from '../../utils/constants';
 import { Wrapper } from './styled';
 
 const { version } = Constants;
 const { getByRole } = screen;
+
+let container: any = null;
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+describe('Footer', () => {
+  let footer: HTMLElement;
+  beforeEach(() => {
+    render(<Footer />);
+    footer = getByRole('footer');
+  });
+
+  describe('render()', () => {
+    const { container, getByText } = render(<Footer />);
+    it('Should contain the creationYear', () => {
+      expect(getByText('2021')).toBeInTheDocument();
+    });
+    it('Should contain the Version', () => {
+      const { container, getByText } = render(<Footer />);
+      expect(getByText(/Bitwise Technology Constulting - 0.1.0 Staging/)).toBeInTheDocument();
+    });
+    it('Should contain the properly formatted copyright date', () => {
+      expect(getByText('copywriteDate')).toBeInTheDocument();
+      expect(copywriteDate()).toEqual(`${creationYear}` || `${creationYear} - ${new Date().getFullYear()}`);
+    });
+  });
+
+  describe('Wrapper', () => {
+    let wrapper: HTMLElement;
+  });
+
+  describe('render()', () => {
+    const { container, getByText } = render(<Wrapper />);
+  });
+});
 
 // class State {
 //   constructor() {
@@ -24,33 +68,3 @@ const { getByRole } = screen;
 //   useParams: () => jest.fn(),
 //   useHistory: () => jest.fn(),
 // }));
-
-describe('Footer', () => {
-  let footer: HTMLElement;
-  // beforeEach(() => {
-  //   render(<Footer />);
-  //   footer = getByRole('footer');
-});
-
-describe('render()', () => {
-  const { container, getByText } = render(<Footer />);
-  it('Should contain the creationYear', () => {
-    expect(getByText('2021')).toBeInTheDocument();
-  });
-  it('Should contain the Version', () => {
-    const { container, getByText } = render(<Footer />);
-    expect(getByText(/Bitwise Technology Constulting - 0.1.0 Staging/)).toBeInTheDocument();
-  });
-  it('Should contain the properly formatted copyright date', () => {
-    // expect(getByText('copywriteDate')).toBeInTheDocument();
-    //   expect(copywriteDate()).toEqual(`${creationYear}` || `${creationYear} - ${new Date().getFullYear()}`);
-  });
-});
-
-describe('Wrapper', () => {
-  let wrapper: HTMLElement;
-});
-
-describe('render()', () => {
-  const { container, getByText } = render(<Wrapper />);
-});
