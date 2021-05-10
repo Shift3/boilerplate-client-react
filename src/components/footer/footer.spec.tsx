eb148b3725ec5a7a5b4b113066efead2157cf728
Elizabeth Eidelson <fluturecode@gmail.com>
2021-05-10 18:35:37 -0400

    test(footer):updated render methos in footer.spec.tsx

--- src/components/footer/footer.spec.tsx
+++ src/components/footer/footer.spec.tsx
@@ -1,5 +1,4 @@
-import { render, screen } from '@testing-library/react';
-import { unmountComponentAtNode } from 'react-dom';
+import { render, rerender, getByText, screen } from '@testing-library/react';
 import { Footer, copyrightDate, creationYear } from './index';
 import { Constants } from '../../utils/constants';
 import { Wrapper } from './styled';
@@ -7,53 +6,55 @@ import { Wrapper } from './styled';
 const { version } = Constants;
 const { getByRole } = screen;
 
-let container: any = null;
+test('screen debug element', () => {
+  render(<Footer />);
+  screen.debug();
+});
 
+test('should return element based on its role', () => {
+  const { getByRole } = render(<div role="footer" />);
+  expect(getByRole('footer')).toMatchInlineSnapshot(`
+    <div
+      role="footer"
+    />
+  `);
+});
+
+let container = null;
 beforeEach(() => {
   container = document.createElement('div');
   document.body.appendChild(container);
 });
 
-afterEach(() => {
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
-//
-// describe('Footer', () => {
-//   let footer: HTMLElement;
-//   beforeEach(() => {
-//     render(<Footer />);
-//     footer = getByRole('footer');
-//   });
-
 describe('footer', () => {
   let footer: HTMLElement;
-  beforeEach(() => {
-    render(<Footer />);
-    footer = getByRole('footer');
-  });
-  const { getByText } = render(<Footer />);
-  it('Should contain the creationYear', () => {
-    expect(container.textContent).toBe('2021');
-  });
-  it('Should contain the Version', () => {
+
+  describe('render()', () => {
     const { getByText } = render(<Footer />);
-    expect(container.textContent).toBe(/Bitwise Technology Constulting - 0.1.0 Staging/);
-  });
-  it('Should contain the properly formatted copyright date', () => {
-    expect(getByText('copywriteDate')).toBeInTheDocument();
-    expect(copyrightDate()).toEqual(`${creationYear}` || `${creationYear} - ${new Date().getFullYear()}`);
+
+    it('Should contain the creationYear', () => {
+      const { getByText } = render('2021');
+      expect(getByText(2021)).toBeInTheDocument();
+    });
+    it('Should contain the Version', () => {
+      const { getByText } = render(<Footer />);
+      expect(getByText(/Bitwise Technology Constulting - 0.1.0 Staging/)).toBeInTheDocument();
+    });
+    it('Should contain the properly formatted copyright date', () => {
+      const { getByText } = render('copyrightDate');
+      expect(getByText('copyrightDate')).toBeInTheDocument();
+      expect(copyrightDate()).toEqual(`${creationYear}` || `${creationYear} - ${new Date().getFullYear()}`);
+    });
   });
 });
 
-describe('Wrapper', () => {
-  let wrapper: HTMLElement;
-});
-
-describe('render()', () => {
-  const { getByText } = render(<Wrapper />);
-});
+// describe('Wrapper', () => {
+//   let wrapper: HTMLElement;
+// });
+//
+// // describe('render()', () => {
+// //   const { container, getByText } = render(<Wrapper />);
+// // });
 
 // class State {
 //   constructor() {
@@ -72,4 +73,4 @@ describe('render()', () => {
 // jest.mock('react-router', () => ({
 //   useParams: () => jest.fn(),
 //   useHistory: () => jest.fn(),
-// }));
+// }))
