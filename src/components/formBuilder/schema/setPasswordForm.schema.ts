e0231fe6d3c581272bc54bc1661ba512bd58b0cd
Elizabeth Eidelson <fluturecode@gmail.com>
2021-05-10 20:17:28 -0400

    fix(rebase):Removed unused directories

--- src/components/formBuilder/schema/setPasswordForm.schema.ts
+++
@@ -1,8 +0,0 @@
-import * as yup from 'yup';
-import { GenerateSetPasswordFormSchema, StateRef } from '../types';
-
-export const generateSetPasswordFormSchema: GenerateSetPasswordFormSchema = (stateRef: StateRef) =>
-  yup.object({
-    password: yup.string().min(7).max(12).required(),
-    confirmPassword: yup.string().required().oneOf([stateRef.password, null], 'Passwords must match.'),
-  });
