5966804a473462f7766cb546ea5edfb6b7f75a07
Elizabeth Eidelson <elizabetheidelson@Elizabeths-MacBook-Air.local>
2021-05-11 12:07:58 -0400

    fix(conflicts):fixed all merge conflicts

--- src/components/signupForm/index.tsx
+++ src/components/signupForm/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { Form, Button } from 'react-bootstrap';
