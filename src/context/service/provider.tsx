1c0ab56b5506279281c89dc032c7d5b3d1f048d3
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-05-13 13:24:33 -0700

    fix(context/service): wrap provider in React.memo to prevent rerendering

--- src/context/service/provider.tsx
+++ src/context/service/provider.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from 'react';
+import { FC, memo, ReactNode } from 'react';
 import { UserService } from '../../services/api/user.service';
 import { Context as ServiceContext } from './context';
 
@@ -6,10 +6,10 @@ interface IProps {
   children: ReactNode;
 }
 
-export const Provider: FC<IProps> = ({ children }) => {
+export const Provider: FC<IProps> = memo(({ children }) => {
   const services = {
     userService: new UserService(),
   };
 
   return <ServiceContext.Provider value={services}>{children}</ServiceContext.Provider>;
-};
+});
