34d97a6342d7522c45836a1d76d0f9b808b02945
Elizabeth Eidelson <fluturecode@gmail.com>
2021-05-16 23:12:39 -0400

    test(holyGrail):added index.spec.tsx

--- src/components/holyGrailLayout/__tests/index.spec.tsx
+++ src/components/holyGrailLayout/__tests/index.spec.tsx
@@ -5,21 +5,45 @@ import { HolyGrailLayout } from '../index';
 // const { getByTestId } = screen;
 // let copyright: HTMLElement;
 
-describe('holyGrailLayout', ({leftSidebar, children }) => {
+describe('holyGrailLayout', () => {
   beforeEach(() => {
     render(<HolyGrailLayout />);
   });
   screen.debug();
-  it('Should render the left sidebar, () => {
-    // expect(leftSidebar).toBeInTheDocument());
-  // it('Should contain the version', () => {
-  //   expect(copyright).toHaveTextContent(version);
-  // });
-  // it('Should contain the copyright date', () => {
-  //   expect(copyright).toHaveTextContent(copyrightDate);
-  // });
-  // it('Should display properly formatted copyright date  ', () => {
-  //   expect(copyrightDate).toEqual(`${creationYear}` || `${creationYear} - ${new Date().getFullYear()}`);
-  // });
+  // it('Should render the left sidebar, () => {
 });
-})
+
+// import { render, screen } from '@testing-library/react';
+// import { HolyGrailLayout } from './';
+// import {
+//     HolyGrailWrapper,
+//     HolyGrailMainWrapper,
+//     HolyGrailLeftAside,
+//     HolyGrailMain,
+//     HolyGrailRightAside
+// } from './styled';
+// const { getByTestId } = screen;
+// xdescribe('HolyGrailLayout', () => {
+//   let holyGrailLayout: HTMLElement;
+//   let children: HTMLElement[];
+//   beforeEach(() => {
+//     render(
+//         <HolyGrailLayout
+//             leftSidebar={<div data-testid="leftSidebar"></div>}
+//             rightSidebar={<div data-testid="leftSidebar"></div>}
+//             footer={<div data-testid="leftSidebar"></div>}
+//         >
+//         { /* @TODO MOCK CHILDREN - this will allow the HolyGrailMain wrapper to be rendered as well*/ }
+//         </HolyGrailLayout>
+//     );
+//     holyGrailLayout = getByTestId("holyGrailLayout");
+//   })
+//   it('Should render', () => {
+//     expect(holyGrailLayout).toBeInTheDocument();
+//     expect(HolyGrailWrapper).toBeInTheDocument();
+//     expect(HolyGrailMainWrapper).toBeInTheDocument();
+//     expect(HolyGrailLeftAside).toBeInTheDocument();
+//     // expect(HolyGrailMain).toBeInTheDocument();
+//     expect(HolyGrailRightAside).toBeInTheDocument();
+//   });
+// })
