ff438870434eddeb86fa6ab858d37fed579125a5
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-05-24 16:02:40 -0700

    fix(environment): freeze environment object so it can't be modified at runtime

--- src/environment/index.ts
+++ src/environment/index.ts
@@ -2,6 +2,7 @@ import { IEnvironment } from './interface';
 import { environment as devEnvironment } from './environment';
 import { environment as prodEnvironment } from './environment.prod';
 
-const getEnv = (): IEnvironment => (process.env.NODE_ENV === 'production' ? prodEnvironment : devEnvironment);
+const getEnv = (): IEnvironment =>
+  Object.freeze(process.env.NODE_ENV === 'production' ? prodEnvironment : devEnvironment);
 
 export const environment: IEnvironment = getEnv();
