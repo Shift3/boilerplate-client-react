9ffa57e45fd2fa9926c371fa2134edfa4e548e5e
Adam Weaver <parkersalterego92@gmail.com>
2021-05-24 10:42:06 -0700

    refactor(context): refactored createDataContext to utilize generics passed in upon method execution

--- src/context/index.context.tsx
+++ src/context/index.context.tsx
@@ -1,12 +1,17 @@
-import { createContext, useReducer, ReactNode, ReducerWithoutAction } from 'react';
-import { IAction, LogoutUserAction, LoginUserAction, SetFlashMessageAction } from './actions/types';
-import { IFlashMessageState, IAuthState } from './types';
+import { createContext, useReducer, ReactNode } from 'react';
+import { ProviderType, ReducerType, CreateDataContextType } from './types';
 
-// eslint-disable-next-line
-export type Reducer = ReducerWithoutAction<any> | ((state: Record<string, unknown>, action: IAction) => (Record<string, unknown>));
+const createDataContext: CreateDataContextType = <ActionType extends Function, StateType> (
+  reducer: ReducerType,
+  actions: Record<string, ActionType>,
+  initialState: StateType
+) => {
+  const Context = createContext({});
 
-export type ContextState = Record<string, unknown> | IFlashMessageState | IAuthState;
+  const Provider: ProviderType = ({ children }: Record<string, ReactNode>) => {
+    const [ state, dispatch ] = useReducer(reducer, initialState);
 
+<<<<<<< HEAD
 export type Actions = Record<string, LogoutUserAction | LoginUserAction | SetFlashMessageAction>;
 
 export type CreateContext = (reducer: Reducer, actions: Actions, initialState: ContextState) => {
@@ -24,11 +29,17 @@ const createDataContext: CreateContext = (reducer: Reducer, actions: Actions, in
 
     const boundActions: { [ x: string ]: unknown } = Object.keys(actions)
       .reduce((actionsObj: Record<string, unknown>, key: string) => (
+=======
+    const boundActions: Record<string, ActionType> = Object.keys(actions)
+      .reduce((actionsObj: Record<string, ActionType>, key: string) => (
+>>>>>>> c155670... refactor(context): refactored createDataContext to utilize generics passed in upon method execution
         { ...actionsObj, [ key ]: actions[ key ](dispatch) }
       ), {});
 
     return (
-      <Context.Provider value={{ state, ...boundActions }}>
+      <Context.Provider
+        value={{ state, ...boundActions }}
+      >
         { children }
       </Context.Provider>
     );
