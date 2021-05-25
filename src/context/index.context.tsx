bdb9e1360a4fcdf8bede7f96ba36b82fe79e88e9
Adam Weaver <parkersalterego92@gmail.com>
2021-05-25 16:46:47 -0700

    chore(loginForm): changed directory structure to utilize tests sub directory and .spec within file name of tests

--- src/context/index.context.tsx
+++ src/context/index.context.tsx
@@ -11,28 +11,8 @@ const createDataContext: CreateDataContextType = <ActionType extends Function, S
   const Provider: ProviderType = ({ children }: Record<string, ReactNode>) => {
     const [ state, dispatch ] = useReducer(reducer, initialState);
 
-<<<<<<< HEAD
-export type Actions = Record<string, LogoutUserAction | LoginUserAction | SetFlashMessageAction>;
-
-export type CreateContext = (reducer: Reducer, actions: Actions, initialState: ContextState) => {
-  // eslint-disable-next-line
-  Context: React.Context<Record<string, any>>;
-  Provider: ({ children }: Record<string, ReactNode>) => JSX.Element;
-}
-
-const createDataContext: CreateContext = (reducer: Reducer, actions: Actions, initialState: ContextState) => {
-  // eslint-disable-next-line
-  const Context = createContext<Record<string, any>>({});
-
-  const Provider = ({ children }: Record<string, ReactNode>) => {
-    const [ state, dispatch ] = useReducer(reducer, initialState);
-
-    const boundActions: { [ x: string ]: unknown } = Object.keys(actions)
-      .reduce((actionsObj: Record<string, unknown>, key: string) => (
-=======
     const boundActions: Record<string, ActionType> = Object.keys(actions)
       .reduce((actionsObj: Record<string, ActionType>, key: string) => (
->>>>>>> c155670... refactor(context): refactored createDataContext to utilize generics passed in upon method execution
         { ...actionsObj, [ key ]: actions[ key ](dispatch) }
       ), {});
 
