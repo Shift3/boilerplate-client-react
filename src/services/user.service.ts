2ea98120633454b41e92a8d148490eb224dd4c61
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-05-25 10:28:41 -0700

    feat(user.service): add user sign up method

---
+++ src/services/user.service.ts
@@ -0,0 +1,20 @@
+/* eslint-disable lines-between-class-members */
+import { environment } from 'environment';
+import { ISignupRequest, IUserDTO } from 'models/user';
+import { ApiService } from './api.service';
+
+export class UserService {
+  private controllerRoute = 'users';
+  private url: string;
+  private apiService: ApiService;
+
+  constructor(apiService?: ApiService) {
+    this.apiService = apiService ?? new ApiService();
+    this.url = `${environment.apiRoute}/${this.controllerRoute}`;
+  }
+
+  public async signUp(payload: ISignupRequest): Promise<IUserDTO> {
+    const endpoint = `${this.url}/signup/`;
+    return this.apiService.post<IUserDTO, ISignupRequest>(endpoint, payload);
+  }
+}
