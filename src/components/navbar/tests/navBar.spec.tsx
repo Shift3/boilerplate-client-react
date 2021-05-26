76d0259f2773a272dc1ce9767facfbd350bd75c0
Adam Weaver <parkersalterego92@gmail.com>
2021-05-26 15:18:07 -0700

    test(navbar.spec): switched to utilize Router from react-router-dom in order to ensure that the route is set to the root when rendering for NavLink tests

--- src/components/navbar/tests/navBar.spec.tsx
+++ src/components/navbar/tests/navBar.spec.tsx
@@ -1,70 +1,49 @@
 import { render, screen, cleanup, act } from '@testing-library/react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Router, Switch, Route } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 import { NavBar } from '../';
 import userEvent from '@testing-library/user-event';
 import { FC } from 'react';
 
 const { click } = userEvent;
-const { getByTestId, getByRole } = screen;
+const { getByTestId } = screen;
+
+const clickByTestId = (id: string) => act(() => click(getByTestId(id)));
 
 const expectInDocByTestId = (id: string) => expect(getByTestId(id)).toBeInTheDocument();
-const clickByTestId = (id: string) => act(() => click(getByTestId(id)));
-const genMock: (id: string) => FC = (id) => () => <div data-testid={id}/>;
+
+const clickExpect = (clickId: string, expectId: string) => {
+  clickByTestId(clickId);
+  expectInDocByTestId(expectId);
+};
+
+const generateMockComponent: (id: string) => FC = (id) => () => <div data-testid={id}/>;
 
 describe('<NavBar/>', () => {
-  beforeEach(() => {
-    render(
-      <Router>
-        <Switch>
-          <Route exact path="/" component={NavBar} />
-          <Route exact path="/logout" component={genMock('logout')} />
-          <Route exact path="/users" component={genMock('users')} />
-          <Route exact path="/directory" component={genMock('directory')} />
-        </Switch>
-      </Router>
-    );
-  });
+  beforeEach(() => render(
+    <Router history={createMemoryHistory({ initialEntries: ['/'] })}>
+      <Switch>
+        <Route exact path="/" component={NavBar} />
+        <Route exact path="/logout" component={generateMockComponent('logout')} />
+        <Route exact path="/users" component={generateMockComponent('users')} />
+        <Route exact path="/directory" component={generateMockComponent('directory')} />
+      </Switch>
+    </Router>
+  ));
+  
+  afterEach(cleanup);
 
   describe('Rendering', () => {
     it('Should render the <NavBarWrapper/>', () => expectInDocByTestId("nbw"));
-
     it('Should render the <NavLogo/>', () => expectInDocByTestId("nl"));
-  
     it('Should render the <NavLinkWrapper/>', () => expectInDocByTestId("nlw"));
-  
-    it('Should render a <NavLinkStyled/> for directory', () => expectInDocByTestId("nls-d"));
-  
     it('Should render a <NavLinkStyled/> for users', () => expectInDocByTestId("nls-u"));
-  
     it('Should render a <NavLinkStyled/> for logout', () => expectInDocByTestId("nls-l"));
+    it('Should render a <NavLinkStyled/> for directory', () => expectInDocByTestId("nls-d"));
   });
-
   describe('<NavLink/>', () => {
-    it('Should navigate to "/users" when Users link is clicked', () => {
-      console.log("====== TEST 1 =======");
-      screen.debug();
-      clickByTestId('nls-u');
-      console.log("====== NEW PAGE ======");
-      screen.debug();
-      expectInDocByTestId('users');
-    });
-  
-    it('Should navigate to "/directory" when Directory link is clicked', () => {
-      console.log("====== TEST 2 =======");
-      screen.debug();
-      clickByTestId('nls-d');
-      console.log("====== NEW PAGE ======");
-      screen.debug();
-      expectInDocByTestId('directory');
-    });
-  
-    it('Should navigate to "/logout" when Logout link is clicked', () => {
-      console.log("====== TEST 3 =======");
-      screen.debug();
-      clickByTestId('nls-l');
-      console.log("====== NEW PAGE ======");
-      screen.debug();
-      expectInDocByTestId('logout');
-    });
+    it('Should navigate to "/users" when Users link is clicked', () => clickExpect('nls-u', 'users'));
+    it('Should navigate to "/logout" when Logout link is clicked', () => clickExpect('nls-l', 'logout'));
+    it('Should navigate to "/directory" when Directory link is clicked', () => clickExpect('nls-d', 'directory'));
   });
 });
