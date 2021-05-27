aa83a4d2c0d7fb4b40ba1f1cc1e0e2c350b9c6da
Adam Weaver <parkersalterego92@gmail.com>
2021-05-27 11:38:17 -0700

    fix(dashboardPage): placeholder <h2> added to give visual indication of the component being rendered on the page.

--- src/components/dashboardPage/index.tsx
+++ src/components/dashboardPage/index.tsx
@@ -4,7 +4,7 @@ import { DashboardPageContainer } from './styled';
 export const DashboardPage: FC = () => {
   return (
     <DashboardPageContainer data-testid="dp-c">
-
+      <h2>Dashboard Page</h2>
     </DashboardPageContainer>
   );
 };
