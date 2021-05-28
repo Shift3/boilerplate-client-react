8d5352d8f361bf46e3dc9325e09b748538bb63fa
Adam Weaver <parkersalterego92@gmail.com>
2021-05-28 14:55:09 -0700

    refactor(signupPage/index.spec): refactored to utilize helper methods within utils/test

--- src/components/signupPage/__tests__/index.spec.tsx
+++ src/components/signupPage/__tests__/index.spec.tsx
@@ -1,33 +1,32 @@
-import { render, screen, cleanup } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { SignupPage } from '..';
-import { FC } from 'react';
-
-const { getByTestId } = screen;
-
-const testPage: FC = () => <div data-testid="test" />;
+import { expectInDocByTestId } from 'utils/test';
 
 beforeEach(() => render(
   <Router>
     <Switch>
-      <Route exact path="/" component={SignupPage} />
-      <Route exact path="/auth/signup" component={testPage} />
+      <Route 
+        exact
+        path="/"
+        component={SignupPage} 
+      />
+      <Route 
+        exact 
+        path="/auth/signup" 
+        component={() => <div data-testid="test" />}
+      />
     </Switch>
   </Router>
 ));
 
-afterEach(cleanup);
-
 describe('<SignupPage/>', () => {
-  it('Should render the <SignupPageContainer/>', () => {
-    expect(getByTestId("spc")).toBeInTheDocument();
-  });
+  it('Should render the <SignupPageContainer/>', () => 
+    expectInDocByTestId("spc"));
 
-  it('Should render the <SignupFormContainer/>', () => {
-    expect(getByTestId("sfc")).toBeInTheDocument();
-  });
+  it('Should render the <SignupFormContainer/>', () => 
+    expectInDocByTestId("sfc"));
 
-  it('Should render the <SignupForm/>', () => {
-    expect(getByTestId("sf")).toBeInTheDocument();
-  });
-})
+  it('Should render the <SignupForm/>', () => 
+    expectInDocByTestId("sf"));
+});
