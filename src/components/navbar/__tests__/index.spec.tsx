a42b815f51f1daa441ce9823f233b3caa5d64789
Adam Weaver <parkersalterego92@gmail.com>
2021-05-28 14:52:50 -0700

    refactor(navbar/index.spec): refactored to utilize helper methods within utils/test

--- src/components/navbar/__tests__/index.spec.tsx
+++ src/components/navbar/__tests__/index.spec.tsx
@@ -1,49 +1,49 @@
-import { render, screen, cleanup, act } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { Router, Switch, Route } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { NavBar } from '..';
-import userEvent from '@testing-library/user-event';
-import { FC } from 'react';
-
-const { click } = userEvent;
-const { getByTestId } = screen;
-
-const clickByTestId = (id: string) => act(() => click(getByTestId(id)));
-
-const expectInDocByTestId = (id: string) => expect(getByTestId(id)).toBeInTheDocument();
-
-const clickExpect = (clickId: string, expectId: string) => {
-  clickByTestId(clickId);
-  expectInDocByTestId(expectId);
-};
-
-const generateMockComponent: (id: string) => FC = (id) => () => <div data-testid={id}/>;
+import { expectInDocByTestId, clickExpectByTestId } from '../../../utils/test';
 
 describe('<NavBar/>', () => {
   beforeEach(() => render(
     <Router history={createMemoryHistory({ initialEntries: ['/'] })}>
       <Switch>
         <Route exact path="/" component={NavBar} />
-        <Route exact path="/logout" component={generateMockComponent('logout')} />
-        <Route exact path="/users" component={generateMockComponent('users')} />
-        <Route exact path="/directory" component={generateMockComponent('directory')} />
+        <Route exact path="/logout" component={() => <div data-testid="logout"/>} />
+        <Route exact path="/users" component={() => <div data-testid="users"/>} />
+        <Route exact path="/directory" component={() => <div data-testid="directory"/>} />
       </Switch>
     </Router>
   ));
-  
-  afterEach(cleanup);
 
   describe('Rendering', () => {
-    it('Should render the <NavBarWrapper/>', () => expectInDocByTestId("nbw"));
-    it('Should render the <NavLogo/>', () => expectInDocByTestId("nl"));
-    it('Should render the <NavLinkWrapper/>', () => expectInDocByTestId("nlw"));
-    it('Should render a <NavLinkStyled/> for users', () => expectInDocByTestId("nls-u"));
-    it('Should render a <NavLinkStyled/> for logout', () => expectInDocByTestId("nls-l"));
-    it('Should render a <NavLinkStyled/> for directory', () => expectInDocByTestId("nls-d"));
+    it('Should render the <NavBarWrapper/>', () =>
+      expectInDocByTestId("nbw"));
+
+    it('Should render the <NavLogo/>', () =>
+      expectInDocByTestId("nl"));
+
+    it('Should render the <NavLinkWrapper/>', () =>
+      expectInDocByTestId("nlw"));
+
+    it('Should render a <NavLinkStyled/> for users', () =>
+      expectInDocByTestId("nls-u"));
+
+    it('Should render a <NavLinkStyled/> for logout', () =>
+      expectInDocByTestId("nls-l"));
+
+    it('Should render a <NavLinkStyled/> for directory', () =>
+      expectInDocByTestId("nls-d"));
   });
+
   describe('<NavLink/>', () => {
-    it('Should navigate to "/users" when Users link is clicked', () => clickExpect('nls-u', 'users'));
-    it('Should navigate to "/logout" when Logout link is clicked', () => clickExpect('nls-l', 'logout'));
-    it('Should navigate to "/directory" when Directory link is clicked', () => clickExpect('nls-d', 'directory'));
+    it('Should navigate to "/users" when Users link is clicked', () =>
+      clickExpectByTestId('nls-u', 'users'));
+
+    it('Should navigate to "/logout" when Logout link is clicked', () =>
+      clickExpectByTestId('nls-l', 'logout'));
+
+    it('Should navigate to "/directory" when Directory link is clicked', () =>
+      clickExpectByTestId('nls-d', 'directory'));
   });
 });
