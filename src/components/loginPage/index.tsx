d33bbd0f4d0c25509d1d3cd7bd03d8c55365d99f
Adam Weaver <parkersalterego92@gmail.com>
2021-05-28 14:42:31 -0700

    refactor(loginPage): utilizing CustomButton, simplified rendering by reverting to previous structure

--- src/components/loginPage/index.tsx
+++ src/components/loginPage/index.tsx
@@ -1,51 +1,35 @@
 import { FC, useContext } from 'react';
 import { Link, useHistory } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
+import { CustomButton } from '../button/styled';
 import { LoginForm } from "../loginForm";
 import { Context as AuthContext } from '../../context/auth.context';
 import {
   LoginFormContainer,
   LoginPageContainer,
   LoginFormContainerLeft,
-  LoginFormContainerRight,
-  customButtonStyles
+  LoginFormContainerRight
 } from './styled';
 
 export const LoginPage: FC = () => {
   const { loginUser } = useContext(AuthContext);
-  const history = useHistory();
-
-  const navToSignup = () => history.push('/auth/signup');
-
-  const renderLoginFormContainerLeft = () => (
-    <LoginFormContainerLeft data-testid="lfc-l">
-      <LoginForm onSubmit={ loginUser } />
-      <br />
-      <Link to="/auth/forgot-password">Forgot Password?</Link>
-    </LoginFormContainerLeft>
-  );
-
-  const renderLoginFormContainerRight = () => (
-    <LoginFormContainerRight data-testid="lfc-r">
-      <h2 data-testid="lfc-r-h2">Not Registered Yet?</h2>
-      <p data-testid="lfc-r-p">Registering for your account is quick and easy</p>
-      <Button
-        style={ customButtonStyles }
-        onClick={ navToSignup }
-        disabled={ false }
-        type="button"
-        data-testid="ca-btn"
-      >
-        CREATE ACCOUNT
-      </Button>
-    </LoginFormContainerRight>
-  );
+  const { push } = useHistory();
+  const navigateToSignup = () => push('/auth/signup');
 
   return (
     <LoginPageContainer data-testid="lpc">
       <LoginFormContainer data-testid="lfc">
-        { renderLoginFormContainerLeft() }
-        { renderLoginFormContainerRight() }
+        <LoginFormContainerLeft data-testid="lfc-l">
+          <LoginForm onSubmit={ loginUser } />
+          <br />
+          <Link to="/auth/forgot-password">Forgot Password?</Link>
+        </LoginFormContainerLeft>
+        <LoginFormContainerRight data-testid="lfc-r">
+          <h2 data-testid="lfc-r-h2">Not Registered Yet?</h2>
+          <p data-testid="lfc-r-p">Registering for your account is quick and easy</p>
+          <CustomButton onClick={ navigateToSignup } data-testid="ca-btn">
+            CREATE ACCOUNT
+          </CustomButton>
+        </LoginFormContainerRight>
       </LoginFormContainer>
     </LoginPageContainer>
   );
