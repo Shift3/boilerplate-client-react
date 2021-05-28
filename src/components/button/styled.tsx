6950900b6716a83756728392e7a95cbe20fb7208
Adam Weaver <parkersalterego92@gmail.com>
2021-05-28 14:33:04 -0700

    feat(button): CustomButtton styled component updated to utilize a default theme and accept other themes via a wrapping <provider/>

--- src/components/button/styled.tsx
+++ src/components/button/styled.tsx
@@ -1,13 +1,33 @@
 import styled from 'styled-components';
 import { Button } from 'react-bootstrap';
+import StyleValues from '../../utils/styleValues';
+
+const { accent, accentDisabled } = StyleValues;
 
 export const CustomButton = styled(Button)`
-  background-color: ${(props) => props.backgroundColor};
-  border-color: ${(props) => props.backgroundColor};
-  color: ${(props) => props.color};
-  width: ${(props) => props.width};
+  background-color: ${(props) => props.theme.backgroundColor};
+  border-color: ${(props) => props.theme.borderColor};
+  color: ${(props) => props.theme.color};
+  width: ${(props) => props.theme.width};
   &:hover {
-    background-color: ${(props) => props.backgroundColor};
-    border-color: ${(props) => props.backgroundColor};
+    background-color: ${(props) => props.theme.hoverBackgroundColor};
+    border-color: ${(props) => props.theme.hoverBorderColor};
+  }
+  &:disabled {
+    background-color: ${(props) => props.theme.disabledBackgroundColor};
+    border-color: ${(props) => props.theme.disabledBorderColor};
   }
 `;
+
+CustomButton.defaultProps = {
+  theme: {
+    backgroundColor: accent,
+    borderColor: accent,
+    color: '#fff',
+    disabledBackgroundColor: accentDisabled,
+    disabledBorderColor: accent,
+    hoverBackgroundColor: accent,
+    hoverBorderColor: accent,
+    width: '90%'
+  }
+};
