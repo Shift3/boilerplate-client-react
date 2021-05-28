c4aa4a3e865bf50241ecbd594a6aae9e7c50cc16
Adam Weaver <parkersalterego92@gmail.com>
2021-05-28 14:52:20 -0700

    refactor(loginPage/index.spec): refactored to utilize helper methods within utils/test

--- src/components/loginPage/__tests__/index.spec.tsx
+++ src/components/loginPage/__tests__/index.spec.tsx
@@ -1,71 +1,58 @@
-import { act, render, screen, cleanup } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { LoginPage } from '..';
-import userEvent from '@testing-library/user-event';
-import { FC } from 'react';
+import { expectInDocByTestId, expectInnerHTMLByTestId, clickNavigateByTestId } from '../../../utils/test';
 
-const { click } = userEvent;
-const { getByTestId } = screen;
-
-const testPage: FC = () => <div data-testid="test" />;
-
-beforeEach(async () => render(
+const renderInitialTestDOM = () => render(
   <Router>
     <Switch>
       <Route exact path="/" component={LoginPage} />
-      <Route exact path="/auth/signup" component={testPage} />
     </Switch>
   </Router>
-));
+);
 
-afterEach(cleanup);
+const renderNavigationTestDOM = () => render(
+  <Router>
+    <Switch>
+      <Route exact path="/" component={LoginPage} />
+      <Route exact path="/auth/signup" component={() => <div/>} />
+    </Switch>
+  </Router>
+);
 
 describe('<LoginPage/>', () => {
+  describe('Rendering', () => {
+    beforeEach(renderInitialTestDOM);
 
-  it('Should render the <LoginPageContainer/>', () => {
-    expect(getByTestId("lpc")).toBeInTheDocument();
+    it('Should render the <LoginPageContainer/>', () =>
+      expectInDocByTestId("lpc"));
+
+    it('Should render the <LoginFormContainer/>', () =>
+      expectInDocByTestId("lfc"));
+
+    it('Should render the <LoginFormContainerRight/>', () =>
+      expectInDocByTestId("lfc-r"));
+
+    it('Should render the <LoginFormContainerLeft/>', () =>
+      expectInDocByTestId("lfc-l"));
+
+    it('Should render the <LoginForm/>', () =>
+      expectInDocByTestId("lf"));
+
+    it('Should render an <h2/> with innerHTML equal to "Not Registered Yet?', () =>
+      expectInnerHTMLByTestId("lfc-r-h2", "Not Registered Yet?"));
+
+    it('Should render a <p/> with innerHTML equal to "Registering for your account is quick and easy', () =>
+      expectInnerHTMLByTestId("lfc-r-p", "Registering for your account is quick and easy"));
+
+    it('Should render a <button/> with innerHTML equal to "CREATE ACCOUNT"', () =>
+      expectInnerHTMLByTestId("ca-btn", "CREATE ACCOUNT")); 
   });
+  
+  describe('navigation', () => {
+    beforeEach(renderNavigationTestDOM);
 
-  it('Should render the <LoginFormContainer/>', () => {
-    expect(getByTestId("lfc")).toBeInTheDocument();
+    it('Should navigate to "/auth/signup" when the "CREATE ACCOUNT" button is clicked', () =>
+      clickNavigateByTestId("ca-btn", "/auth/signup"));
   });
-
-  it('Should render the <LoginFormContainerRight/>', () => {
-    expect(getByTestId("lfc-r")).toBeInTheDocument();
-  });
-
-  it('Should render the <LoginFormContainerLeft/>', () => {
-    expect(getByTestId("lfc-l")).toBeInTheDocument();
-  });
-
-  it('Should render the <LoginForm/>', () => {
-    expect(getByTestId("lf")).toBeInTheDocument();
-  });
-
-  it('Should render an <h2/> with innerHTML equal to "Not Registered Yet?', () => {
-    const h2 = getByTestId("lfc-r-h2");
-
-    expect(h2).toBeInTheDocument();
-    expect(h2.innerHTML).toEqual("Not Registered Yet?");
-  });
-
-  it('Should render a <p/> with innerHTML equal to "Registering for your account is quick and easy', () => {
-    const p = getByTestId("lfc-r-p");
-
-    expect(p).toBeInTheDocument();
-    expect(p.innerHTML).toEqual("Registering for your account is quick and easy");
-  });
-
-  it('Should render a <button/> with innerHTML equal to "CREATE ACCOUNT"', () => {
-    const button = getByTestId("ca-btn");
-    
-    expect(button).toBeInTheDocument();
-    expect(button.innerHTML).toEqual("CREATE ACCOUNT");
-  });
-
-  it('Should navigate to "/auth/signup" when the "CREATE ACCOUNT" button is clicked', () => {
-    act(() => click(getByTestId("ca-btn")));
-
-    expect(getByTestId('test')).toBeInTheDocument();
-  })
 });
