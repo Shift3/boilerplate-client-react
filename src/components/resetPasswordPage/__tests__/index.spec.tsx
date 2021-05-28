4498d4182a84fe4fad0b6dd34ec977f547a96f04
Adam Weaver <parkersalterego92@gmail.com>
2021-05-28 14:53:19 -0700

    refactor(resetPasswordPage/index.spec): refactored to utilize helper methods within utils/test

--- src/components/resetPasswordPage/__tests__/index.spec.tsx
+++ src/components/resetPasswordPage/__tests__/index.spec.tsx
@@ -1,35 +1,24 @@
-import { render, screen, cleanup } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ResetPasswordPage } from '..';
-import { FC } from 'react';
-
-const { getByTestId } = screen;
-
-const testPage: FC = () => <div data-testid="test" />; 
-
-
+import { expectInDocByTestId } from '../../../utils/test';
 
 describe('<SignupPage/>', () => {
   beforeEach(() => render(
     <Router>
       <Switch>
         <Route exact path="/" component={ResetPasswordPage} />
-        <Route exact path="/auth/signup" component={testPage} />
+        <Route exact path="/auth/signup" component={() => <div data-testid="test" />} />
       </Switch>
     </Router>
   ));
-  
-  afterEach(cleanup);
 
-  it('Should render the <ResetPasswordPageContainer/>', () => {
-    expect(getByTestId("rppc")).toBeInTheDocument();
-  });
+  it('Should render the <ResetPasswordPageContainer/>', () => 
+    expectInDocByTestId('rppc'));
 
-  it('Should render the <ResetPasswordFormContainer/>', () => {
-    expect(getByTestId("rpfc")).toBeInTheDocument();
-  });
+  it('Should render the <ResetPasswordFormContainer/>', () =>
+    expectInDocByTestId('rpfc'));
 
-  it('Should render the <SetPasswordForm/>', () => {
-    expect(getByTestId("spf")).toBeInTheDocument();
-  });
+  it('Should render the <SetPasswordForm/>', () =>
+    expectInDocByTestId('spf'));
 })
