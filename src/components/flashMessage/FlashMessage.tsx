63bf55cdc960a4b20b5be9f1be708f22e936a118
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-06-02 16:32:03 -0700

    refactor(components/flashMessage): use redux instead of context

--- src/components/flashMessage/FlashMessage.tsx
+++ src/components/flashMessage/FlashMessage.tsx
@@ -1,16 +1,25 @@
-import { FC, useContext } from "react";
+// React imports
+import { FC } from 'react';
+
+// Third party imports
+import { useDispatch, useSelector } from 'react-redux';
 import Alert from 'react-bootstrap/Alert';
-import { Context as FlashMessageContext } from '../../context/flashMessage.context';
+
+// App imports
+import { selectFlashMessage } from 'redux/flashMessage/selectors';
+import { clearMessage } from 'redux/flashMessage/slice';
 
 export const FlashMessage: FC = () => {
-  const { state: { flashMessage } } = useContext(FlashMessageContext);
+  const flashMessage = useSelector(selectFlashMessage);
+  const dispatch = useDispatch();
 
   return (
     <>
-      {
-        flashMessage &&
-            <Alert variant={ flashMessage.variant }>{ flashMessage.message }</Alert>
-      }
+      {flashMessage && (
+        <Alert variant={flashMessage.variant} dismissible onClose={() => dispatch(clearMessage())}>
+          {flashMessage.message}
+        </Alert>
+      )}
     </>
   );
 };
