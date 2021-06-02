f69f7b74c492e81f139e40b2ba87ae5f4df14e8e
Adam Weaver <parkersalterego92@gmail.com>
2021-06-02 13:24:25 -0700

    refactor(signup): updated data-testids to be explicit rather than abbreviated

--- src/components/signupPage/index.tsx
+++ src/components/signupPage/index.tsx
@@ -7,8 +7,8 @@ import { SignupPageContainer, SignupFormContainer } from './styled';
 const onSubmit = (formData: ISignupFormData) => {};
 
 export const SignupPage: FC = () => (
-  <SignupPageContainer data-testid="spc">
-    <SignupFormContainer data-testid="sfc">
+  <SignupPageContainer data-testid="signupPageContainer">
+    <SignupFormContainer data-testid="signupFormContainer">
       <SignupForm  onSubmit={onSubmit}/>
     </SignupFormContainer>
   </SignupPageContainer>
