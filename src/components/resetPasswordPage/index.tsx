1c5eee81472b724001fa10de7a66caa19bc89dbc
Elizabeth Eidelson <fluturecode@gmail.com>
2021-06-05 10:29:55 -0400

    feat(restPassword):removed tests

--- src/components/resetPasswordPage/index.tsx
+++ src/components/resetPasswordPage/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable lines-around-comment */
 import { FC } from 'react';
 import { useHistory } from 'react-router-dom';
 import { ResetPasswordForm } from '../resetPasswordForm/index';
