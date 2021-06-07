0ba72b0f252feab25eda3c5631fd53ebf1ff955f
Elizabeth Eidelson <fluturecode@gmail.com>
2021-06-07 13:29:53 -0400

    feat(loginPage):fixed erros

--- src/components/loginPage/__tests__/styled.spec.tsx
+++ src/components/loginPage/__tests__/styled.spec.tsx
@@ -1,10 +1,8 @@
-import { LoginPageContainer, LoginFormContainer, LoginFormContainerLeft, LoginFormContainerRight } from '../styled';
+/* eslint-disable no-undef */
+import { Wrapper } from '../styled';
 import { expectToMatchSnapshot } from '../../../utils/test';
 import 'jest-styled-components';
 
 describe('Login Page Styled Components', () => {
-  it('Should match the stored <LoginPageContainer /> snapshot', () => expectToMatchSnapshot(<LoginPageContainer />));
-  it('Should match the stored <LoginFormContainer /> snapshot', () => expectToMatchSnapshot(<LoginFormContainer />));
-  it('Should match the stored <LoginFormContainerLeft /> snapshot', () => expectToMatchSnapshot(<LoginFormContainerLeft />));
-  it('Should match the stored <LoginFormContainerRight /> snapshot', () => expectToMatchSnapshot(<LoginFormContainerRight />));
+  it('Should match the stored <Wrapper/> snapshot', () => expectToMatchSnapshot(<Wrapper />));
 });
