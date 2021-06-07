5dd5e02f242f5ac14b851264fbfc6e8fae4ef98e
Elizabeth Eidelson <fluturecode@gmail.com>
2021-06-07 12:06:01 -0400

    feat(loginForm):udpated navbar/index.tsx

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,14 +1,15 @@
+/* eslint-disable jsx-quotes */
 import { FC } from 'react';
 import { NavBarWrapper, NavLinkStyled, NavLogo, NavLinkWrapper } from './styled';
 import logo from '../../assets/img/logo.png';
 
 export const NavBar: FC = () => (
   <NavBarWrapper>
-    <NavLogo src={logo} alt="Bitwise Technology Consulting" />
+    <NavLogo src={logo} alt='Bitwise Technology Consulting' />
     <NavLinkWrapper>
-      <NavLinkStyled to="/directory">Directory</NavLinkStyled>
-      <NavLinkStyled to="/users">Users</NavLinkStyled>
-      <NavLinkStyled to="/logout">Log Out</NavLinkStyled>
+      <NavLinkStyled to='/directory'>Directory</NavLinkStyled>
+      <NavLinkStyled to='/users'>Users</NavLinkStyled>
+      <NavLinkStyled to='/'>Log Out</NavLinkStyled>
     </NavLinkWrapper>
   </NavBarWrapper>
 );
