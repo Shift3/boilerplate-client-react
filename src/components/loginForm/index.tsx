79e19c11d28983227b57e4cafc7cb1b862ccdf34
Elizabeth Eidelson <fluturecode@gmail.com>
2021-06-08 16:14:12 -0400

    feat(UIforms):updated logInForm/index.tsx

--- src/components/loginForm/index.tsx
+++ src/components/loginForm/index.tsx
@@ -28,26 +28,14 @@ export const LogInForm: LogInFormType = ({ onSubmit, onCancel }) => {
             <Form.Label htmlFor='email'>Email</Form.Label>
           </FieldTitle>
           <Form.Control id='email' type='email' {...register('email')} />
-          <Error>
-            {errors.email?.message && (
-              <span role='alert' className='text-danger'>
-                {errors.email?.message}
-              </span>
-            )}
-          </Error>
+          <Error>{errors.email?.message && <span role='alert'>{errors.email?.message}</span>}</Error>
         </Form.Group>
         <Form.Group>
           <FieldTitle>
             <Form.Label htmlFor='password'>Password</Form.Label>
           </FieldTitle>
           <Form.Control id='password' type='password' {...register('password')} />
-          <Error>
-            {errors.password?.message && (
-              <span role='alert' className='text-danger'>
-                {errors.password?.message}
-              </span>
-            )}
-          </Error>
+          <Error>{errors.password?.message && <span role='alert'>{errors.password?.message}</span>}</Error>
         </Form.Group>
         <ButtonWrapper>
           <CancelButton data-testid='Cancel Button' onClick={onCancel}>
