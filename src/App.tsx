14aeb6199862b8b5c8cb7bbaa5180820c3d98d90
Elizabeth Eidelson <fluturecode@gmail.com>
2021-06-09 14:31:26 -0400

    feat(UIforms):updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,18 @@ import { FC } from 'react';
 import { ErrorBoundary } from '@sentry/react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import { LogInPage } from 'components/logInPage';
+import { SignUpPage } from 'components/signUpPage';
+import { ResetPasswordPage } from 'components/resetPasswordPage';
+import { DashboardPage } from './components/dashboardPage';
+import { FlashMessage } from './components/flashMessage';
+import { HolyGrailLayout } from './components/holyGrailLayout';
+import { NavBar } from './components/navbar';
 import { GlobalStyle } from './GlobalStyle';
 import AppTheme from './utils/styleValues';
 
-// Context
 import { Provider as AuthProvider } from './context/auth.context';
 import { Provider as FlashMessageProvider } from './context/flashMessage.context';
-
-// Components
-
-import { DashboardPage } from './components/dashboardPage';
-import { LogInPage } from './components/logInPage';
-import { SignUpPage } from './components/signUpPage';
-import { ResetPasswordPage } from './components/resetPasswordPage';
-import { FlashMessage } from './components/flashMessage';
-
-import { HolyGrailLayout } from './components/holyGrailLayout';
-import { NavBar } from './components/navbar';
 
 export const App: FC = () => (
   <ErrorBoundary>
