b208d8ddc0d3c039b308ddec39ce29d1265652e6
Elizabeth Eidelson <fluturecode@gmail.com>
2021-06-09 12:55:56 -0400

    feat(UIforms):fixed merge conflicts

--- src/components/signupForm/styled.tsx
+++ src/components/signupForm/styled.tsx
@@ -1,16 +1,26 @@
 import styled from 'styled-components';
+import { Button } from 'react-bootstrap';
 
 export const Wrapper = styled.form`
   background-color: ${(props) => props.theme.primary};
   padding: 40px;
+  border-radius: 5px;
+  max-width: 40%;
 `;
 
 export const Title = styled.div`
   color: ${(props) => props.theme.cardHeader};
   font-size: 2em;
   font-style: bold;
+  padding-bottom: 10px;
 `;
 
 export const FieldTitle = styled.div`
   color: white;
+`;
+
+export const StyledButton = styled(Button)`
+  color: white;
+  background: red;
+  border: none;
 `;
--- src/components/signupPage/index.tsx
+++ src/components/signupPage/index.tsx
@@ -1,15 +1,13 @@
 import { FC } from 'react';
 import { SignupForm } from '../signupForm';
 import { ISignupFormData } from '../signupForm/types';
-import { SignupPageContainer, SignupFormContainer } from './styled';
+import { Wrapper } from './styled';
 
 //  eslint-disable-next-line
 const onSubmit = (formData: ISignupFormData) => {};
 
 export const SignupPage: FC = () => (
-  <SignupPageContainer data-testid="signupPageContainer">
-    <SignupFormContainer data-testid="signupFormContainer">
-      <SignupForm  onSubmit={onSubmit}/>
-    </SignupFormContainer>
-  </SignupPageContainer>
+  <Wrapper>
+    <SignupForm onSubmit={onSubmit} />
+  </Wrapper>
 );
