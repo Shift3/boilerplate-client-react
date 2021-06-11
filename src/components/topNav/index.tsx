55b4982d409fc81c1c0df393e332b9064cabd6a1
Adam Weaver <parkersalterego92@gmail.com>
2021-06-10 17:42:32 -0700

    feat(topNav): styles updated (EOD Push)

--- src/components/topNav/index.tsx
+++ src/components/topNav/index.tsx
@@ -1,18 +1,26 @@
 import { FC } from 'react';
 import { ThemeProvider } from 'styled-components';
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { faUser, faStethoscope } from '@fortawesome/free-solid-svg-icons';
 import { DropDownItemType } from './types';
-import { CustomButton, StandardButtonTheme } from '../button/styled';
-import { NavLinkStyled, NavLogo } from '../navbar/styled';
+import { CustomButton } from '../button/styled';
+import { NavLinkStyled } from '../sideNav/styled';
 import logo from '../../assets/img/logo.png';
 import placeholder_portrait from '../../assets/img/portrait_placeholder.png';
 import {
   NavWrapper,
-  DropDownContentLeft,
-  DropDownContentRight,
+  DropDownContainerLeft,
+  DropDownContainerRight,
   VerticalLine,
   NavContainerLeft,
-  NavContainerRight
+  NavContainerRight,
+  CustomNavDropdown,
+  ProfilePhoto,
+  UserIcon,
+  StethoscopeIcon,
+  TopNavLogo,
+  DropDownButton,
+  DirectoryLink
 } from './styled';
 
 // @TODO - utilize redux to get user data and logged in state in order to display variations of the navbar
@@ -37,38 +45,40 @@ const NavDropDownItem: DropDownItemType = ({ linkText, pathname, testid }) => (
 export const TopNav: FC = () => (
   <NavWrapper data-testid="navWrapper">
     <NavContainerLeft>
-      <NavLogo src={ logo } alt="Bitwise Technology Consulting" />
-      <button type="button" className="navLink">
-        <span className="fa fa-stethoscope" />
-      </button>
+      <TopNavLogo src={ logo } alt="Bitwise Technology Consulting" />
+      <DirectoryLink to="/content/agent-list">
+        <StethoscopeIcon icon={ faStethoscope } />
+        <span>Directory</span>
+      </DirectoryLink>
     </NavContainerLeft>
     <NavContainerRight>
       {
         isLoggedIn && (
-          <CustomButton>
-            <span className="fa fa-user"/>
-            Hi { mockUser.firstName }
-            <NavDropdown title="" id="test" data-testid="navDropdown" >
-              <DropDownContentLeft>
-                <img src={ placeholder_portrait } alt="Placeholder Portrait" />
-                { fullUserName() }
-              </DropDownContentLeft>
-              <VerticalLine />
-              <DropDownContentRight>
-                <NavDropDownItem linkText="Profile" pathname="/user/profile" testid="profile" />
-                <NavDropDownItem linkText="Change Password" pathname="/user/change-password" testid="changePassword" />
-              </DropDownContentRight>
-            </NavDropdown>
-          </CustomButton>
+            <DropDownButton>
+              <UserIcon icon={ faUser }/>
+              <CustomNavDropdown
+                title={ `Hi ${ mockUser.firstName }` }
+                id="topNavDropdown"
+                data-testid="navDropdown"
+              >
+                <DropDownContainerLeft>
+                  <ProfilePhoto src={ placeholder_portrait } alt="Placeholder Portrait" />
+                  { fullUserName() }
+                </DropDownContainerLeft>
+                <VerticalLine />
+                <DropDownContainerRight>
+                  <NavDropDownItem linkText="Profile" pathname="/user/profile" testid="profile" />
+                  <NavDropDownItem linkText="Change Password" pathname="/user/change-password" testid="changePassword" />
+                </DropDownContainerRight>
+              </CustomNavDropdown>
+            </DropDownButton>
         )
       }
       {
         !isLoggedIn && (
-          <ThemeProvider theme={StandardButtonTheme}>
-            <CustomButton data-testid="loginOrCreateAccountLink">
-              LOGIN/CREATE ACCOUNT
-            </CustomButton>
-          </ThemeProvider>
+          <CustomButton data-testid="loginOrCreateAccountLink">
+            LOGIN/CREATE ACCOUNT
+          </CustomButton>
         )
       }
     </NavContainerRight>
--- src/components/topNav/styled.tsx
+++ src/components/topNav/styled.tsx
@@ -1,17 +1,93 @@
 import styled from 'styled-components';
 import Nav from 'react-bootstrap/Nav';
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Button from 'react-bootstrap/Button';
+import { NavLink } from 'react-router-dom';
 
 export const NavWrapper = styled(Nav)`
+    padding: 8px 16px;
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-    align-items: center;
     height: ${(props) => props.theme.topNavHeight}
 `;
 
-export const DropDownContentLeft = styled.div``;
+export const TopNavLogo = styled.img`
+    height: ${(props) => `calc(${props.theme.topNavHeight} - 20px)`};
+`;
 
-export const DropDownContentRight = styled.div``;
+export const DirectoryLink = styled(NavLink)`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+export const DropDownButton = styled(Button)`
+    background-color: rgba(0, 0, 0, 0);
+    border-color: rgba(0, 0, 0, 0);
+
+    &:active {
+        background-color: rgba(0, 0, 0, 0) !important;
+        border-color: #175f6e !important;
+    }
+
+    &:focus {
+        background-color: rgba(0, 0, 0, 0);
+        border-color: rgba(0, 0, 0, 0);
+    }
+
+    &:hover {
+        background-color: rgba(0, 0, 0, 0);
+        border-color: rgba(0, 0, 0, 0);
+    }
+    
+
+`;
+
+export const CustomNavDropdown = styled(NavDropdown)`
+    & a {
+        color: #175f6e
+    }
+
+    & .dropdown-menu {
+        display: flex;
+        align-items: center;
+
+        & a {
+            color: #000;
+        }
+
+        & .dropdown-item > a:hover {
+            background-color: rgba(0, 0, 0, 0);
+            text-decoration: none;
+        }
+    }
+`;
+
+export const DropDownContainerLeft = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    padding: 10px;
+    text-align: center;
+`;
+
+export const ProfilePhoto = styled.img`
+    border-radius: 50%;
+    max-height: 50px;
+    max-width: 50px;
+    width: auto;
+    height: auto;
+`;
+
+export const DropDownContainerRight = styled.div`
+    display: flex; 
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
 
 export const VerticalLine = styled.span`
     display: inline-block;
@@ -20,6 +96,21 @@ export const VerticalLine = styled.span`
     height: 125px;
 `;
 
-export const NavContainerLeft = styled.div``;
+export const NavContainerLeft = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`;
 
-export const NavContainerRight = styled.div``;
+export const NavContainerRight = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+export const UserIcon = styled(FontAwesomeIcon)`
+    color: #175f6e
+`;
+
+export const StethoscopeIcon = styled(FontAwesomeIcon)`
+    color: #175f6e
+`;
