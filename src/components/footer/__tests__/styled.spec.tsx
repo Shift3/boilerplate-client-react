fa5108fced14ae45a4527e10e48af2cb8f5ee42f
Adam Weaver <parkersalterego92@gmail.com>
2021-06-11 11:13:45 -0700

    chore(footer/__tests__/styled.spec): added missing jest-styled-components import

--- src/components/footer/__tests__/styled.spec.tsx
+++ src/components/footer/__tests__/styled.spec.tsx
@@ -1,6 +1,8 @@
 import renderer from 'react-test-renderer';
 import { Wrapper } from '../styled';
 
+import 'jest-styled-components';
+
 describe('<Wrapper />', () => {
   it('should match the stored snapshot', () => {
     expect(renderer.create(<Wrapper />).toJSON()).toMatchSnapshot();
