3c0f774f8a3b05b26913f748531cb903bfcbd704
Adam Weaver <parkersalterego92@gmail.com>
2021-06-15 15:35:15 -0700

    feat(topNav): updated to utilize useState and useEffect for future loginData coming from redux, updated styled component names changed and temporary isLoggedInUser variable added for testing purposes

--- src/components/topNav/index.tsx
+++ src/components/topNav/index.tsx
@@ -1,14 +1,13 @@
-import { FC, useState } from 'react';
+import { FC, useState, useEffect } from 'react';
 import { faUser, faStethoscope } from '@fortawesome/free-solid-svg-icons';
 import { DropDownItemType } from './types';
-import { CustomButton } from '../button/styled';
 import { useHistory } from 'react-router-dom';
 import logo from '../../assets/img/logo.png';
 import placeholder_portrait from '../../assets/img/portrait_placeholder.png';
 import {
-  Navbar,
-  DropDownContainerLeft,
-  DropDownContainerRight,
+  TopNavbar,
+  DropdownContainerLeft,
+  DropdownContainerRight,
   VerticalLine,
   NavContainerLeft,
   NavContainerRight,
@@ -16,27 +15,37 @@ import {
   ProfilePhoto,
   NavIcon,
   TopNavLogo,
-  DropDownButton,
+  DropdownButton,
   DirectoryLink,
-  NavDropdownItem
+  NavDropdownItem,
+  LoginCreateAccountButton
 } from './styled';
 
 export const TopNav: FC = () => {
+  const isLoggedInUser = true;
   // eslint-disable-next-line
-  const [ { firstName, lastName, isLoggedIn }, setUserData ] = useState({
-    firstName: "Testy",
-    lastName: "Testerson",
-    isLoggedIn: true,
+  const [ userData, setUserData ] = useState({
+    firstName: "",
+    lastName: "",
+    profile_picture: ""
   });
 
   // @TODO - utilize redux to get user data and logged in state in order to display variations of the navbar
   // @TODO - utilize redux to get users signed in status
   // @TODO - useEffect hook to setUserData
 
+  useEffect(() => {
+    setUserData({
+      firstName: "Testy",
+      lastName: "Testerson",
+      profile_picture: "../../assets/img/profile.png"
+    });
+  }, []);
+
   const history = useHistory();
 
   const DropdownItem: DropDownItemType = ({ linkText, testid }) => (
-    <NavDropdownItem data-testid={ `${testid}DropDownItem` } eventKey={testid}>
+    <NavDropdownItem data-testid={ `${testid}DropdownItem` } eventKey={testid}>
       { linkText }
     </NavDropdownItem>
   );
@@ -48,7 +57,7 @@ export const TopNav: FC = () => {
         break;
 
       case "changePassword":
-        history.push("/");
+        history.push("/auth/change-password");
         break;
 
       case "signOut":
@@ -63,45 +72,53 @@ export const TopNav: FC = () => {
   };
 
   return (
-    <Navbar data-testid="navWrapper" onSelect={onDropdownItemSelect} >
-      <NavContainerLeft>
-        <TopNavLogo src={ logo } alt="Bitwise Technology Consulting" />
-        <DirectoryLink to="/content/agent-list">
+    <TopNavbar data-testid="topNavbar" onSelect={onDropdownItemSelect} >
+      <NavContainerLeft data-testid="navContainerLeft">
+        <TopNavLogo data-testid="navLogo" src={ logo } alt="Bitwise Technology Consulting" />
+        <DirectoryLink data-testid="directoryLink" to="/content/agent-list">
           <NavIcon icon={ faStethoscope } />
-          <span>Directory</span>
+          <span data-testid="directoryLinkText">Directory</span>
         </DirectoryLink>
       </NavContainerLeft>
-      <NavContainerRight>
+      <NavContainerRight data-testid="navContainerRight">
         {
-          isLoggedIn ?
-            (
-              <DropDownButton>
-                <NavIcon icon={ faUser }/>
-                <Dropdown
-                  title={ `Hi ${ firstName }` }
-                  id="topNavDropdown"
-                  data-testid="navDropdown"
-                >
-                  <DropDownContainerLeft>
-                    <ProfilePhoto src={ placeholder_portrait } alt="Placeholder Portrait" />
-                    { `${firstName} ${lastName}` }
-                  </DropDownContainerLeft>
-                  <VerticalLine />
-                  <DropDownContainerRight>
-                    <DropdownItem linkText="Profile" testid="profile" />
-                    <DropdownItem linkText="Change Password" testid="changePassword" />
-                    <DropdownItem linkText="Sign Out" testid="signOut" />
-                  </DropDownContainerRight>
-                </Dropdown>
-              </DropDownButton>
-            )
-            : (
-              <CustomButton data-testid="loginOrCreateAccountLink">
-                LOGIN/CREATE ACCOUNT
-              </CustomButton>
-            )
+          isLoggedInUser && (
+            <DropdownButton data-testid="dropdownButton">
+              <NavIcon icon={ faUser } />
+              <Dropdown
+                title={ `Hi ${ userData?.firstName }` }
+                id="topNavDropdown"
+                data-testid="navDropdown"
+              >
+                <DropdownContainerLeft data-testid="dropdownContainerLeft">
+                  <ProfilePhoto
+                    data-testid="profilePhoto"
+                    src={ userData?.profile_picture ?? placeholder_portrait }
+                    alt="Placeholder Portrait"
+                  />
+                  { `${ userData?.firstName } ${ userData?.lastName }` }
+                </DropdownContainerLeft>
+                <VerticalLine data-testid="verticalLine"/>
+                <DropdownContainerRight data-testid="dropdownContainerRight">
+                  <DropdownItem linkText="Profile" testid="profile" />
+                  <DropdownItem linkText="Change Password" testid="changePassword" />
+                  <DropdownItem linkText="Sign Out" testid="signOut" />
+                </DropdownContainerRight>
+              </Dropdown>
+            </DropdownButton>
+          )
+        }
+        {
+          !isLoggedInUser && (
+            <LoginCreateAccountButton
+              to="/auth/login"
+              data-testid="loginCreateAccountLink"
+            >
+              LOGIN/CREATE ACCOUNT
+            </LoginCreateAccountButton>
+          )
         }
       </NavContainerRight>
-    </Navbar>
+    </TopNavbar>
   );
 };
