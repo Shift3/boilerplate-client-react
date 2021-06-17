528b75829000fcb33817449a471c203287c8e599
flutureCode <lizeidelson@FlutureCode.local>
2021-06-16 22:03:11 -0400

    feat(UIforms):fixed failing validation tests

--- src/components/resetPasswordForm/schema.ts
+++ src/components/resetPasswordForm/schema.ts
@@ -1,23 +1,12 @@
 import * as yup from 'yup';
 import { Constants } from '../../utils/constants';
 
-export enum errorMessages {
-  FIELD_REQUIRED = 'Field is required.',
-  PASSWORD_LENGTH = 'Password must be at least 8 characters.',
-  PASSWORD_LOWERCASE = 'Password must contain at least one lowercase letter',
-  PASSWORD_UPPERCASE = 'Password must contain at least one uppercase letter',
-  PASSWORD_SPECIAL_CHARACTER = 'Password must contain at least one special character',
-  PASSWORD_NUMBER = 'Password must contain at least one number',
-  PASSWORD_MUST_MATCH = 'Passwords must match.',
-}
-
-const minPassLength = 8;
+const { errorMessages } = Constants;
 
 export const ResetPasswordFormSchema = yup.object().shape({
   newPassword: yup
     .string()
-    .required(errorMessages.FIELD_REQUIRED)
-    .min(minPassLength, errorMessages.PASSWORD_LENGTH)
+    .required(errorMessages.NEW_PASSWORD_REQUIRED)
     .matches(Constants.patterns.LOWERCASE_REGEX, errorMessages.PASSWORD_LOWERCASE)
     .matches(Constants.patterns.UPPERCASE_REGEX, errorMessages.PASSWORD_UPPERCASE)
     .matches(Constants.patterns.SYMBOL_REGEX, errorMessages.PASSWORD_SPECIAL_CHARACTER)
@@ -25,6 +14,6 @@ export const ResetPasswordFormSchema = yup.object().shape({
 
   confirmPassword: yup
     .string()
-    .required(errorMessages.FIELD_REQUIRED)
+    .required(errorMessages.CONFIRM_PASSWORD_REQUIRED)
     .oneOf([yup.ref('newPassword')], errorMessages.PASSWORD_MUST_MATCH),
 });
