9b66ffa6a983acd7ccac6a5c8a9d7d7ad4851f16
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-06-17 09:14:01 -0700

    feat(environment): add additional variables to define app environment more fine grained

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,13 +1,17 @@
 import { name, version } from '../../package.json';
 
 export interface IEnvironment {
+  apiRoute: string;
+  environment: string;
+  isProduction: boolean;
   name: string;
   version: string;
-  apiRoute: string;
 }
 
 export const environment: IEnvironment = {
+  apiRoute: process.env.REACT_APP_API_ROUTE ?? '',
+  environment: process.env.REACT_APP_ENV ?? '',
+  isProduction: process.env.NODE_ENV === 'production',
   name,
   version,
-  apiRoute: process.env.REACT_APP_API_ROUTE ?? '',
 };
