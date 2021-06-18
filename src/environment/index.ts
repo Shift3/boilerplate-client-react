f3b3b45a06015d29748d615d643f908a4952010c
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-06-17 17:36:54 -0700

    refactor(environment): switch back to choosing environement object at runtime

--- src/environment/index.ts
+++ src/environment/index.ts
@@ -1,1 +1,16 @@
-export * from './environment';
+import * as developmentEnvironmentModule from './environment.dev';
+import * as productionEnvironmentModule from './environment.prod';
+import * as stagingEnvironmentModule from './environment.staging';
+import { EnvironmentConfiguration, IEnvironment } from './types';
+
+let environmentModule;
+
+if (process.env.NODE_ENV === 'production' && process.env.REACT_APP_CONFIGURATION === EnvironmentConfiguration.Staging) {
+  environmentModule = stagingEnvironmentModule;
+} else if (process.env.NODE_ENV === 'production') {
+  environmentModule = productionEnvironmentModule;
+} else {
+  environmentModule = developmentEnvironmentModule;
+}
+
+export const environment: IEnvironment = Object.freeze(environmentModule.environment);
---
+++ src/environment/types.ts
@@ -0,0 +1,13 @@
+export enum EnvironmentConfiguration {
+  Development = 'development',
+  Staging = 'staging',
+  Production = 'production',
+}
+
+export interface IEnvironment {
+  apiRoute: string;
+  environment: EnvironmentConfiguration;
+  isProduction: boolean;
+  name: string;
+  version: string;
+}
