8ae3a5cb39eab1ee988bd23a4381e01681fe1978
Adam Weaver <parkersalterego92@gmail.com>
2021-06-22 17:25:18 -0700

    chore(app.tsx): added navPosition to HolyGrailLayout component instance and added labels to imports as it was monolithic

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,25 @@ import { FC } from 'react';
 import { ErrorBoundary } from '@sentry/react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import { Constants } from 'utils/constants';
+
+// Components
 import { LogInPage } from 'components/loginPage';
 import { SignUpPage } from 'components/signupPage';
 import { ChangePasswordPage } from 'components/changePasswordPage';
 import { DashboardPage } from './components/dashboardPage';
 import { FlashMessage } from './components/flashMessage';
 import { HolyGrailLayout } from './components/holyGrailLayout';
-import { NavBar } from './components/navbar';
-import { GlobalStyle } from './GlobalStyle';
-import AppTheme from './utils/styleValues';
-import { Provider as AuthProvider } from './context/auth.context';
-import { Provider as FlashMessageProvider } from './context/flashMessage.context';
 import { ResetPasswordPage } from 'components/resetPasswordPage';
 import { ForgotPasswordPage } from 'components/forgotPasswordPage';
+
+// Styling
+import { GlobalStyle } from './GlobalStyle';
+import AppTheme from './utils/styleValues';
+
+// Context
+import { Provider as AuthProvider } from './context/auth.context';
+import { Provider as FlashMessageProvider } from './context/flashMessage.context';
 
 export const App: FC = () => (
   <ErrorBoundary>
@@ -24,7 +30,7 @@ export const App: FC = () => (
           <FlashMessage />
           <Router>
             <Switch>
-              <HolyGrailLayout leftSidebar={<NavBar />}>
+              <HolyGrailLayout navPosition={Constants.navPosition}>
                 <Route exact path='/' component={DashboardPage} />
                 <Route exact path='/auth/login' component={LogInPage} />
                 <Route exact path='/auth/signup' component={SignUpPage} />
