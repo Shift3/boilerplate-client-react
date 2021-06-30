963bcfa58596b8436066ab14a6934016ab23d035
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-06-30 14:12:20 -0700

    chore(CircleCI): add job to push wiki pages to the wiki repo

---
+++ .circleci/deploy_wiki.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -x
+set -o errexit
+set -o pipefail
+set -o nounset
+
+DOCS_DIR=docs
+WIKI_REPO=https://github.com/Shift3/boilerplate-client-react.wiki.git
+WIKI_DIR=wiki
+
+git clone $WIKI_REPO $WIKI_DIR
+cp $DOCS_DIR/*.md $WIKI_DIR
+
+cd $WIKI_DIR
+git checkout master
+git add .
+git commit -m "chore: update wiki pages"
+git push
