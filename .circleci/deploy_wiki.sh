d07a1ac3732895bcca4049e2df17f44bdcaae2ad
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-06-30 23:50:47 -0700

    chore(CircleCI): kill terminate CI if no changes to wiki repo

--- .circleci/deploy_wiki.sh
+++ .circleci/deploy_wiki.sh
@@ -5,17 +5,30 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-DOCS_DIR=docs
-WIKI_REPO=https://github.com/Shift3/boilerplate-client-react.wiki.git
-WIKI_DIR=wiki
+PROJECT_DIR=boilerplate-client-react
+WIKI_DIR=boilerplate-client-react.wiki
+GH_WIKI_REPO=https://github.com/Shift3/$WIKI_DIR.git
 
-git clone $WIKI_REPO $WIKI_DIR
-cp $DOCS_DIR/*.md $WIKI_DIR
+# Change out of the project directory and clone the wiki repo
+cd ..
+git clone $GH_WIKI_REPO
 
+# Update wiki repository with documentation folder contents
+cp -rf $PROJECT_DIR/docs/* $WIKI_DIR/
+
+# Switch into wiki repository
 cd $WIKI_DIR
-git checkout master
-git config --global user.email "CircleCI"
-git config --global user.name "CircleCI"
-git add .
-git commit -m "chore: update wiki pages"
-git push
+
+# Terminate CI build if no changes detected to wiki repo
+if git diff-index --quiet HEAD && [ ! -n "$(git status -s)" ]; then 
+  set +e 
+  pkill -9 -P $$ &> /dev/null || true 
+  exit 0
+else
+# Otherwise, push changes to master branch of wiki repo
+  git config --global user.name "CircleCI"
+  git config --global user.email "boilerplate-client-react@bitwiseindustries.com"
+  git add .
+  git commit -m ""
+  git push
+fi
