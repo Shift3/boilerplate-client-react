52e5acae6c19e063496ad7b84578c47aa703d6db
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-07-01 00:44:46 -0700

    chore(CircleCI): get commit message and author info from last commit

--- .circleci/deploy_wiki.sh
+++ .circleci/deploy_wiki.sh
@@ -9,6 +9,12 @@ PROJECT_DIR=boilerplate-client-react
 WIKI_DIR=boilerplate-client-react.wiki
 GH_WIKI_REPO=https://github.com/Shift3/$WIKI_DIR.git
 
+# CircleCI sets the $CIRCLE_SHA1 environement variable to the sha1 hash of the last commit.
+# Using this hash, we can prase out the commit message and author info.
+GIT_LAST_COMMIT_MESSAGE=\"$(git log --format=%B -n 1 $CIRCLE_SHA1)\"
+GIT_LAST_COMMIT_AUTHOR_NAME=\"$(git log --format=%an -n 1 $CIRCLE_SHA1)\"
+GIT_LAST_COMMIT_AUTHOR_EMAIL=\"$(git log --format=%ae -n 1 $CIRCLE_SHA1)\"
+
 # Change out of the project directory and clone the wiki repo
 cd ..
 git clone $GH_WIKI_REPO
@@ -26,9 +32,10 @@ if git diff-index --quiet HEAD && [ ! -n "$(git status -s)" ]; then
   exit 0
 else
 # Otherwise, push changes to master branch of wiki repo
-  git config --global user.name $USER
-  git config --global user.email "boilerplate-client-react@bitwiseindustries.com"
+  git config --global user.name $GIT_LAST_COMMIT_AUTHOR_NAME
+  git config --global user.email $GIT_LAST_COMMIT_AUTHOR_EMAIL
   git add .
-  git commit -m ""
+  git commit -m $GIT_LAST_COMMIT_MESSAGE
   git push
 fi
+
