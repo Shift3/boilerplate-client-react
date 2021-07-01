35e93ef7766411e46193aa34b03a1669fe8bd0a8
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-07-01 01:56:29 -0700

    chore(deploy_wiki): test to see if variables get wrapped in single quote

--- .circleci/deploy_wiki.sh
+++ .circleci/deploy_wiki.sh
@@ -11,9 +11,9 @@ set -o nounset
 # https://stackoverflow.com/questions/9786498/expr-awk-sed-get-git-directory-name-from-repo-url
 GITHUB_REPO_NAME="$(echo $CIRCLE_REPOSITORY_URL | sed 's%^.*/\([^/]*\)\.git$%\1%g')"
 GITHUB_WIKI_REPO_URL=https://github.com/Shift3/$GITHUB_REPO_NAME.wiki.git
-PROJECT_DIR=boilerplate-client-react
+# PROJECT_DIR=boilerplate-client-react
 WIKI_DIR=boilerplate-client-react.wiki
-# PROJECT_DIR=$CIRCLE_WORKING_DIRECTORY
+PROJECT_DIR=$CIRCLE_WORKING_DIRECTORY
 # WIKI_DIR=$CIRCLE_WORKING_DIRECTORY/../$GITHUB_REPO_NAME.wiki
 
 # CircleCI sets the $CIRCLE_SHA1 environement variable to the sha1 hash of the last commit.
