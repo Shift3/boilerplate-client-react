433c1c6f5302eb91bb0e147a30802c5b572395d2
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-07-13 14:23:38 -0700

    feat(core/navigation): add TopNav component

---
+++ src/core/modules/navigation/presentation/topNav.tsx
@@ -0,0 +1,95 @@
+// React imports
+import { FC } from 'react';
+
+// Third party library imports
+import { useHistory } from 'react-router-dom';
+import styled, { css } from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+// App imports
+import logo from 'assets/img/logo.png';
+import { useNavData } from '../application/useNavLinks';
+import { CustomButton } from 'components/button/styled';
+
+const centeredFlexColumnStyle = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CustomNavLink = styled(Nav.Link)`
+  ${centeredFlexColumnStyle}
+`;
+
+const CustomNavDropdown = styled(NavDropdown)`
+  .dropdown-toggle.nav-link {
+    ${centeredFlexColumnStyle}
+  }
+
+  .dropdown-toggle.nav-link::after {
+    margin: 0;
+  }
+`;
+
+const CustomNavDropdownTitle: FC<{ name: string }> = ({ name }) => {
+  return (
+    <>
+      <FontAwesomeIcon icon='user' />
+      <span>Hi {name}</span>
+    </>
+  );
+};
+
+export const TopNav: FC = () => {
+  const history = useHistory();
+  const { authUser, navLinks } = useNavData();
+
+  const onLoginButtonClick = () => {
+    history.push('/auth/login');
+  };
+
+  return (
+    <Navbar collapseOnSelect expand='lg' className='shadow'>
+      <Navbar.Brand href='/content/agent-list'>
+        <img src={logo} alt='Bitwise Technology Consulting' width='160px' />
+      </Navbar.Brand>
+      {authUser ? (
+        <>
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse id='responsive-navbar-nav'>
+            <Nav className='mr-auto'>
+              {navLinks
+                .filter((link) => link.isActive)
+                .map((link) => (
+                  <CustomNavLink key={link.path} href={link.path}>
+                    <FontAwesomeIcon icon={link.icon} />
+                    <span>{link.label}</span>
+                  </CustomNavLink>
+                ))}
+            </Nav>
+            <Nav>
+              <CustomNavDropdown
+                id='top-nav-dropdown'
+                title={<CustomNavDropdownTitle name={authUser.firstName} />}
+                alignRight
+              >
+                <NavDropdown.Item href='/user/profile'>Profile</NavDropdown.Item>
+                <NavDropdown.Item href='/user/change-password'>Change Password</NavDropdown.Item>
+                <NavDropdown.Item>Toggle Navigation Bar</NavDropdown.Item>
+                <NavDropdown.Item>Sign Out</NavDropdown.Item>
+              </CustomNavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </>
+      ) : (
+        <Nav className='ml-auto'>
+          <CustomButton onClick={onLoginButtonClick}>LOGIN/CREATE ACCOUNT</CustomButton>
+        </Nav>
+      )}
+    </Navbar>
+  );
+};
