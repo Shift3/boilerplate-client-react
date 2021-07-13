4401ead433bd63382f66abca94bb42974f3e052b
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-07-13 14:23:08 -0700

    feat(core/navigation): add application logic to get nav data

---
+++ src/core/modules/navigation/application/useNavLinks.ts
@@ -0,0 +1,58 @@
+import { IconName } from '@fortawesome/fontawesome-svg-core';
+import { useAuthState } from 'core/modules/auth/application/useAuthState';
+import { User } from 'core/modules/user/domain/user';
+
+export interface IAuthUserData {
+  firstName: string;
+  lastName: string;
+}
+
+export interface INavLinkData {
+  icon: IconName;
+  label: string;
+  path: string;
+  isActive: boolean;
+}
+
+export interface INavData {
+  authUser: IAuthUserData | null;
+  navLinks: INavLinkData[];
+}
+
+export const useNavData = (): INavData => {
+  const session = useAuthState();
+  const user = session?.user ? User.fromPlainObject(session.user) : null;
+
+  const authUser = user
+    ? {
+        firstName: user.firstName,
+        lastName: user.lastName,
+      }
+    : null;
+
+  const navLinks: INavLinkData[] = [
+    {
+      icon: 'stethoscope',
+      label: 'Directory',
+      path: '/content/agent-list',
+      isActive: user?.role.canViewAgents() ?? false,
+    },
+    {
+      icon: 'users',
+      label: 'Directory',
+      path: '/admin/user-list',
+      isActive: user?.role.canViewUsers() ?? false,
+    },
+    {
+      icon: 'building',
+      label: 'Directory',
+      path: '/admin/agency-list',
+      isActive: user?.role.canViewAgencies() ?? false,
+    },
+  ];
+
+  return {
+    authUser,
+    navLinks,
+  };
+};
