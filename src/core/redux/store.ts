35c891f9764ef222d2523eb33d769b9c12b973a3
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-08-17 03:20:07 -0700

    refactor(redux/agencySlice): remove agency state slice

--- src/core/redux/store.ts
+++ src/core/redux/store.ts
@@ -1,14 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authSlice from 'core/modules/auth/infrastructure/store/authSlice';
 import notificationsSlice from 'core/modules/notifications/infrastructure/store/notificationsSlice';
-import agencySlice from 'redux/agency/slice';
 import { agencyApi } from 'services/agencyApi';
 
 const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
     notifications: notificationsSlice.reducer,
-    agencies: agencySlice.reducer,
     [agencyApi.reducerPath]: agencyApi.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(agencyApi.middleware),
--- src/redux/agency/selectors.ts
+++
@@ -1,5 +0,0 @@
-import { RootState } from 'core/redux';
-import { IAgency } from 'models/agency';
-import { agencySelectors } from './slice';
-
-export const selectAgencies = (state: RootState): IAgency[] => agencySelectors.selectAll(state);
