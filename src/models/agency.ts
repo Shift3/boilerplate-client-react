cb4683ce937e8a366b798d6f7b3333df8e313d28
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-08-17 09:30:43 -0700

    refactor(models): remove unnecessary models

--- src/models/agency.ts
+++ src/models/agency.ts
@@ -1,16 +1,3 @@
-export interface IAgency {
-  id: number;
-  agencyName: string;
-}
-
-export interface IAgencyDTO {
-  id: number;
-  createdBy?: number;
-  deletedAt?: string;
-  deletedBy?: number;
-  agencyName: string;
-}
-
 export interface Agency {
   id: number;
   createdBy?: number;
