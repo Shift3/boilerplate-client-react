dce31f5b195d6293f423ae4f413ab3d1f7039436
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-08-17 03:28:29 -0700

    refactor(notificationSlice): notificationAdapter doesn't need to be exported

--- src/core/modules/notifications/infrastructure/store/notificationsSlice.ts
+++ src/core/modules/notifications/infrastructure/store/notificationsSlice.ts
@@ -2,7 +2,7 @@ import { createEntityAdapter, createSlice } from '@reduxjs/toolkit';
 import { INotification } from 'core/modules/notifications/domain/notification';
 import { RootState } from 'core/redux';
 
-export const notificationAdapter = createEntityAdapter<INotification>();
+const notificationAdapter = createEntityAdapter<INotification>();
 
 const notificationSelectors = notificationAdapter.getSelectors<RootState>((state) => state.notifications);
 
