470b1b11c9bdc979975f9269ff0e0a69e3d4518d
Elizabeth Eidelson <fluturecode@gmail.com>
2021-08-24 12:07:27 -0400

    feat(agentList-view):modified /src/app/redux/store.ts

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -3,6 +3,7 @@ import authSlice from 'core/modules/auth/infrastructure/store/authSlice';
 import notificationsSlice from 'core/modules/notifications/infrastructure/store/notificationsSlice';
 import { agencyApi } from 'features/admin/agency';
 import { userApi } from 'features/admin/user/usersApi';
+import { agentApi } from '../../features/agent-dashboard/agentApi';
 
 const store = configureStore({
   reducer: {
@@ -10,9 +11,10 @@ const store = configureStore({
     notifications: notificationsSlice.reducer,
     [userApi.reducerPath]: userApi.reducer,
     [agencyApi.reducerPath]: agencyApi.reducer,
+    [agentApi.reducerPath]: agentApi.reducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(userApi.middleware, agencyApi.middleware),
-
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(userApi.middleware, agencyApi.middleware, agentApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
