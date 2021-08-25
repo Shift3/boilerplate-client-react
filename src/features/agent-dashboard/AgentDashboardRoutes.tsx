a1414aa3842ebb868fcda0c856208c7738c935bb
Elizabeth Eidelson <fluturecode@gmail.com>
2021-08-25 10:24:37 -0400

    feat(agentList-view):added CRUD view files

--- src/features/agent-dashboard/AgentDashboardRoutes.tsx
+++ src/features/agent-dashboard/AgentDashboardRoutes.tsx
@@ -1,6 +1,9 @@
 import { FC } from 'react';
 import { Route, Switch, useRouteMatch } from 'react-router-dom';
 import { AgentListView } from './pages/AgentListView';
+import { CreateAgentView } from './pages/CreateAgentView';
+import { DeleteAgentView } from './pages/DeleteAgentView';
+import { EditAgentView } from './pages/EditAgentView';
 
 export const AgentDashboardRoutes: FC = () => {
   const { path } = useRouteMatch();
@@ -8,8 +11,9 @@ export const AgentDashboardRoutes: FC = () => {
   return (
     <Switch>
       <Route path={`${path}/agent-list`} component={AgentListView} />
-      {/* <Route path={`${path}/create-agent`} component={CreateAgentView} />
-      <Route path={`${path}/edit-agent`} component={EditAgentView} /> */}
+      <Route path={`${path}/create-agent`} component={CreateAgentView} />
+      <Route path={`${path}/edit-agent`} component={EditAgentView} />
+      <Route path={`${path}/delete-agent`} component={DeleteAgentView} />
     </Switch>
   );
 };
---
+++ src/features/agent-dashboard/pages/DeleteAgentView.tsx
@@ -0,0 +1,5 @@
+import { FC } from 'react';
+
+export const DeleteAgentView: FC = () => {
+  return <h1>Delete Agent View</h1>;
+};
