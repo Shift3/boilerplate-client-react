191634a2ca178db8e3ed3994b329440df44ca70f
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-08-25 11:31:52 -0700

    feat(AgencyListView): hanlde deleting agencies

--- src/features/admin/agency/pages/AgencyListView.tsx
+++ src/features/admin/agency/pages/AgencyListView.tsx
@@ -1,12 +1,12 @@
-/* eslint-disable react/no-array-index-key */
-/* eslint-disable react/no-unused-prop-types */
 /* eslint-disable no-console */
-import { FC, useMemo } from 'react';
-import { useGetAgenciesQuery } from 'features/admin/agency/agencyApi';
+import { FC } from 'react';
+import { useDeleteAgencyMutation, useGetAgenciesQuery } from 'features/admin/agency/agencyApi';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import GenericTable, { TableHeader } from 'common/components/GenericTable';
 import ActionButton, { ActionButtonProps } from 'common/components/ActionButton';
+import { useConfirmationModal } from 'common/hooks';
+import { Agency } from 'common/models';
 
 type AgencyTableItem = {
   id: number;
@@ -16,6 +16,21 @@ type AgencyTableItem = {
 
 export const AgencyListView: FC = () => {
   const { data: agencies = [] } = useGetAgenciesQuery();
+  const [deleteAgency] = useDeleteAgencyMutation();
+  const { Modal: ConfirmationModal, openModal, closeModal } = useConfirmationModal();
+
+  const handleDelete = (agency: Agency) => {
+    const message = `Delete ${agency.agencyName}?`;
+
+    const onConfirm = () => {
+      deleteAgency(agency.id);
+      closeModal();
+    };
+
+    const onCancel = () => closeModal();
+
+    openModal(message, onConfirm, onCancel);
+  };
 
   // Set up table headers
   const headers: TableHeader<AgencyTableItem>[] = [
@@ -24,39 +39,30 @@ export const AgencyListView: FC = () => {
   ];
 
   // Transform Agency objects returned from the API into the table item data format expected by the table.
-  const items: AgencyTableItem[] = useMemo(
-    () =>
-      agencies.map((agency) => ({
-        id: agency.id,
-        name: agency.agencyName,
-        actions: [
-          { icon: 'edit', tooltipText: 'Edit', onClick: () => console.log(`Edit ${agency.agencyName}`) },
-          {
-            icon: 'trash-alt',
-            tooltipText: 'Delete',
-            onClick: () => console.log(`Delete ${agency.agencyName}`),
-          },
-        ],
-      })),
-    // In this case it's ok to use the agencies array as a dependency because RTK Query
-    // memoizes the returned values.
-    [agencies],
-  );
+  const items: AgencyTableItem[] = agencies.map((agency) => ({
+    id: agency.id,
+    name: agency.agencyName,
+    actions: [
+      { icon: 'edit', tooltipText: 'Edit', onClick: () => console.log(`Edit ${agency.agencyName}`) },
+      {
+        icon: 'trash-alt',
+        tooltipText: 'Delete',
+        onClick: () => handleDelete(agency),
+      },
+    ],
+  }));
 
   // Specify custom render methods for any property in the table items that need to be rendered as a custom component.
   // Here we want the actions to be rendered using a custom component.
-  const customRenderers = useMemo(
-    () => ({
-      actions: ({ actions }: AgencyTableItem) => (
-        <>
-          {actions.map((action, index) => (
-            <ActionButton key={index} icon={action.icon} tooltipText={action.tooltipText} onClick={action.onClick} />
-          ))}
-        </>
-      ),
-    }),
-    [],
-  );
+  const customRenderers = {
+    actions: ({ actions }: AgencyTableItem) => (
+      <>
+        {actions.map((action, index) => (
+          <ActionButton key={index} icon={action.icon} tooltipText={action.tooltipText} onClick={action.onClick} />
+        ))}
+      </>
+    ),
+  };
 
   return (
     <Container>
@@ -64,6 +70,7 @@ export const AgencyListView: FC = () => {
         <Button>ADD AGENCY</Button>
       </div>
       <GenericTable<AgencyTableItem> headers={headers} items={items} customRenderers={customRenderers} />
+      <ConfirmationModal />
     </Container>
   );
 };
