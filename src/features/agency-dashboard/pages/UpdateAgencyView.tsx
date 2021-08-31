64764e0deb39d8626552cc1de0173924fa20fae8
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-08-31 13:14:55 -0700

    feat(agency-dashboard): add boilerplate for update view

---
+++ src/features/agency-dashboard/pages/UpdateAgencyView.tsx
@@ -0,0 +1,6 @@
+import { FC } from 'react';
+import { Container } from 'react-bootstrap';
+
+export const UpdateAgencyView: FC = () => {
+  return <Container className='d-flex justify-content-center'>Update agency view</Container>;
+};
