1477e3b22c07dbd9f46aba2f6344b4f11b9a15f1
Elizabeth Eidelson <fluturecode@gmail.com>
2021-08-31 12:56:07 -0400

    feat(generic-table): removed admin/agencyListView.tsx

--- src/common/components/GenericTable/types.ts
+++ src/common/components/GenericTable/types.ts
@@ -3,7 +3,6 @@ import { ReactNode } from 'react';
 export type BaseTableItem = {
   id: number | string;
 };
-
 export type TableHeader<TableItem> = { key: keyof TableItem; label: string };
 
 export type CustomRenderer<TableItem> = { key: keyof TableItem; renderer: (item: TableItem) => ReactNode };
