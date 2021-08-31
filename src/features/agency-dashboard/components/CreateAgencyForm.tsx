6638166e7ccd5cd1a30f40d517e934c13c0951a6
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-08-31 04:33:48 -0700

    refactor(agency-dashboard): modify type names

--- src/features/agency-dashboard/components/CreateAgencyForm.tsx
+++ src/features/agency-dashboard/components/CreateAgencyForm.tsx
@@ -5,30 +5,30 @@ import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { StyledCancelButton, StyledFormButtonWrapper, StyledSubmitButton } from './styled';
 
-export interface FormInputs {
+export interface CreateAgencyFormData {
   agencyName: string;
 }
 
-export interface Props {
+export interface CreateAgencyFormProps {
   onCancel: () => void;
-  onSubmit: (data: FormInputs) => void;
+  onSubmit: (data: CreateAgencyFormData) => void;
 }
 
 const schema = yup.object().shape({
-  agencyName: yup.string().required(),
+  agencyName: yup.string().required('Agency Name is required.'),
 });
 
-export const CreateAgencyForm: FC<Props> = ({ onCancel, onSubmit }) => {
+export const CreateAgencyForm: FC<CreateAgencyFormProps> = ({ onCancel, onSubmit }) => {
   const {
     register,
     formState: { errors, isValid },
     handleSubmit,
     trigger,
-  } = useForm<FormInputs>({ resolver: yupResolver(schema), mode: 'all' });
+  } = useForm<CreateAgencyFormData>({ resolver: yupResolver(schema), mode: 'all' });
 
   useEffect(() => {
     // Trigger form validation when the component renders for the first time
-    // so validation errors show.
+    // so validation errors show right away.
     trigger();
   }, [trigger]);
 
@@ -37,7 +37,7 @@ export const CreateAgencyForm: FC<Props> = ({ onCancel, onSubmit }) => {
       <Form.Group controlId='create-agency-form-agency-name'>
         <Form.Label>Agency Name</Form.Label>
         <Form.Control type='text' {...register('agencyName')} isInvalid={!!errors.agencyName} />
-        <Form.Control.Feedback type='invalid'>Agency Name is required.</Form.Control.Feedback>
+        <Form.Control.Feedback type='invalid'>{errors.agencyName?.message}</Form.Control.Feedback>
       </Form.Group>
       <StyledFormButtonWrapper>
         <StyledCancelButton onClick={onCancel}>CANCEL</StyledCancelButton>
--- src/features/agency-dashboard/pages/CreateAgencyView.tsx
+++ src/features/agency-dashboard/pages/CreateAgencyView.tsx
@@ -1,22 +1,32 @@
+import { useShowNotification } from 'core/modules/notifications/application/useShowNotification';
 import { FC } from 'react';
 import Container from 'react-bootstrap/Container';
 import { useHistory } from 'react-router-dom';
-import { CreateAgencyForm } from '../components/CreateAgencyForm';
+import { useCreateAgencyMutation } from '../agencyApi';
+import { CreateAgencyForm, CreateAgencyFormData } from '../components/CreateAgencyForm';
 import { StyledFormTitle, StyledFormWrapper } from '../components/styled';
 
 export const CreateAgencyView: FC = () => {
   const history = useHistory();
+  const [createAgency] = useCreateAgencyMutation();
+  const { showErrorNotification, showSuccessNotification } = useShowNotification();
 
   const handleFormCancel = () => {
     history.goBack();
   };
 
-  const handleFormSubmit = () => {
-    console.log('submitted');
+  const handleFormSubmit = async (data: CreateAgencyFormData) => {
+    try {
+      await createAgency(data).unwrap();
+      showSuccessNotification('Agency created.');
+      history.push('/agencies');
+    } catch (error) {
+      showErrorNotification(JSON.stringify(error));
+    }
   };
 
   return (
-    <Container className=''>
+    <Container className='d-flex justify-content-center'>
       <StyledFormWrapper>
         <StyledFormTitle>Create Agency</StyledFormTitle>
         <CreateAgencyForm onCancel={handleFormCancel} onSubmit={handleFormSubmit} />
