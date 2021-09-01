d7a13c383ad8a1cb853f5644f67008938ff87c41
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-09-01 15:25:31 -0700

    feat(agency-dashboard): add update form

---
+++ src/features/agency-dashboard/components/UpdateAgencyForm.tsx
@@ -0,0 +1,50 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { FC, useEffect } from 'react';
+import Form from 'react-bootstrap/Form';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { StyledCancelButton, StyledFormButtonWrapper, StyledSubmitButton } from './styled';
+
+export interface UpdateAgencyFormData {
+  agencyName: string;
+}
+
+export interface UpdateAgencyFormProps {
+  onCancel: () => void;
+  onSubmit: (data: UpdateAgencyFormData) => void;
+}
+
+const schema = yup.object().shape({
+  agencyName: yup.string().required('Agency Name is required.'),
+});
+
+export const UpdateAgencyForm: FC<UpdateAgencyFormProps> = ({ onCancel, onSubmit }) => {
+  const {
+    register,
+    formState: { errors, isValid },
+    handleSubmit,
+    trigger,
+  } = useForm<UpdateAgencyFormData>({ resolver: yupResolver(schema), mode: 'all' });
+
+  useEffect(() => {
+    // Trigger form validation when the component renders for the first time
+    // so validation errors show right away.
+    trigger();
+  }, [trigger]);
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <Form.Group controlId='create-agency-form-agency-name'>
+        <Form.Label>Agency Name</Form.Label>
+        <Form.Control type='text' {...register('agencyName')} isInvalid={!!errors.agencyName} />
+        <Form.Control.Feedback type='invalid'>{errors.agencyName?.message}</Form.Control.Feedback>
+      </Form.Group>
+      <StyledFormButtonWrapper>
+        <StyledCancelButton onClick={onCancel}>CANCEL</StyledCancelButton>
+        <StyledSubmitButton type='submit' disabled={!isValid}>
+          UPDATE
+        </StyledSubmitButton>
+      </StyledFormButtonWrapper>
+    </Form>
+  );
+};
--- src/features/agency-dashboard/pages/UpdateAgencyView.tsx
+++ src/features/agency-dashboard/pages/UpdateAgencyView.tsx
@@ -1,6 +1,20 @@
 import { FC } from 'react';
 import { Container } from 'react-bootstrap';
+import { StyledFormTitle, StyledFormWrapper } from '../components/styled';
+import { UpdateAgencyForm } from '../components/';
+
+export interface RouteParams {
+  id: string;
+}
 
 export const UpdateAgencyView: FC = () => {
-  return <Container className='d-flex justify-content-center'>Update agency view</Container>;
+  return (
+    <Container className='d-flex justify-content-center'>
+      <StyledFormWrapper>
+        <StyledFormTitle>Update Agency</StyledFormTitle>
+        {/* eslint-disable-next-line @typescript-eslint/no-empty-function*/}
+        <UpdateAgencyForm onSubmit={() => {}} onCancel={() => {}} />
+      </StyledFormWrapper>
+    </Container>
+  );
 };
