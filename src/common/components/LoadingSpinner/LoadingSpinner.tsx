8ca998c39c3708996d3eebc033f0b2f6eac07fa9
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-09-30 11:29:39 -0700

    feat(LoadingSpinner): center spinner and make it larger

--- src/common/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/common/components/LoadingSpinner/LoadingSpinner.tsx
@@ -2,7 +2,9 @@ import { FC } from 'react';
 import { Spinner } from 'react-bootstrap';
 
 export const LoadingSpinner: FC = () => (
-  <Spinner animation='border' variant='primary' role='status'>
-    <span className='visually-hidden'>Loading...</span>
-  </Spinner>
+  <div className='py-3 text-center'>
+    <Spinner animation='border' variant='primary' role='status' style={{ width: '4em', height: '4em' }}>
+      <span className='visually-hidden'>Loading...</span>
+    </Spinner>
+  </div>
 );
