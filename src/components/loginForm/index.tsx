3efc1f26fa785b579707a2d0e19d2e2adf343aa1
Elizabeth Eidelson <fluturecode@gmail.com>
2021-10-01 05:22:45 -0400

    feat(login-refactor):updated /components/loginForm/index.tsx & /components/loginForm.styled.tsx

--- src/components/loginForm/index.tsx
+++ src/components/loginForm/index.tsx
@@ -1,11 +1,24 @@
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { Form } from 'react-bootstrap';
-import { ButtonWrapper, CancelButton, InputError, FormLabel, LogInButton, StyledForm, Title } from './styled';
+import {
+  ButtonWrapper,
+  CreateAccountButton,
+  CancelButton,
+  InputError,
+  FormLabel,
+  LogInButton,
+  StyledForm,
+  Title,
+  LinkWrapper,
+  Text,
+} from './styled';
 import { LogInFormSchema } from './schema';
 import { LogInFormType } from './types';
+import { Link, useHistory } from 'react-router-dom';
 
 export const LogInForm: LogInFormType = ({ onSubmit, onCancel }) => {
+  const history = useHistory();
   const {
     register,
     handleSubmit,
@@ -15,29 +28,41 @@ export const LogInForm: LogInFormType = ({ onSubmit, onCancel }) => {
     mode: 'onChange',
   });
 
+  const navigateToSignup = () => history.push('/auth/signup');
+
   return (
-    <>
-      <StyledForm data-testid='loginForm' onSubmit={handleSubmit(onSubmit)}>
-        <Title>Member Log In</Title>
-        <Form.Group>
-          <FormLabel htmlFor='email'>Email</FormLabel>
-          <Form.Control id='email' type='email' {...register('email')} placeholder='Enter email' />
-          {errors.email?.message && <InputError role='alert'>{errors.email?.message}</InputError>}
-        </Form.Group>
-        <Form.Group>
-          <FormLabel htmlFor='password'>Password</FormLabel>
-          <Form.Control id='password' type='password' {...register('password')} placeholder='Enter password' />
-          {errors.password?.message && <InputError role='alert'>{errors.password?.message}</InputError>}
-        </Form.Group>
-        <ButtonWrapper>
-          <CancelButton data-testid='cancelButton' onClick={onCancel}>
-            CANCEL
-          </CancelButton>
-          <LogInButton data-testid='submitButton' type='submit' disabled={!isValid}>
-            LOG IN
-          </LogInButton>
-        </ButtonWrapper>
-      </StyledForm>
-    </>
+    <StyledForm data-testid='loginForm' onSubmit={handleSubmit(onSubmit)}>
+      <Title>Member Log In</Title>
+      <Form.Group>
+        <FormLabel htmlFor='email'>Email</FormLabel>
+        <Form.Control id='email' type='email' {...register('email')} placeholder='Enter email' />
+        {errors.email?.message && <InputError role='alert'>{errors.email?.message}</InputError>}
+      </Form.Group>
+      <Form.Group>
+        <FormLabel htmlFor='password'>Password</FormLabel>
+        <Form.Control id='password' type='password' {...register('password')} placeholder='Enter password' />
+        {errors.password?.message && <InputError role='alert'>{errors.password?.message}</InputError>}
+      </Form.Group>
+      <ButtonWrapper>
+        <CancelButton data-testid='cancelButton' onClick={onCancel}>
+          CANCEL
+        </CancelButton>
+        <LogInButton data-testid='submitButton' type='submit' disabled={!isValid}>
+          LOG IN
+        </LogInButton>
+      </ButtonWrapper>
+      <Title>
+        <h2 data-testid='loginPageInfoHeading'>Not Registered Yet?</h2>
+      </Title>
+      <Text>
+        <p data-testid='loginPageInfoContent'>Registering for your account is quick and easy.</p>
+      </Text>
+      <CreateAccountButton data-testid='createAccountButton' onClick={navigateToSignup}>
+        CREATE ACCOUNT
+      </CreateAccountButton>
+      <LinkWrapper>
+        <Link to='/auth/forgot-password'>Forgot Password?</Link>
+      </LinkWrapper>
+    </StyledForm>
   );
 };
--- src/components/loginPage/index.tsx
+++ src/components/loginPage/index.tsx
@@ -1,10 +1,9 @@
 import { LogInForm } from 'components/loginForm';
 import { useLogin } from 'features/auth/hooks';
 import { FC } from 'react';
-import { Button } from 'react-bootstrap';
-import { Link, useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import { ILogInFormData } from '../loginForm/types';
+import { useHistory } from 'react-router-dom';
 
 const Wrapper = styled.div`
   display: flex;
@@ -15,6 +14,15 @@ const Wrapper = styled.div`
   background-color: ${(props) => props.theme.authBackground};
 `;
 
+// const RightLogin = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: space-between;
+//   padding: 50px;
+//   max-width: 350px;
+//   min-height: 400px;
+// `;
+
 const LoginWrapper = styled.div`
   justify-content: center;
   align-items: center;
@@ -22,41 +30,6 @@ const LoginWrapper = styled.div`
   background-color: ${(props) => props.theme.primary};
   border-radius: 5px;
 `;
-
-const LeftLogin = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-`;
-
-const LinkWrapper = styled.div`
-  padding-bottom: 30px;
-  font-size: 1.2em;
-`;
-
-const RightLogin = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  padding: 50px;
-  max-width: 350px;
-  min-height: 400px;
-`;
-
-const Title = styled.div`
-  color: ${(props) => props.theme.cardHeader};
-  font-style: bold;
-  padding-bottom: 10px;
-  width: 200px;
-`;
-
-const Text = styled.div`
-  color: white;
-`;
-
-const CreateAccountButton = styled(Button)``;
 
 export const LogInPage: FC = () => {
   const history = useHistory();
@@ -68,28 +41,10 @@ export const LogInPage: FC = () => {
 
   const onCancel = () => history.push('/auth/login');
 
-  const navigateToSignup = () => history.push('/auth/signup');
-
   return (
     <Wrapper data-testid='wrapper'>
       <LoginWrapper data-testid='loginWrapper'>
-        <LeftLogin data-testid='leftLogin'>
-          <LogInForm onSubmit={onSubmit} onCancel={onCancel} />
-          <LinkWrapper>
-            <Link to='/auth/forgot-password'>Forgot Password?</Link>
-          </LinkWrapper>
-        </LeftLogin>
-        <RightLogin data-testid='rightLogin'>
-          <Title>
-            <h2 data-testid='loginPageInfoHeading'>Not Registered Yet?</h2>
-          </Title>
-          <Text>
-            <p data-testid='loginPageInfoContent'>Registering for your account is quick and easy.</p>
-          </Text>
-          <CreateAccountButton data-testid='createAccountButton' onClick={navigateToSignup}>
-            CREATE ACCOUNT
-          </CreateAccountButton>
-        </RightLogin>
+        <LogInForm onSubmit={onSubmit} onCancel={onCancel} />
       </LoginWrapper>
     </Wrapper>
   );
