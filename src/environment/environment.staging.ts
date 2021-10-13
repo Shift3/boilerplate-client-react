23df0c4f17a95ba7f01ce1931dbddbf9c8771b60
Elizabeth Eidelson <fluturecode@gmail.com>
2021-10-13 17:49:46 -0400

    feat(version-number): modified deploy_staging.sh package.json src/environment/enviroment.staging.ts

--- src/environment/environment.staging.ts
+++ src/environment/environment.staging.ts
@@ -2,7 +2,7 @@ import { name, version } from '../../package.json';
 import { EnvironmentConfiguration, IEnvironment } from './types';
 
 export const environment: IEnvironment = {
-  apiRoute: 'https://boilerplate-server-node-policy-test.us-west-2.elasticbeanstalk.com',
+  apiRoute: 'https://react-bp-server-node.shift3sandbox.com',
   environment: EnvironmentConfiguration.Staging,
   isProduction: process.env.NODE_ENV === 'production',
   name,
