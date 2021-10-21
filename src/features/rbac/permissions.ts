d7b2c852fbd5529a938728aea04e28f5d9076017
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-10-21 13:27:18 -0700

    feat(rbac): add permissions and policy definitions

---
+++ src/features/rbac/permissions.ts
@@ -0,0 +1,14 @@
+export type Permission =
+  | 'agency:create'
+  | 'agency:read'
+  | 'agency:update'
+  | 'agency:delete'
+  | 'agent:create'
+  | 'agent:read'
+  | 'agent:update'
+  | 'agent:delete'
+  | 'role:read'
+  | 'user:create'
+  | 'user:read'
+  | 'user:update'
+  | 'user:delete';
