37d4a8949d4c20e7c0c0cf53c32064937bff571d
Elizabeth Eidelson <fluturecode@gmail.com>
2021-10-21 15:13:47 -0400

    feat(dashboard-refactor):fixed failing tests in GenericTable.spec.tsx

--- src/common/components/GenericTable/GenericTable.spec.tsx
+++ src/common/components/GenericTable/GenericTable.spec.tsx
@@ -31,31 +31,51 @@ describe('GenericTable', () => {
   });
 
   it('should render one table element', () => {
-    const { container } = render(<GenericTable<TestTableItem> items={[]} headers={testTableHeaders} />);
+    const { container } = render(
+      <ThemeProvider theme={AppTheme}>
+        <GenericTable<TestTableItem> items={[]} headers={testTableHeaders} />
+      </ThemeProvider>,
+    );
     expect(container.querySelector('table')).toBeInTheDocument();
     expect(container.querySelectorAll('table')).toHaveLength(1);
   });
 
   it('should render one thead element', () => {
-    const { container } = render(<GenericTable<TestTableItem> items={[]} headers={testTableHeaders} />);
+    const { container } = render(
+      <ThemeProvider theme={AppTheme}>
+        <GenericTable<TestTableItem> items={[]} headers={testTableHeaders} />
+      </ThemeProvider>,
+    );
     expect(container.querySelector('thead')).toBeInTheDocument();
     expect(container.querySelectorAll('thead')).toHaveLength(1);
   });
 
   it('should render one tbody element', () => {
-    const { container } = render(<GenericTable<TestTableItem> items={[]} headers={testTableHeaders} />);
+    const { container } = render(
+      <ThemeProvider theme={AppTheme}>
+        <GenericTable<TestTableItem> items={[]} headers={testTableHeaders} />
+      </ThemeProvider>,
+    );
     expect(container.querySelector('tbody')).toBeInTheDocument();
     expect(container.querySelectorAll('tbody')).toHaveLength(1);
   });
 
   it('should render a th tag for each object in headers prop', () => {
-    const { container } = render(<GenericTable<TestTableItem> items={[]} headers={testTableHeaders} />);
+    const { container } = render(
+      <ThemeProvider theme={AppTheme}>
+        <GenericTable<TestTableItem> items={[]} headers={testTableHeaders} />
+      </ThemeProvider>,
+    );
     const headers = container.querySelectorAll('th');
     expect(headers).toHaveLength(testTableHeaders.length);
   });
 
   it('should render the correct text in each header column', () => {
-    const { container } = render(<GenericTable<TestTableItem> items={[]} headers={testTableHeaders} />);
+    const { container } = render(
+      <ThemeProvider theme={AppTheme}>
+        <GenericTable<TestTableItem> items={[]} headers={testTableHeaders} />
+      </ThemeProvider>,
+    );
     const headers = container.querySelectorAll('th');
     headers.forEach((th, index) => {
       expect(th.textContent).toBe(testTableHeaders[index].label);
@@ -63,31 +83,51 @@ describe('GenericTable', () => {
   });
 
   it('should not render any rows in the table body if the items prop is an empty array', () => {
-    const { container } = render(<GenericTable<TestTableItem> items={[]} headers={testTableHeaders} />);
+    const { container } = render(
+      <ThemeProvider theme={AppTheme}>
+        <GenericTable<TestTableItem> items={[]} headers={testTableHeaders} />
+      </ThemeProvider>,
+    );
     const tbody = container.querySelector('tbody');
     const rows = tbody?.querySelectorAll('tr');
     expect(rows).toHaveLength(0);
   });
 
   it('should render "No data" text if the items prop is an empty array', () => {
-    const { getByText } = render(<GenericTable<TestTableItem> items={[]} headers={testTableHeaders} />);
+    const { getByText } = render(
+      <ThemeProvider theme={AppTheme}>
+        <GenericTable<TestTableItem> items={[]} headers={testTableHeaders} />
+      </ThemeProvider>,
+    );
     expect(getByText('No data')).toBeInTheDocument();
   });
 
   it('should not render "No data" text if the items prop is not an empty array', () => {
-    const { queryByText } = render(<GenericTable<TestTableItem> items={testTableItems} headers={testTableHeaders} />);
+    const { queryByText } = render(
+      <ThemeProvider theme={AppTheme}>
+        <GenericTable<TestTableItem> items={testTableItems} headers={testTableHeaders} />
+      </ThemeProvider>,
+    );
     expect(queryByText('No data')).toBeNull();
   });
 
   it('should render a row in the table body for each item in the items prop', () => {
-    const { container } = render(<GenericTable<TestTableItem> items={testTableItems} headers={testTableHeaders} />);
+    const { container } = render(
+      <ThemeProvider theme={AppTheme}>
+        <GenericTable<TestTableItem> items={testTableItems} headers={testTableHeaders} />
+      </ThemeProvider>,
+    );
     const tbody = container.querySelector('tbody');
     const rows = tbody?.querySelectorAll('tr');
     expect(rows).toHaveLength(testTableItems.length);
   });
 
   it('should render the correct data in each column of the rows', () => {
-    const { container } = render(<GenericTable<TestTableItem> items={testTableItems} headers={testTableHeaders} />);
+    const { container } = render(
+      <ThemeProvider theme={AppTheme}>
+        <GenericTable<TestTableItem> items={testTableItems} headers={testTableHeaders} />
+      </ThemeProvider>,
+    );
     const tbody = container.querySelector('tbody');
     const rows = tbody?.querySelectorAll('tr');
 
@@ -110,11 +150,14 @@ describe('GenericTable', () => {
     ];
 
     const { container } = render(
-      <GenericTable<TestTableItem>
-        items={testTableItems}
-        headers={testTableHeaders}
-        customRenderers={customRenderers}
-      />,
+      <ThemeProvider theme={AppTheme}>
+        <GenericTable<TestTableItem>
+          items={testTableItems}
+          headers={testTableHeaders}
+          customRenderers={customRenderers}
+        />
+        ,
+      </ThemeProvider>,
     );
 
     const tbody = container.querySelector('tbody');
