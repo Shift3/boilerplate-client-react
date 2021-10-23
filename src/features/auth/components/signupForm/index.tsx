b4cfa50f6d4549d4a6b91a7cc647d0f5825c9e14
Elizabeth Eidelson <fluturecode@gmail.com>
2021-10-23 02:19:39 -0400

    feat(signupForm-test)): modified /src/features/auth/signupForm/index.tsx

--- src/features/auth/components/signupForm/index.tsx
+++ src/features/auth/components/signupForm/index.tsx
@@ -4,6 +4,7 @@ import { Form } from 'react-bootstrap';
 import { SignUpFormSchema } from './schema';
 import { SignUpFormType } from './types';
 import { ButtonWrapper, CancelButton, SubmitButton, StyledForm } from '../../../styles/PageStyles';
+
 export const SignUpForm: SignUpFormType = ({ onSubmit, onCancel }) => {
   const {
     register,
