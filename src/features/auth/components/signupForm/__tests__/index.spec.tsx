afa7e396800767a1d10cf9c76553c062082e286e
Elizabeth Eidelson <fluturecode@gmail.com>
2021-10-22 21:55:15 -0400

    feat(signupForm-test):modified src/features/auth/components/signupForm/__tests__/index.spec.tsx

--- src/features/auth/components/signupForm/__tests__/index.spec.tsx
+++ src/features/auth/components/signupForm/__tests__/index.spec.tsx
@@ -1,129 +1,55 @@
-import { render } from '@testing-library/react';
-import {
-  clickByTestIdAsync,
-  expectInDocByLabelText,
-  expectInDocByTestId,
-  expectInnerHTMLByRole,
-  expectLengthByRole,
-  expectMockFunctionCalled,
-  expectMockFunctionNotCalled,
-  setValueByLabelText,
-  formAlertMessageCheck,
-} from 'utils/test';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { SignUpForm } from '../index';
-import { Constants } from 'utils/constants';
 import { ThemeProvider } from 'styled-components';
 import AppTheme from 'utils/styleValues';
 
-const { EMAIL_REQUIRED, INVALID_EMAIL, EMAIL_MATCH, FIRST_NAME_REQUIRED, LAST_NAME_REQUIRED } = Constants.errorMessages;
+const mockOnSubmit = jest.fn();
+const mockOnCancel = jest.fn();
 
 describe('SignupForm', () => {
-  const validEmail = 'test@test.com';
-  const invalidEmail = 'test.com';
-  const mismatchEmail = 'test@tets.com';
-  const validName = 'test';
-  const shortName = 't';
-  const longName = 'thisisclearlywaytoolongandisnotavalidnamebecauseitiswelloverfiftycharacters';
-
-  const mockOnSubmit = jest.fn();
-  const mockOnCancel = jest.fn();
-
   beforeEach(async () => {
     render(
       <ThemeProvider theme={AppTheme}>
         <SignUpForm onSubmit={mockOnSubmit} onCancel={mockOnCancel} />
       </ThemeProvider>,
     );
-
-    await setValueByLabelText('Email', validEmail);
-    await setValueByLabelText('Confirm Email', validEmail);
-    await setValueByLabelText('First Name', validName);
-    await setValueByLabelText('Last Name', validName);
   });
 
-  it('Should render email field', () => expectInDocByLabelText('Email'));
-
-  it.skip('Should render confirm email field', () => expectInDocByLabelText('Confirm Email'));
-
-  it.skip('Should render first name field', () => expectInDocByLabelText('First Name'));
-
-  it.skip('Should render last name field', () => expectInDocByLabelText('Last Name'));
-
-  it.skip('Should render sign up button', () => expectInDocByTestId('signUpButton'));
-
-  describe('Valid input', () => {
-    it.skip('Should call onSubmit once all form data is valid, ', async () => {
-      await clickByTestIdAsync('signUpButton');
-      expectMockFunctionCalled(mockOnSubmit);
-
-      mockOnSubmit.mockReset();
-    });
-
-    it.skip('Should not display error messages', () => expectLengthByRole('alert', 0));
+  it('should render the basic fields', () => {
+    expect(screen.getByRole('textbox', { name: /^email$/i })).toBeInTheDocument();
+    expect(screen.getByRole('textbox', { name: /^Confirm Email$/i })).toBeInTheDocument();
+    expect(screen.getByRole('textbox', { name: /^First Name$/i })).toBeInTheDocument();
+    expect(screen.getByRole('textbox', { name: /^Last Name$/i })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'CANCEL' })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'SIGN UP' })).toBeInTheDocument();
   });
 
-  describe('Invalid input', () => {
-    it.skip('Should not call onSubmit', async () => {
-      await setValueByLabelText('Email', '');
-      await setValueByLabelText('Confirm Email', '');
-      await setValueByLabelText('First Name', '');
-      await setValueByLabelText('Last Name', '');
-      await clickByTestIdAsync('signUpButton');
-      expectMockFunctionNotCalled(mockOnSubmit);
+  it('should validate form fields', async () => {
+    userEvent.type(screen.getByRole('textbox', { name: /^email$/i }), 'testEmail');
+    userEvent.type(screen.getByRole('textbox', { name: /^Confirm Email$/i }), 'confirmTestEmail');
+    userEvent.type(screen.getByRole('textbox', { name: /^First Name$/i }), '123');
+    userEvent.type(screen.getByRole('textbox', { name: /^Last Name$/i }), '456');
 
-      mockOnSubmit.mockReset();
-    });
-
-    it.skip('Should display error messages', async () => {
-      await setValueByLabelText('Email', invalidEmail);
-      await setValueByLabelText('Confirm Email', mismatchEmail);
-      await setValueByLabelText('First Name', longName);
-      await setValueByLabelText('Last Name', shortName);
-      expectLengthByRole('alert', 2);
-    });
+    fireEvent.submit(screen.getByRole('button', { name: 'SIGN UP' }));
+    expect(await screen.findAllByRole('alert')).toHaveLength(4);
+    expect(mockOnSubmit).not.toBeCalled();
   });
 
-  describe('Invalid email', () => {
-    it.skip('Should only display invalid email error message', async () => {
-      await setValueByLabelText('Email', invalidEmail);
-      expectLengthByRole('alert', 1);
-      expectInnerHTMLByRole('alert', INVALID_EMAIL);
-    });
-  });
+  it('should submit the form', async () => {
+    const emailInput = screen.getByRole('textbox', { name: /^email/i });
+    userEvent.type(emailInput, 'test@email.com');
 
-  describe('Non matching email', () => {
-    it.skip('Should only display email mismatch error message', async () => {
-      await setValueByLabelText('Confirm Email', mismatchEmail);
-      expectLengthByRole('alert', 1);
-      expectInnerHTMLByRole('alert', EMAIL_MATCH);
-    });
-  });
+    const confirmEmailInput = screen.getByRole('textbox', { name: /^Confirm Email$/i });
+    userEvent.type(confirmEmailInput, 'test@email.com');
 
-  describe('Required email', () => {
-    it.skip('Should display email required message', async () => {
-      await setValueByLabelText('Email', '');
-      await setValueByLabelText('Confirm Email', '');
-      expectLengthByRole('alert', 2);
-      formAlertMessageCheck(EMAIL_REQUIRED);
-    });
+    const firstNameInput = screen.getByRole('textbox', { name: /^First Name$/i });
+    userEvent.type(firstNameInput, 'Test');
 
-    it.skip('Should display emailConfirm required message', async () => {
-      await setValueByLabelText('Confirm Email', '');
-      await setValueByLabelText('Email', '');
-      expectLengthByRole('alert', 2);
-      formAlertMessageCheck(EMAIL_REQUIRED);
-    });
+    const lastNameInput = screen.getByRole('textbox', { name: /^Last Name$/i });
+    userEvent.type(lastNameInput, 'Last');
 
-    it.skip('Should only display first name required message', async () => {
-      await setValueByLabelText('First Name', '');
-      expectLengthByRole('alert', 1);
-      formAlertMessageCheck(FIRST_NAME_REQUIRED);
-    });
-
-    it.skip('Should only display last name required message', async () => {
-      await setValueByLabelText('Last Name', '');
-      expectLengthByRole('alert', 1);
-      formAlertMessageCheck(LAST_NAME_REQUIRED);
-    });
+    fireEvent.submit(screen.getByRole('button', { name: 'SIGN UP' }));
+    await waitFor(() => expect(mockOnSubmit).toHaveBeenCalled());
   });
 });
