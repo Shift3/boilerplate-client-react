09c20bd2f7ee59817b1edbbcb8fb5ec3ac8c8bdb
Elizabeth Eidelson <fluturecode@gmail.com>
2021-10-23 02:08:44 -0400

    bug(redux-content-error):modified App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,7 +5,7 @@ import { DashboardPage } from 'features/auth/components/dashboardPage';
 import { PrivateRoute } from 'features/auth/components/privateRoute.tsx';
 import { Routes as UserRoutes } from 'features/user-dashboard/Routes';
 import { FC } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from '../GlobalStyle';
 import AppTheme from 'utils/styleValues';
@@ -18,27 +18,37 @@ import { ForgotPasswordPage } from 'features/auth/components/forgotPasswordPage'
 import { ResetPasswordPage } from 'features/auth/components/resetPasswordPage';
 import { ChangePasswordPage } from 'features/auth/components/changePasswordPage';
 import { UpdateUserProfilePage } from 'features/auth/components/updateUserProfilePage';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 export const App: FC = () => (
   <ErrorBoundary>
-    <ThemeProvider theme={AppTheme}>
-      <FlashMessage />
-      <HolyGrailLayout>
-        <Switch>
-          <Route exact path='/auth/login' component={LogInPage} />
-          <Route exact path='/auth/signup' component={SignUpPage} />
-          <Route path='/auth/activate-account/:token' component={ActivateAccountPage} />
-          <Route path='/auth/forgot-password' component={ForgotPasswordPage} />
-          <Route path='/auth/reset-password/:token' component={ResetPasswordPage} />
-          <PrivateRoute exact path='/user/change-password/' component={ChangePasswordPage} />
-          <PrivateRoute exact path='/user/profile/' component={UpdateUserProfilePage} />
-          <PrivateRoute path='/agents' component={AgentRoutes} requiredRoles={['Admin', 'Super Administrator']} />
-          <PrivateRoute path='/agencies' component={AgencyRoutes} requiredRoles={['Admin', 'Super Administrator']} />
-          <PrivateRoute path='/users' component={UserRoutes} requiredRoles={['Admin', 'Super Administrator']} />
-          <PrivateRoute exact path='/' component={DashboardPage} />
-        </Switch>
-      </HolyGrailLayout>
-    </ThemeProvider>
-    <GlobalStyle />
+    <Provider store={store}>
+      <ThemeProvider theme={AppTheme}>
+        <FlashMessage />
+        <HolyGrailLayout>
+          <BrowserRouter>
+            <Switch>
+              <Route exact path='/auth/login' component={LogInPage} />
+              <Route exact path='/auth/signup' component={SignUpPage} />
+              <Route path='/auth/activate-account/:token' component={ActivateAccountPage} />
+              <Route path='/auth/forgot-password' component={ForgotPasswordPage} />
+              <Route path='/auth/reset-password/:token' component={ResetPasswordPage} />
+              <PrivateRoute exact path='/user/change-password/' component={ChangePasswordPage} />
+              <PrivateRoute exact path='/user/profile/' component={UpdateUserProfilePage} />
+              <PrivateRoute path='/agents' component={AgentRoutes} requiredRoles={['Admin', 'Super Administrator']} />
+              <PrivateRoute
+                path='/agencies'
+                component={AgencyRoutes}
+                requiredRoles={['Admin', 'Super Administrator']}
+              />
+              <PrivateRoute path='/users' component={UserRoutes} requiredRoles={['Admin', 'Super Administrator']} />
+              <PrivateRoute exact path='/' component={DashboardPage} />
+            </Switch>
+          </BrowserRouter>
+        </HolyGrailLayout>
+      </ThemeProvider>
+      <GlobalStyle />
+    </Provider>
   </ErrorBoundary>
 );
