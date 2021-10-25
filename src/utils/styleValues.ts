709b7e2761150897a6faa2424fb7f070dfb923d6
Elizabeth Eidelson <fluturecode@gmail.com>
2021-10-25 18:20:21 -0400

    feat(style-constants):modified src/utils/styleValues.ts

--- src/utils/styleValues.ts
+++ src/utils/styleValues.ts
@@ -12,19 +12,6 @@ const colors = {
 };
 
 export default {
-  adminBackground: '#f1f0eb',
-  authBackground: '#ecf6f9',
-  contentBackground: '#f1f0eb',
-  userBackground: '#f1f0eb',
-  primary: '#175f6e',
-  primaryBorder: '#66bbcd',
-  accent: '#d44325',
-  accentBorder: '#d44325',
-  accentDisabled: '#e78b78',
-  cardHeader: '#c5e6ec',
-  navBackground: '#ffffff',
-  navLink: '#2B373B',
-
   app: {
     backgroundColor: colors.white,
   },
