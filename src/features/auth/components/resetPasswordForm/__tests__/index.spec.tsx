6333458ae731942a70dd072c9a2a424ea77b3686
Elizabeth Eidelson <fluturecode@gmail.com>
2021-10-27 17:25:19 -0400

    feat(changePasswordForm-tset):reverted /ResetPasswordForm/__tests__/index.spec.tsx;

--- src/features/auth/components/resetPasswordForm/__tests__/index.spec.tsx
+++ src/features/auth/components/resetPasswordForm/__tests__/index.spec.tsx
@@ -46,7 +46,7 @@ describe('ResetPasswordForm', () => {
     userEvent.type(newPasswordInput, '123');
 
     const confirmNewPasswordInput = screen.getByLabelText(/Confirm Password/i);
-    userEvent.type(confirmNewPasswordInput, '123');
+    userEvent.type(confirmNewPasswordInput, 'abc');
 
     fireEvent.click(screen.getByRole('button', { name: 'SUBMIT' }));
     await waitFor(() => expect(mockOnSubmit).not.toHaveBeenCalledTimes);
