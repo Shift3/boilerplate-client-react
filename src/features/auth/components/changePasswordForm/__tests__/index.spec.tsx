63ba6b67a4fc81fd6183f21774c935ee4874e9c9
Elizabeth Eidelson <fluturecode@gmail.com>
2021-10-28 01:17:31 -0400

    feat(changePasswordForm-test): modified src/features/auth/components/changePasswordForm/__tests__/index.spec.tsx

--- src/features/auth/components/changePasswordForm/__tests__/index.spec.tsx
+++ src/features/auth/components/changePasswordForm/__tests__/index.spec.tsx
@@ -1,4 +1,4 @@
-import { act, fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { ChangePasswordForm } from '../index';
 import { ThemeProvider } from 'styled-components';
