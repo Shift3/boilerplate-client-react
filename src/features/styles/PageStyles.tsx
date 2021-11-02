13255e0dcff86372b7f4a4b0a5cd4afd8ce40329
Elizabeth Eidelson <fluturecode@gmail.com>
2021-11-02 15:47:26 -0400

    feat(validation-errors): modified ./src/features/styles/PageStyles.tsx

--- src/features/styles/PageStyles.tsx
+++ src/features/styles/PageStyles.tsx
@@ -25,10 +25,23 @@ export const StyledFormWrapper = styled.div`
     color: ${(props) => props.theme.forms.labelColor};
     margin: 5px;
   }
-  & .is-invalid {
+  & .form-control.is-invalid,
+  .form-select.is-invalid {
     border-color: ${(props) => props.theme.forms.errorBorderColor};
+  }
+
+  & .form-control.is-invalid,
+  .was-validated .form-control:invalid {
     background-image: none;
   }
+
+  & .form-select.is-invalid:not([multiple]):not([size]),
+  .form-select.is-invalid:not([multiple])[size='1'],
+  .was-validated .form-select:invalid:not([multiple]):not([size]),
+  .was-validated .form-select:invalid:not([multiple])[size='1'] {
+    background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3e%3cpath fill='none' stroke='%23343a40' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M2 5l6 6 6-6'/%3e%3c/svg%3e");
+  }
+
   & .invalid-feedback {
     /* Modify style of validation error messages here */
     color: ${(props) => props.theme.forms.errorTextColor};
