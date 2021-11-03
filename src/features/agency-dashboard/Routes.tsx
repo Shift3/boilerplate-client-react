8d87de2bf9fbc771ddfd9ceb64586aa4b032a8ea
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-11-03 16:52:40 -0700

    feat(app): add a 404 not found page

--- src/features/agency-dashboard/Routes.tsx
+++ src/features/agency-dashboard/Routes.tsx
@@ -1,3 +1,4 @@
+import { NotFoundView } from 'common/components/NotFound';
 import { FC } from 'react';
 import { Route, Switch, useRouteMatch } from 'react-router-dom';
 import { AgencyListView, CreateAgencyView, UpdateAgencyView } from './pages';
@@ -6,9 +7,10 @@ export const Routes: FC = () => {
   const { path } = useRouteMatch();
   return (
     <Switch>
-      <Route exact path={`${path}`} component={AgencyListView} />
-      <Route path={`${path}/create-agency`} component={CreateAgencyView} />
-      <Route path={`${path}/update-agency/:id`} component={UpdateAgencyView} />
+      <Route exact path={path} component={AgencyListView} />
+      <Route exact path={`${path}/create-agency`} component={CreateAgencyView} />
+      <Route exact path={`${path}/update-agency/:id`} component={UpdateAgencyView} />
+      <Route path={path} component={NotFoundView} />
     </Switch>
   );
 };
