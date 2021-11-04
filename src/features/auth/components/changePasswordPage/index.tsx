b246139d6633621353d848618882fe7d97870913
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-11-04 11:02:58 -0700

    fix(ChangePasswordPage): make actual api request

--- src/features/auth/components/changePasswordPage/index.tsx
+++ src/features/auth/components/changePasswordPage/index.tsx
@@ -1,3 +1,6 @@
+import { ChangePasswordRequest, useChangePasswordMutation } from 'common/api/userApi';
+import * as notificationService from 'common/services/notification';
+import { useAuth } from 'features/auth/hooks';
 import { PageWrapper, StyledFormWrapper, Title } from 'features/styles/PageStyles';
 import { FC } from 'react';
 import { useHistory } from 'react-router-dom';
@@ -6,11 +9,22 @@ import { IChangePasswordFormData } from '../changePasswordForm/types';
 
 export const ChangePasswordPage: FC = () => {
   const history = useHistory();
+  const { user } = useAuth();
+  const [changePassword] = useChangePasswordMutation();
 
   //  eslint-disable-next-line
-  const onSubmit = (formData: IChangePasswordFormData) => {
-    // TODO: make API call to handle success and error cases.
-    history.push('/');
+  const onSubmit = async (formData: IChangePasswordFormData) => {
+    const { currentPassword, newPassword, confirmPassword } = formData;
+    const request: ChangePasswordRequest = { id: user!.id, oldPassword: currentPassword, newPassword, confirmPassword };
+
+    try {
+      await changePassword(request).unwrap();
+      notificationService.showSuccessMessage('Password updated.');
+    } catch (error) {
+      notificationService.showErrorMessage(error.data.message);
+    }
+
+    history.push('/agents');
   };
 
   return (
