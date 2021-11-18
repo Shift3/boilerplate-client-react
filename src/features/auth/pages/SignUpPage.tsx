d54ae590d30e1c0b0690ff98c75612e7e67984da
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-11-18 09:55:20 -0800

    refactor(SignUpPage): move page component to auth/pages

---
+++ src/features/auth/pages/SignUpPage.tsx
@@ -0,0 +1,34 @@
+import { FC } from 'react';
+import { useHistory } from 'react-router-dom';
+import { FormData, SignUpForm } from '../components/SignUpForm';
+import { PageWrapper, StyledFormWrapper, Title } from 'features/styles/PageStyles';
+import { useSignUpMutation } from 'common/api/userApi';
+import { showSuccessMessage } from 'common/services/notification';
+import { handleApiError } from 'common/api/handleApiError';
+import { FetchBaseQueryError } from '@reduxjs/toolkit/dist/query';
+
+export const SignUpPage: FC = () => {
+  const history = useHistory();
+  const [signUp] = useSignUpMutation();
+
+  const onSubmit = async (data: FormData) => {
+    try {
+      await signUp(data).unwrap();
+      showSuccessMessage(`An activation email has been sent to ${data.email}.`);
+      history.push('/auth/login');
+    } catch (error) {
+      handleApiError(error as FetchBaseQueryError);
+    }
+  };
+
+  const onCancel = () => history.push('/auth/login');
+
+  return (
+    <PageWrapper>
+      <StyledFormWrapper>
+        <Title>Sign Up</Title>
+        <SignUpForm onSubmit={onSubmit} onCancel={onCancel} />
+      </StyledFormWrapper>
+    </PageWrapper>
+  );
+};
