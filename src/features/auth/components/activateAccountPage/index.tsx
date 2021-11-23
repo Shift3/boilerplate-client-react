8c7509b803671cf9a72f623b552fb8b9dacbcfae
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-11-22 23:52:05 -0800

    refactor(ActivateAccountPage): use new mutations from userApi

--- src/features/auth/components/activateAccountPage/index.tsx
+++ src/features/auth/components/activateAccountPage/index.tsx
@@ -1,19 +1,28 @@
 import { FC } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
+import { FetchBaseQueryError } from '@reduxjs/toolkit/dist/query';
 import { IResetPasswordFormData } from '../resetPasswordForm/types';
 import { PageWrapper, StyledFormWrapper, Title } from 'features/styles/PageStyles';
-import { useAccountCreation } from 'core/modules/user/application/useAccountCreation';
 import { ActivateAccountForm } from 'features/auth/components/activateAccountForm';
+import { useActivateAccountMutation } from 'common/api/userApi';
+import { handleApiError } from 'common/api/handleApiError';
+import * as notificationService from 'common/services/notification';
 
 export const ActivateAccountPage: FC = () => {
   const history = useHistory();
   const { token } = useParams<{ token: string }>();
-  const { activateAccount } = useAccountCreation();
+  const [activateAccount] = useActivateAccountMutation();
 
   const onSubmit = async (formData: IResetPasswordFormData) => {
     const data = { ...formData, token };
-    const onSuccess = () => history.push('/auth/login');
-    await activateAccount(data, onSuccess);
+
+    try {
+      await activateAccount(data);
+      notificationService.showSuccessMessage('This account has been activated. Please log in.');
+      history.push('/auth/login');
+    } catch (error) {
+      handleApiError(error as FetchBaseQueryError);
+    }
   };
 
   const onCancel = () => history.push('/auth/login');
