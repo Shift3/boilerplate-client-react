535bc69ef4c6bf46e7c7913f9eaf16cadbeb67f1
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-11-22 16:34:36 -0800

    fix(App): redirect root path / to /agents

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
 import { ErrorBoundary } from '@sentry/react';
 import { Routes as AgencyRoutes } from 'features/agency-dashboard/Routes';
 import { Routes as AgentRoutes } from 'features/agent-dashboard/Routes';
-import { DashboardPage } from 'features/auth/components/dashboardPage';
 import { PrivateRoute } from 'features/auth/components/privateRoute.tsx';
 import { Routes as UserRoutes } from 'features/user-dashboard/Routes';
 import { FC } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from '../GlobalStyle';
 import AppTheme from 'utils/styleValues';
@@ -34,8 +33,10 @@ export const App: FC = () => (
           <PrivateRoute path='/agents' component={AgentRoutes} />
           <PrivateRoute path='/agencies' component={AgencyRoutes} requiredRoles={['Super Administrator']} />
           <PrivateRoute path='/users' component={UserRoutes} requiredRoles={['Admin', 'Super Administrator']} />
-          <PrivateRoute exact path='/' component={DashboardPage} />
-          <Route path='/' component={NotFoundView} />
+          <PrivateRoute exact path='/'>
+            <Redirect to='/agents' />
+          </PrivateRoute>
+          <Route component={NotFoundView} />
         </Switch>
       </HolyGrailLayout>
     </ThemeProvider>
