d835b18da59f4f717311cc875de0f26f4c6ec118
Marco Chavez <marcode.chavez.jr@gmail.com>
2021-12-06 12:12:09 -0800

    feat(FormPrompt): Creating Form Prompt

---
+++ src/common/components/FormPrompt/FormPrompt.tsx
@@ -0,0 +1,17 @@
+import { Prompt } from 'react-router';
+import { Control, useFormState } from 'react-hook-form';
+import { FormData } from '../../../features/agent-dashboard/components/AgentDetailForm';
+
+type FormPromptProps = {
+    control: Control<FormData> | Control<Partial<FormData>>;
+}
+
+export const FormPrompt = ({control}: FormPromptProps): JSX.Element => {
+    const { isDirty } = useFormState({control});
+
+    return(
+        <>
+            <Prompt when={isDirty} message="There are unsaved changes, would you still like to leave?" />
+        </>
+    )
+}
