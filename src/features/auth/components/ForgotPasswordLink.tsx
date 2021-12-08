e818d07ef31bfed30172152ac4d1698c53773b86
fluturecode <fluturecode@gmail.com>
2021-12-08 13:15:09 -0500

    chore(remove-cancelButton):modified:   src/features/auth/components/ForgotPasswordLink.tsx

--- src/features/auth/components/ForgotPasswordLink.tsx
+++ src/features/auth/components/ForgotPasswordLink.tsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const ForgotPassword = styled.div`
+  display: flex;
+  justify-content: center;
   font-size: 1.2em;
   padding-top: 18px;
 `;
