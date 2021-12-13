70fb76028d5220ff6e439cfa88fb7451c2600255
Tony Medel <tonymedel95@gmail.com>
2021-12-13 15:08:21 -0800

    refactor(phone-input): trim() since isn't necessary

--- src/common/components/PhoneInput/index.tsx
+++ src/common/components/PhoneInput/index.tsx
@@ -5,15 +5,15 @@ const formatPhoneNumber = (value: string): string => {
     value = value.replace(/\D/g, '');
 
     if (value.length < 4 && value.length >= 1) {
-      return `${value.substring(0, 3)}`.trim();
+      return `${value.substring(0, 3)}`;
     }
   
     if (value.length < 7 && value.length >= 4){
-      return `(${value.substring(0, 3)}) ${value.substring(3, 6)}`.trim();
+      return `(${value.substring(0, 3)}) ${value.substring(3, 6)}`;
     }
   
     if (value.length >= 7) {
-      return `(${value.substring(0, 3)}) ${value.substring(3, 6)}-${value.substring(6,value.length)}`.trim();
+      return `(${value.substring(0, 3)}) ${value.substring(3, 6)}-${value.substring(6,value.length)}`;
     }
   
     return '';
