aaa05c4b1c2bcfd09e52513e004ca46d3ae9ad53
Tony Medel <tonymedel95@gmail.com>
2021-12-16 11:10:40 -0800

    feat(breadcrumb): link to component in crumb

--- src/common/components/Breadcrumb/index.tsx
+++ src/common/components/Breadcrumb/index.tsx
@@ -5,13 +5,29 @@ type Props = {
     path: Array<string>;
 };
 
+interface PathMap {
+    'Home': string, 
+    'Login': string,
+    'Agency List': string,
+    'Agent List': string,
+    'User List': string 
+}
+
+const pathMap = {
+    'Home':'/agents', 
+    'Login':'/auth/login',
+    'Agency List': '/agencies',
+    'Agent List': '/agents',
+    'User List': '/users'
+};
+
 function createCrumbs(Path: Array<string>){
     const crumbs = Path.map((element, i, {length}) => {
         if (length-1 === i) {
             return <Breadcrumb.Item active>{element}</Breadcrumb.Item>
         }
 
-        return <Breadcrumb.Item href="#">{element}</Breadcrumb.Item>
+        return <Breadcrumb.Item href={pathMap[element as keyof PathMap]}>{element}</Breadcrumb.Item>
     });
     return crumbs;
 }
