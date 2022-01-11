91601a9eef438513e743a5e3a38d9cae35234167
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2022-01-11 10:38:47 -0800

    feat(UserListView): wrap query hook in usePageableQuery

--- src/features/user-dashboard/pages/UserListView.tsx
+++ src/features/user-dashboard/pages/UserListView.tsx
@@ -15,6 +15,7 @@ import { HasPermission, useRbac } from 'features/rbac';
 import * as notificationService from 'common/services/notification';
 import { CreateButton } from 'common/styles/button';
 import { useConfirmationModal } from 'features/confirmation-modal';
+import { usePageableQuery } from 'common/api/paginate';
 
 type UserTableItem = {
   id: number;
@@ -29,7 +30,11 @@ type UserTableItem = {
 export const UserListView: FC = () => {
   const history = useHistory();
   const { userHasPermission } = useRbac();
-  const { data: users = [], isLoading: isLoadingUsers, isFetching: isFetchingUsers } = useGetUsersQuery();
+  const {
+    data: users = [],
+    isLoading: isLoadingUsers,
+    isFetching: isFetchingUsers,
+  } = usePageableQuery<User, 'User'>(useGetUsersQuery);
   const [deleteUser] = useDeleteUserMutation();
   const [forgotPassword] = useForgotPasswordMutation();
   const [resendActivationEmail] = useResendActivationEmailMutation();
