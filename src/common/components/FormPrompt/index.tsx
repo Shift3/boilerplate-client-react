0ac8a13f707b537d8ee815659e1ce3a87a208c1e
Marco Chavez <marcode.chavez.jr@gmail.com>
2022-01-11 17:22:41 -0800

    refactor(FormPrompt): removing extra files

--- src/common/components/FormPrompt/index.tsx
+++ src/common/components/FormPrompt/index.tsx
@@ -1,3 +1,15 @@
-import { FormPrompt } from './FormPrompt';
+import { Prompt } from "react-router-dom"
+
+type PromptProps = {
+    isDirty: boolean;
+    isSubmitting: boolean;
+}
+
+const FormPrompt = ({ isDirty, isSubmitting }: PromptProps): JSX.Element => {
+
+    return (
+        <Prompt when={isDirty && !isSubmitting} message="You have unsaved changes, are you sure you want to leave?" />
+    )
+}
 
 export default FormPrompt;
