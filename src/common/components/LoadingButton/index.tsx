79eab81a15a68a67b57139e5de266eee507ce857
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2022-01-20 11:44:27 -0800

    feat(loading-button): add back type and width

--- src/common/components/LoadingButton/index.tsx
+++ src/common/components/LoadingButton/index.tsx
@@ -5,24 +5,34 @@ import styled from 'styled-components';
 const BootstrapButton: FC<ButtonProps> = ({ children, ...rest }) => <Button {...rest}>{children}</Button>;
 
 type LoadingButtonProps = ButtonProps & { loading: boolean };
+// type LoadingButtonProps = ButtonProps & { loading: boolean; as: FC<ButtonProps> };
 
 const StyledButton = styled(BootstrapButton)`
   color: ${props => props.theme.buttons.submitTextColor};
   background-color: ${props => props.theme.buttons.submitBackgroundColor};
   border-color: ${props => props.theme.buttons.submitBorderColor};
   padding: 0.5rem 1rem;
-  width: 40%;
+  // width: 40%;
 
   & .spinner-border {
     margin-right: 1em;
   }
 `;
 
-export const LoadingButton: FC<LoadingButtonProps> = ({ onClick, disabled, loading, children,  }) => {
+export const LoadingButton: FC<LoadingButtonProps> = ({ onClick, disabled, loading, children }) => {
   return (
-    <StyledButton disabled={disabled || loading} onClick={onClick}>
+    <StyledButton type='submit' disabled={disabled || loading} onClick={onClick}>
       {loading && <span className='spinner-border spinner-border-sm' />}
       {children}
     </StyledButton>
   );
 };
+
+// export const LoadingButton: FC<LoadingButtonProps> = ({ onClick, disabled, loading, as: AsButton children }) => {
+//   return (
+//     <AsButton disabled={disabled || loading} onClick={onClick}>
+//       {loading && <span className='spinner-border spinner-border-sm' />}
+//       {children}
+//     </AsButton>
+//   );
+// };
