b678a7b6780f5bb1bb7f0b0cebea9aeb1a7b47d1
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2022-01-21 13:49:24 -0800

    fix(useConfirmationModal): change property name in config type to match property name in redux slice state

--- src/features/confirmation-modal/useConfirmationModal.ts
+++ src/features/confirmation-modal/useConfirmationModal.ts
@@ -1,12 +1,17 @@
 import { useAppDispatch } from 'app/redux';
-import { ConfirmationModalCallback, confirmConfirmationModal, declineConfirmationModal, openConfirmationModal } from './slice';
+import {
+  ConfirmationModalCallback,
+  confirmConfirmationModal,
+  declineConfirmationModal,
+  openConfirmationModal,
+} from './slice';
 
 export interface ConfirmationModalConfig {
-    message?: string;
-    cancelButtonLabel?: string;
-    confirmButtonLabel?: string; 
-    onConfirm?: ConfirmationModalCallback;
-    onDecline?: ConfirmationModalCallback;
+  message?: string;
+  declineButtonLabel?: string;
+  confirmButtonLabel?: string;
+  onConfirm?: ConfirmationModalCallback;
+  onDecline?: ConfirmationModalCallback;
 }
 
 export interface ConfirmationModalManager {
@@ -18,23 +23,23 @@ export interface ConfirmationModalManager {
 export type useConfirmationModal = () => ConfirmationModalManager;
 
 export const useConfirmationModal: useConfirmationModal = () => {
-    const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
 
-    const openModal = (config: ConfirmationModalConfig) => {
-        dispatch(openConfirmationModal(config));
-    }
+  const openModal = (config: ConfirmationModalConfig) => {
+    dispatch(openConfirmationModal(config));
+  };
 
-    const confirmModal = () => {
-        dispatch(confirmConfirmationModal());
-    }
+  const confirmModal = () => {
+    dispatch(confirmConfirmationModal());
+  };
 
-    const declineModal = () => {
-        dispatch(declineConfirmationModal());
-    }
+  const declineModal = () => {
+    dispatch(declineConfirmationModal());
+  };
 
-    return {
-        openModal,
-        confirmModal,
-        declineModal
-    }
-}
+  return {
+    openModal,
+    confirmModal,
+    declineModal,
+  };
+};
