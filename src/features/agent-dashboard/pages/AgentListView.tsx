4ec86b4ad18904ed2562181667ba7f6ca3e27c48
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2022-01-27 11:53:18 -0800

    refactor(AgentListView): move logic of creating table data into custom hook

--- src/features/agent-dashboard/pages/AgentListView.tsx
+++ src/features/agent-dashboard/pages/AgentListView.tsx
@@ -1,100 +1,21 @@
-import ActionButton, { ActionButtonProps } from 'common/components/ActionButton';
 import { Agent, PaginatedResult } from 'common/models';
-import { FC, useCallback, useMemo } from 'react';
+import { FC, useMemo } from 'react';
 import Container from 'react-bootstrap/Container';
 import { Link, useHistory } from 'react-router-dom';
 import { WithLoadingOverlay } from 'common/components/LoadingSpinner';
-import { HasPermission, useRbac } from 'features/rbac';
-import { useDeleteAgentMutation, useGetAgentsQuery } from 'common/api/agentApi';
-import * as notificationService from 'common/services/notification';
+import { HasPermission } from 'features/rbac';
+import { useGetAgentsQuery } from 'common/api/agentApi';
 import { CreateButton } from 'common/styles/button';
-import { useConfirmationModal } from 'features/confirmation-modal';
-import { Column } from 'react-table';
 import { DataTable } from 'common/components/DataTable';
 import { usePSFQuery } from 'common/hooks';
-
-type AgentTableItem = {
-  id: number;
-  name: string;
-  description: string;
-  email: string;
-  phoneNumber: string;
-  actions: ActionButtonProps[];
-};
+import { AgentTableItem, useAgentTableData } from '../hooks/useAgentTableData';
 
 export const AgentListView: FC = () => {
-  const history = useHistory();
-  const { userHasPermission } = useRbac();
   const { data, isLoading, page, pageSize, getPage, changePageSize } =
     usePSFQuery<PaginatedResult<Agent>>(useGetAgentsQuery);
-  const [deleteAgent] = useDeleteAgentMutation();
-  const { openModal } = useConfirmationModal();
   const agents = useMemo(() => data?.results ?? [], [data]);
+  const { columns, data: tableData } = useAgentTableData(agents);
   const isPageLoading = isLoading;
-
-  const navigateToUpdateView = useCallback(
-    (agent: Agent) => {
-      history.push(`/agents/update-agent/${agent.id}`);
-    },
-    [history],
-  );
-
-  const handleDelete = useCallback(
-    (agent: Agent) => {
-      const message = `Delete ${agent.name}?`;
-
-      const onConfirm = async () => {
-        await deleteAgent(agent.id);
-        notificationService.showSuccessMessage('Agent deleted.');
-      };
-
-      openModal({ message, confirmButtonLabel: 'DELETE', onConfirm });
-    },
-    [openModal, deleteAgent],
-  );
-
-  // Set up columns and headers
-  const columns: Column<AgentTableItem>[] = useMemo(
-    () => [
-      { accessor: 'name', Header: 'Agent Name' },
-      { accessor: 'description', Header: 'Description' },
-      { accessor: 'email', Header: 'Email' },
-      { accessor: 'phoneNumber', Header: 'Phone Number' },
-      {
-        accessor: 'actions',
-        Header: 'Action',
-        Cell: ({ value, row }) => value.map(props => <ActionButton key={`${row.id}-${props.icon}`} {...props} />),
-      },
-    ],
-    [],
-  );
-
-  // Transform Agent objects returned from the API into the table item data format expected by the table.
-  const items: AgentTableItem[] = useMemo(
-    () =>
-      agents.map(agent => ({
-        id: agent.id,
-        name: agent.name,
-        description: agent.description,
-        email: agent.email,
-        phoneNumber: agent.phoneNumber,
-        actions: [
-          {
-            icon: 'edit',
-            tooltipText: 'Edit',
-            onClick: () => navigateToUpdateView(agent),
-            show: userHasPermission({ permission: 'agent:update', data: agent }),
-          },
-          {
-            icon: 'trash-alt',
-            tooltipText: 'Delete',
-            onClick: () => handleDelete(agent),
-            show: userHasPermission({ permission: 'agent:delete', data: agent }),
-          },
-        ],
-      })),
-    [agents, userHasPermission, handleDelete, navigateToUpdateView],
-  );
 
   return (
     <Container>
@@ -108,7 +29,7 @@ export const AgentListView: FC = () => {
       <WithLoadingOverlay isLoading={isPageLoading}>
         <DataTable<AgentTableItem>
           columns={columns}
-          data={items}
+          data={tableData}
           pagination={{
             page,
             pageSize,
