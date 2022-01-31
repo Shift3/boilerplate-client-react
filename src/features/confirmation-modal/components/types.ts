76cb0143bf2d63524dfdf6968580466d276cc90a
fluturecode <fluturecode@gmail.com>
2022-01-31 12:47:21 -0500

    feat(delete-spinner):removed duplicate files remaining after rebase

--- src/features/confirmation-modal/components/types.ts
+++
@@ -1,9 +0,0 @@
-export type ConfirmationModalProps = {
-  show: boolean;
-  message: string;
-  cancelButtonLabel?: string;
-  confirmButtonLabel?: string;
-  onCancel: VoidFunction;
-  onConfirm: VoidFunction;
-  loading?: boolean;
-};
