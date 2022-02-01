0e1c913be510f156f777187c29fbea1594f684f5
Pratima Sakinala <pratimasakinala13@gmail.com>
2022-01-31 16:45:16 -0800

    feat(user-factory,test): uses a transient param to determine whether or not to add a `newEmail` property to the user factory. creates a test file for the `UpdateUserEmailForm` component.

--- src/common/models/testing-factories/user-factory.ts
+++ src/common/models/testing-factories/user-factory.ts
@@ -4,14 +4,22 @@ import { User } from '../user';
 import { AgencyFactory } from './agency-factory';
 import { RoleFactory } from './role-factory';
 
-export const UserFactory = Factory.define<User>(({ sequence, associations }) => ({
-  id: sequence,
-  email: Faker.internet.email(),
-  activatedAt: Faker.date.recent().toISOString(),
-  firstName: Faker.name.firstName(),
-  lastName: Faker.name.lastName(),
-  profilePicture: Faker.internet.avatar(),
-  agency: associations.agency ?? AgencyFactory.build(),
-  role: associations.role ?? RoleFactory.build(),
-  newEmail: Faker.internet.email(),
-}));
+type UserTransientParams = {
+  hasNewEmail?: boolean;
+}
+
+export const UserFactory = Factory.define<User, UserTransientParams>(({ transientParams, sequence, associations }) => {
+  const { hasNewEmail } = transientParams;
+
+  return {
+    id: sequence,
+    email: Faker.internet.email(),
+    activatedAt: Faker.date.recent().toISOString(),
+    firstName: Faker.name.firstName(),
+    lastName: Faker.name.lastName(),
+    profilePicture: Faker.internet.avatar(),
+    agency: associations.agency ?? AgencyFactory.build(),
+    role: associations.role ?? RoleFactory.build(),
+    newEmail: hasNewEmail ? Faker.internet.email() : null,
+  }
+});
