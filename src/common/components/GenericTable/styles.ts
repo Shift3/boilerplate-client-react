29608813a6b554de2d99ab65b8594435c8e54f18
Justin Schiff <AmbientRevolution@gmail.com>
2022-02-09 08:54:24 -0800

    Remove generic table.

--- src/common/components/GenericTable/styles.ts
+++
@@ -1,5 +0,0 @@
-import Table from 'react-bootstrap/Table';
-import styled from 'styled-components';
-
-export const StyledTable = styled(Table)`
-`;
--- src/common/components/index.ts
+++ src/common/components/index.ts
@@ -1,3 +1,2 @@
 export * from './ActionButton';
 export * from './CustomSelect';
-export * from './GenericTable';
