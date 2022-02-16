b0acacd7535357554e1907d56b476cd48e7bde35
Justin <AmbientRevolution@gmail.com>
2022-02-16 12:06:46 -0800

    Add Production Deploys (#527)
    Automatic deployments, now deploys to production infrastructure from `main` branch.

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -2,7 +2,7 @@ import { name, version } from '../../package.json';
 import { EnvironmentConfiguration, IEnvironment } from './types';
 
 export const environment: IEnvironment = {
-  apiRoute: 'http://localhost:3000',
+  apiRoute: 'https://boilerplate-prod.shift3sandbox.com',
   environment: EnvironmentConfiguration.Production,
   isProduction: process.env.NODE_ENV === 'production',
   name,
