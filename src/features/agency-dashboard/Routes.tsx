27e0e25d7ea9e3fb6fe1685fda7d054ffb1543e2
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2022-02-23 12:40:47 -0800

    fix(app): fix error notification when logged in as `User` role

--- src/features/agency-dashboard/Routes.tsx
+++ src/features/agency-dashboard/Routes.tsx
@@ -2,15 +2,18 @@ import { NotFoundView } from 'common/components/NotFound';
 import { FC } from 'react';
 import { Route, Switch, useRouteMatch } from 'react-router-dom';
 import { AgencyListView, CreateAgencyView, UpdateAgencyView } from './pages';
+import { HolyGrailLayout } from 'common/components/HolyGrailLayout';
 
 export const Routes: FC = () => {
   const { path } = useRouteMatch();
   return (
-    <Switch>
-      <Route exact path={path} component={AgencyListView} />
-      <Route exact path={`${path}/create-agency`} component={CreateAgencyView} />
-      <Route exact path={`${path}/update-agency/:id`} component={UpdateAgencyView} />
-      <Route path={path} component={NotFoundView} />
-    </Switch>
+    <HolyGrailLayout>
+      <Switch>
+        <Route exact path={path} component={AgencyListView} />
+        <Route exact path={`${path}/create-agency`} component={CreateAgencyView} />
+        <Route exact path={`${path}/update-agency/:id`} component={UpdateAgencyView} />
+        <Route path={path} component={NotFoundView} />
+      </Switch>
+    </HolyGrailLayout>
   );
 };
