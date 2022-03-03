41f6a02987cd1ede7de741edd1c56ce1ed799444
Joshwin Greene <joshwin.greene@gmail.com>
2022-03-02 16:38:40 -0800

    fix(DimmableContent): Stops the flicker in the corners of the TableCard from showing up when the overlay hasn't disappeared

--- src/common/styles/utilities.tsx
+++ src/common/styles/utilities.tsx
@@ -43,10 +43,10 @@ export const DimmableContent = styled.div<{
         &:after {
           content: '';
           position: absolute;
-          left: 0;
-          right: 0;
-          top: 0;
-          bottom: 0;
+          left: 5;
+          right: 5;
+          top: 5;
+          bottom: 5;
           background: white;
           opacity: 0.5;
         }
