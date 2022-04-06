5886f26a5ab6d74cb379436d14079de114ab8db2
Joshwin Greene <joshwin.greene@gmail.com>
2022-04-06 15:56:44 -0700

    feat(user-dashboard, Routes): Changed how the HolyGrailLayout was used in order to be able to pass content to its rightAside prop

--- src/features/user-dashboard/Routes.tsx
+++ src/features/user-dashboard/Routes.tsx
@@ -1,16 +1,18 @@
 import { NotFoundView } from 'common/components/NotFound';
-import { FC } from 'react';
+import { FC, ReactNode } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { CreateUserView, UpdateUserView, UserListView } from './pages';
 import { HolyGrailLayout } from 'common/components/HolyGrailLayout';
 
+const encloseWithHolyGrailLayout = (child: ReactNode, rightAside?: ReactNode) => {
+  return <HolyGrailLayout rightAside={rightAside}>{child}</HolyGrailLayout>;
+};
+
 export const UserRoutes: FC = () => (
-  <HolyGrailLayout>
-    <Routes>
-      <Route path='/' element={<UserListView />} />
-      <Route path='/create-user' element={<CreateUserView />} />
-      <Route path='/update-user/:id' element={<UpdateUserView />} />
-      <Route path='*' element={<NotFoundView />} />
-    </Routes>
-  </HolyGrailLayout>
+  <Routes>
+    <Route path='/' element={encloseWithHolyGrailLayout(<UserListView />, <p>Right Side</p>)} />
+    <Route path='/create-user' element={encloseWithHolyGrailLayout(<CreateUserView />)} />
+    <Route path='/update-user/:id' element={encloseWithHolyGrailLayout(<UpdateUserView />)} />
+    <Route path='*' element={encloseWithHolyGrailLayout(<NotFoundView />)} />
+  </Routes>
 );
