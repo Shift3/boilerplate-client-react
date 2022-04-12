4bafc8eae4126c618355a146ba0b06f209a94951
Jose Gustavo Hernandez <jgustavoht@gmail.com>
2022-04-12 10:14:34 -0700

    Refactors s3 resource creation syntax that is needed for 4.8.0 terraform version ( for m1 chips) (#545)
    * refactor(main.tf): adds 4.8.0 version
    
    * refactor(main.tf): Adds cloudfront s3 bucket creation to handle terraform version 4.8.0
    
    * refactor(variables.tf): adds variables to be usable with main.tf inside cloudfront

--- terraform/cloudfront/variables.tf
+++ terraform/cloudfront/variables.tf
@@ -2,16 +2,26 @@ variable "assume_role_arn" {
   description = "Assume Role ARN"
 }
 
+variable "environment" {
+  description = "This value will be used to tag the resources created with the environment (staging | production | dev)"
+  default     = "staging"
+}
+
 variable "profile" {
   description = "Name of your profile inside ~/.aws/credentials"
   default     = "shift3"
 }
 
-variable "region" {
-  default     = "us-west-2"
-  description = "Defines where your app should be deployed"
-}
-
 variable "web_domain_name" {
   description = "Domain name for the s3 bucket"
+}
+
+variable "secure_response_headers_id"{
+  description = "Default security header policy ID (shift3 account)"
+  default = "b52c8245-8965-4882-a446-8773a50b46ab"
+}
+
+variable "route53_zone" {
+  description = "Zone for Route53"
+  default = "shift3sandbox.com."
 }
