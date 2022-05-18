fa8d606842c5fb71562420e9fe8f2a4c001c7163
Justin Schiff <AmbientRevolution@gmail.com>
2022-05-18 12:09:22 -0700

    Update staging apiRoute

--- src/environment/environment.staging.ts
+++ src/environment/environment.staging.ts
@@ -2,7 +2,7 @@ import packageJson from '../../package.json';
 import { EnvironmentConfiguration, IEnvironment } from './types';
 
 export const environment: IEnvironment = {
-  apiRoute: 'https://dj-starter-staging.shift3sandbox.com',
+  apiRoute: 'https://dj-starter-demo-staging.shift3sandbox.com',
   environment: EnvironmentConfiguration.Staging,
   isProduction: process.env.NODE_ENV === 'production',
   name: packageJson.name,
