e7693744aa41cfd8490ab6fd7c11fc0f8f4eba43
Justin Schiff <AmbientRevolution@gmail.com>
2022-05-19 12:52:59 -0700

    Update production reference

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -2,7 +2,7 @@ import packageJson from '../../package.json';
 import { EnvironmentConfiguration, IEnvironment } from './types';
 
 export const environment: IEnvironment = {
-  apiRoute: 'https://bp-server-production.shift3sandbox.com',
+  apiRoute: 'https://dj-starter-demo-prod.shift3sandbox.com',
   environment: EnvironmentConfiguration.Production,
   isProduction: process.env.NODE_ENV === 'production',
   name: packageJson.name,
