c26fdab33eb8c95ddc445de89abd629feb9ebdc4
Elizabeth Eidelson <fluturecode@gmail.com>
2022-05-23 16:44:10 -0400

    feat(i18n):Added language detection to config.ts

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,12 +1,24 @@
 import i18n from 'i18next';
+import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector';
+import I18NextHttpBackend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
+import en1 from './en/ns1.json';
 
 export const resources = {
-  en: {},
+  en: {
+    en1,
+  },
 } as const;
 
-i18n.use(initReactI18next).init({
-  lng: 'en',
-  ns: ['ns1', 'ns2'],
-  resources,
-});
+i18n
+  .use(I18NextHttpBackend)
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(I18nextBrowserLanguageDetector)
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
+  .init({
+    lng: 'en',
+    fallbackLng: 'en',
+    debug: true,
+  });
