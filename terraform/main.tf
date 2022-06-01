6bbd79ff633bd891298d61bf4180cff196536e26
alexrdrgz <alejandro.xsc@gmail.com>
2022-06-01 13:43:35 -0700

    refact(terraform): restructured terraform directory and cloudfront module to point at github module

---
+++ terraform/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  version = "4.8.0"
+  profile = "${var.aws_profile}"
+  region  = "${var.aws_region}"
+}
+
+terraform {
+  backend "s3" {
+    bucket  = "shift3-terraform-state"
+    key     = "boilerplate-client-react/staging/terraform.tfstate"
+    region  = "us-west-2"
+    profile = ""
+  }
+}
+
+module "cloudfront" {
+  source                        = "git@github.com:Shift3/terraform-modules.git//modules/aws/cloudfront"
+  assume_role_arn               = var.aws_assume_role_arn
+  web_domain_name               = var.web_domain_name
+  profile                       = var.aws_profile
+  secure_response_headers_id    = var.secure_response_headers_id
+  route53_zone                  = var.route53_zone
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,9 @@
-variable "assume_role_arn" {
-  default     = "arn:aws:iam::008036621198:role/SuperDevAssumeRole"
-  description = "Assume Role ARN"
-}
-
-variable "profile" {
+variable "aws_profile" {
   default     = "shift3"
   description = "Name of your profile inside ~/.aws/credentials"
 }
 
-variable "region" {
+variable "aws_region" {
   default     = "us-west-2"
   description = "Defines where your app should be deployed"
 }
