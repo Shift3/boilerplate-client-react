50530879e2cc3c35dc2cd3ae384541c727ec0ec2
alexrdrgz <alejandro.xsc@gmail.com>
2022-06-02 14:54:45 -0700

    feat(variables.tf): refactored var values to be located in .tfvars file

--- terraform/environment/test.tfvars
+++ terraform/environment/test.tfvars
@@ -1,1 +1,7 @@
 web_domain_name             = "ar-sandbox.bwtcdevops.com"
+route53_zone                = "bwtcdevops.com"
+secure_response_headers_id  = "2cdf05bf-47ed-4ca3-a2a4-2fd8b2d3a959" 
+aws_profile                 = "devops-sandbox-admin"
+
+
+
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,4 @@
 variable "aws_profile" {
-  default     = "devops-sandbox-admin"
   description = "Name of your profile inside ~/.aws/credentials"
 }
 
@@ -14,10 +13,8 @@ variable "web_domain_name" {
 
 variable "secure_response_headers_id"{
   description = "Default security header policy ID (shift3 account)"
-  default = "2cdf05bf-47ed-4ca3-a2a4-2fd8b2d3a959"
 }
 
 variable "route53_zone" {
   description = "Zone for Route53"
-  default = "bwtcdevops.com."
 }
