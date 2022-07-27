8b1df41babfa94ec96f3454e9b5f77803b82c9d7
Elizabeth Eidelson <fluturecode@gmail.com>
2022-07-27 12:09:23 -0400

    feat(stripe):updated Checkout.tsx

--- src/features/payments/Checkout.tsx
+++ src/features/payments/Checkout.tsx
@@ -1,66 +1,77 @@
 import { useState, useEffect } from 'react';
-
 import { Elements, PaymentElement } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import { Plan, useGetPlansQuery } from 'common/api/paymentsApi';
 import { WithLoadingOverlay } from 'common/components/LoadingSpinner';
 import { Button } from 'react-bootstrap';
+import styled from 'styled-components';
 
-// Make sure to call `loadStripe` outside of a component’s render to avoid
-// recreating the `Stripe` object on every render.
+const PlanContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+const PlanCard = styled.div`
+  background: grey;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  width: 20%;
+  height: 15%;
+  border-radius: 6px;
+`;
+
+const Title = styled.div`
+  display: flex;
+  flex: 1;
+`;
+
+const Description = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const PlanPrice = styled.div``;
+
+const PlanInterval = styled.div``;
+
 const stripePromise = loadStripe(
   'pk_test_51LKnI7LBoYuqAVlJCiBaRj3JGO7ud4yqqxSwwaG94okOq4jB3hUQkEwR9eFJYIEvSWewbK9eZhN95gxiuy7bujHA00c47wfziI',
 );
 
-const ProductDisplay = () => (
-  <section>
-    <div className='product'>
-      <img src='https://i.imgur.com/EHyR2nP.png' alt='The cover of Stubborn Attachments' />
-      <div className='description'>
-        <h3>Stubborn Attachments</h3>
-        <h5>$20.00</h5>
-      </div>
-    </div>
-    <form action='/create-checkout-session' method='POST'>
-      <button type='submit'>Checkout</button>
-    </form>
-  </section>
-);
-
-const Message = ({ message }: any) => (
-  <section>
-    <p>{message}</p>
-  </section>
-);
-
 export const Checkout = () => {
   const { data: plans, isLoading } = useGetPlansQuery();
-  // TODO: When the button is clicked, make a call to
-  // `POST /subscriptions/` with the price_id of the plan
-  // they selected.
-
   // Endpoint will return a `client_secret` that you will use
   // in the `PaymentElement` stripe elements component.
   // https://stripe.com/docs/stripe-js/react
 
+  const handleSubscribe = () => {
+    // TODO: When the button is clicked, make a call to
+    // `POST /subscriptions/` with the price_id of the plan
+    // they selected.
+  };
+
   return (
     <WithLoadingOverlay isLoading={isLoading} containerHasRoundedCorners containerBorderRadius='6px'>
-      <ul>
+      <PlanContainer>
         {plans &&
           plans.map((plan: Plan) => (
-            <>
-              <li>{plan.name}</li>
-              <li>{plan.description}</li>
-              <li>{plan.prices[0].id}</li>
-              <li>${(plan.prices[0].unitAmount / 100).toFixed(2)}</li>
-              <li>
-                per {plan.prices[0].recurring.intervalCount} {plan.prices[0].recurring.interval}
-              </li>
-              <Button>Subscribe to {plan.name}</Button>
-              <br />
-            </>
+            <PlanCard>
+              <Title>{plan.name}</Title>
+              <Description>{plan.description}</Description>
+              <PlanPrice>
+                {plan.prices[0].id}${(plan.prices[0].unitAmount / 100).toFixed(2)}
+              </PlanPrice>
+              per
+              <PlanInterval>
+                {plan.prices[0].recurring.intervalCount} {plan.prices[0].recurring.interval}
+              </PlanInterval>
+              <Button onClick={handleSubscribe}>Subscribe to {plan.name}</Button>
+            </PlanCard>
           ))}
-      </ul>
+      </PlanContainer>
     </WithLoadingOverlay>
   );
   /* const [message, setMessage] = useState('');
