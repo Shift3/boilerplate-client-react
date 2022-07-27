553bab11544bd9ee4a9c2c28b2579c29348a3692
Elizabeth Eidelson <fluturecode@gmail.com>
2022-07-27 13:19:10 -0400

    feat(stripe):Added form component to Checkout page

--- src/features/payments/Checkout.tsx
+++ src/features/payments/Checkout.tsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from 'react';
 import { Elements, PaymentElement } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
+import { Stripe, loadStripe } from '@stripe/stripe-js';
 import { Plan, useGetPlansQuery } from 'common/api/paymentsApi';
 import { WithLoadingOverlay } from 'common/components/LoadingSpinner';
 import { Button } from 'react-bootstrap';
 import styled from 'styled-components';
+import Form from 'react-bootstrap/Form';
 
 const PlanContainer = styled.div`
   display: flex;
@@ -37,20 +38,32 @@ const PlanPrice = styled.div``;
 
 const PlanInterval = styled.div``;
 
-const stripePromise = loadStripe(
-  'pk_test_51LKnI7LBoYuqAVlJCiBaRj3JGO7ud4yqqxSwwaG94okOq4jB3hUQkEwR9eFJYIEvSWewbK9eZhN95gxiuy7bujHA00c47wfziI',
-);
+let stripePromise: Promise<Stripe | null>;
+
+const getStripe = () => {
+  if (!stripePromise) {
+    stripePromise = loadStripe(
+      'pk_test_51LKnI7LBoYuqAVlJCiBaRj3JGO7ud4yqqxSwwaG94okOq4jB3hUQkEwR9eFJYIEvSWewbK9eZhN95gxiuy7bujHA00c47wfziI',
+    );
+  }
+  return stripePromise;
+};
 
 export const Checkout = () => {
   const { data: plans, isLoading } = useGetPlansQuery();
+  const getPlans = useGetPlansQuery();
   // Endpoint will return a `client_secret` that you will use
   // in the `PaymentElement` stripe elements component.
   // https://stripe.com/docs/stripe-js/react
 
-  const handleSubscribe = () => {
+  const handleSubscribe: React.FormEventHandler<HTMLFormElement> = async e => {
+    e.preventDefault();
+    // set isLoading to true;
+
     // TODO: When the button is clicked, make a call to
     // `POST /subscriptions/` with the price_id of the plan
     // they selected.
+    const response = await getPlans;
   };
 
   return (
@@ -59,16 +72,18 @@ export const Checkout = () => {
         {plans &&
           plans.map((plan: Plan) => (
             <PlanCard>
-              <Title>{plan.name}</Title>
-              <Description>{plan.description}</Description>
-              <PlanPrice>
-                {plan.prices[0].id}${(plan.prices[0].unitAmount / 100).toFixed(2)}
-              </PlanPrice>
-              per
-              <PlanInterval>
-                {plan.prices[0].recurring.intervalCount} {plan.prices[0].recurring.interval}
-              </PlanInterval>
-              <Button onClick={handleSubscribe}>Subscribe to {plan.name}</Button>
+              <Form onSubmit={handleSubscribe}>
+                <Title>{plan.name}</Title>
+                <Description>{plan.description}</Description>
+                <PlanPrice>
+                  {plan.prices[0].id}${(plan.prices[0].unitAmount / 100).toFixed(2)}
+                </PlanPrice>
+                per
+                <PlanInterval>
+                  {plan.prices[0].recurring.intervalCount} {plan.prices[0].recurring.interval}
+                </PlanInterval>
+                <Button type='submit'>Subscribe to {plan.name}</Button>
+              </Form>
             </PlanCard>
           ))}
       </PlanContainer>
