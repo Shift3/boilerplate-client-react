f1792d9681835554692af230889075b932fc8e7b
Joshwin Greene <joshwin.greene@gmail.com>
2022-08-03 14:56:27 -0700

    feat(AccountChecker): Initial commit
    This will be used to determine if the user's account has been deleted or frozen.

---
+++ src/features/auth/components/AccountChecker.tsx
@@ -0,0 +1,6 @@
+import { FC, PropsWithChildren, useEffect, useRef, useState } from 'react';
+import * as notificationService from 'common/services/notification';
+
+export const AccountChecker: FC<PropsWithChildren<unknown>> = ({ children }) => {
+  return <>{children}</>;
+};
