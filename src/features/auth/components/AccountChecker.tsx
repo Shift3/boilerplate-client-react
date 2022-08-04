96395fd63d8b9f021de9acdd218f773d73dfc839
Joshwin Greene <joshwin.greene@gmail.com>
2022-08-03 17:17:57 -0700

    feat(AccountChecker): A problem page will now show when there is a problem with the user's account, ex. it gets deleted.

--- src/features/auth/components/AccountChecker.tsx
+++ src/features/auth/components/AccountChecker.tsx
@@ -1,25 +1,27 @@
-import { FC, PropsWithChildren, useEffect, useRef, useState } from 'react';
+import { FC, PropsWithChildren, useState } from 'react';
 import { useGetMeQuery } from 'common/api/userApi';
 import { isFetchBaseQueryError } from 'common/api/handleApiError';
 import { useLogout } from '../hooks';
 import { StatusCodes } from 'http-status-codes';
+import { getAuthState } from '../authLocalStorage';
 
 export const AccountChecker: FC<PropsWithChildren<unknown>> = ({ children }) => {
   const [accountHasIssue, setAccountHasIssue] = useState(false);
-  const { data, error } = useGetMeQuery({}, { pollingInterval: 5000 });
+  const { data, error } = useGetMeQuery({}, { pollingInterval: 5000, skip: accountHasIssue });
 
+  const localAuth = getAuthState();
+
+  console.log('localAuth:', localAuth);
   console.log('data:', data);
   console.log('error:', error);
 
-  useEffect(() => {
-    if (error && accountHasIssue) {
-      if (isFetchBaseQueryError(error)) {
-        if (error.status === StatusCodes.FORBIDDEN) {
-          setAccountHasIssue(true);
-        }
+  if (localAuth && error) {
+    if (isFetchBaseQueryError(error)) {
+      if (error.status === StatusCodes.FORBIDDEN) {
+        setAccountHasIssue(true);
       }
     }
-  }, [error]);
+  }
 
   return !accountHasIssue ? <>{children}</> : <p>Problem Page</p>;
 };
