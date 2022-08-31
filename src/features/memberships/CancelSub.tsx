1434b4c16ba8a0d83322cf327a6a9c076ee2d9e4
Justin Schiff <AmbientRevolution@gmail.com>
2022-08-31 16:35:14 -0700

    Add container

--- src/features/memberships/CancelSub.tsx
+++ src/features/memberships/CancelSub.tsx
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useGetMySubscriptionQuery } from 'common/api/paymentsApi';
 import { useConfirmationModal } from 'features/confirmation-modal';
-import { Button, Card, Col, Row } from 'react-bootstrap';
+import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 import Moment from 'react-moment';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
@@ -82,74 +82,76 @@ export const CancelSub = () => {
   };
 
   return (
-    <Row>
-      <Col className='mb-3' md={12} xl={6}>
-        <Card>
-          <Card.Body>
-            <div className='mb-3 d-flex align-items-center'>
-              <h4 className='flex-fill m-0'>Current Plan</h4>
-              <a className='text-danger' href='#'>
-                Cancel
-              </a>
-            </div>
-            <div className='mb-2'>
-              Your Plan: {data?.activeSubscription.plan.product}
-              <br />
-              <Link to='/memberships/change-plan'>Change...</Link>
-            </div>
-            <div>Amount: ${data?.activeSubscription.plan.amount}</div>
-            <p>
-              Renews: <Moment format='MMM D, YYYY'>{data?.activeSubscription.currentPeriodEnd}</Moment>
-            </p>
-            <div className=''></div>
-          </Card.Body>
-        </Card>
-      </Col>
-
-      <Col className='mb-3' md={12} xl={6}>
-        <Card>
-          <Card.Body>
-            <h4>Payment Methods</h4>
-
-            <CreditCard>
-              <div className='d-flex align-items-center'>
-                <img
-                  className='me-3'
-                  src='https://brand.mastercard.com/content/dam/mccom/brandcenter/thumbnails/mastercard_circles_92px_2x.png'
-                  alt='Credit Card'
-                />
-                {/* Image tag that has the VISA logo or Mastercard or w/e */}
-                <div>
-                  <span>•••• •••• •••• 4242</span>
-                  <small>4/27</small>
-                </div>
-
-                <Button className='ms-3' variant='link'>
-                  <FontAwesomeIcon size='1x' icon='ellipsis-vertical' />
-                </Button>
+    <Container>
+      <Row>
+        <Col className='mb-3' md={12} xl={6}>
+          <Card>
+            <Card.Body>
+              <div className='mb-3 d-flex align-items-center'>
+                <h4 className='flex-fill m-0'>Current Plan</h4>
+                <a className='text-danger' href='#'>
+                  Cancel
+                </a>
               </div>
-            </CreditCard>
-          </Card.Body>
-        </Card>
-      </Col>
+              <div className='mb-2'>
+                Your Plan: {data?.activeSubscription.plan.product}
+                <br />
+                <Link to='/memberships/change-plan'>Change...</Link>
+              </div>
+              <div>Amount: ${data?.activeSubscription.plan.amount}</div>
+              <p>
+                Renews: <Moment format='MMM D, YYYY'>{data?.activeSubscription.currentPeriodEnd}</Moment>
+              </p>
+              <div className=''></div>
+            </Card.Body>
+          </Card>
+        </Col>
 
-      <Col className='mt-3'>
-        <h4>Billing History</h4>
+        <Col className='mb-3' md={12} xl={6}>
+          <Card>
+            <Card.Body>
+              <h4>Payment Methods</h4>
 
-        <table className='table'>
-          <tbody>
-            {data?.billingHistory.map(row => (
-              <tr>
-                <td>
-                  <Moment format='MMM D, YYYY'>{row.date}</Moment>
-                </td>
-                <td>{row.description}</td>
-                <td>{row.amount}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </Col>
-    </Row>
+              <CreditCard>
+                <div className='d-flex align-items-center'>
+                  <img
+                    className='me-3'
+                    src='https://brand.mastercard.com/content/dam/mccom/brandcenter/thumbnails/mastercard_circles_92px_2x.png'
+                    alt='Credit Card'
+                  />
+                  {/* Image tag that has the VISA logo or Mastercard or w/e */}
+                  <div>
+                    <span>•••• •••• •••• 4242</span>
+                    <small>4/27</small>
+                  </div>
+
+                  <Button className='ms-3' variant='link'>
+                    <FontAwesomeIcon size='1x' icon='ellipsis-vertical' />
+                  </Button>
+                </div>
+              </CreditCard>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col className='mt-3'>
+          <h4>Billing History</h4>
+
+          <table className='table'>
+            <tbody>
+              {data?.billingHistory.map(row => (
+                <tr>
+                  <td>
+                    <Moment format='MMM D, YYYY'>{row.date}</Moment>
+                  </td>
+                  <td>{row.description}</td>
+                  <td>{row.amount}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </Col>
+      </Row>
+    </Container>
   );
 };
