5ff96596f5bdf22f36afb24ea287799f581ab6b8
Justin Schiff <AmbientRevolution@gmail.com>
2022-10-18 17:08:29 -0700

    Handle 413 response from nginx.

--- src/features/user-profile/hooks/useUpdateProfilePicture.ts
+++ src/features/user-profile/hooks/useUpdateProfilePicture.ts
@@ -6,6 +6,7 @@ import * as notificationService from 'common/services/notification';
 import { useUpdateProfilePictureMutation, UpdateProfilePictureRequest } from 'common/api/userApi';
 import * as authLocalStorage from '../../auth/authLocalStorage';
 import { isObject, isStringArray, isKeyOfObject } from 'common/error/utilities';
+import { StatusCodes } from 'http-status-codes';
 
 export const useUpdateProfilePicture = () => {
   const dispatch = useAppDispatch();
@@ -25,7 +26,9 @@ export const useUpdateProfilePicture = () => {
         }
       } catch (error) {
         if (isFetchBaseQueryError(error)) {
-          if (error.data && isObject(error.data) && isKeyOfObject('file', error.data)) {
+          if (error.status === StatusCodes.REQUEST_TOO_LONG) {
+            notificationService.showErrorMessage('File is too large, please upload a smaller file.');
+          } else if (error.data && isObject(error.data) && isKeyOfObject('file', error.data)) {
             const fileErrorMessages = error.data.file;
             if (isStringArray(fileErrorMessages)) {
               notificationService.showErrorMessage(fileErrorMessages.join(' '));
