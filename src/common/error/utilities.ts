faa3728959e89c1aef6c9629f5011fa132cf3d0c
Justin Schiff <jschiff@justins-mbp.attlocal.net>
2023-02-08 10:17:20 -0800

    Fix intermittent build issue with isKeyOfObject

--- src/common/error/utilities.ts
+++ src/common/error/utilities.ts
@@ -1,6 +1,6 @@
 import { FieldValues, UseFormSetError } from 'react-hook-form';
 
-export function isKeyOfObject<T>(key: string | number | symbol, obj: T): key is keyof T {
+export function isKeyOfObject<T extends object>(key: string | number | symbol, obj: T): key is keyof T {
   return key in obj;
 }
 
