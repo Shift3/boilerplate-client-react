078758006e2aa6f9817a21a2997e9cadb337085e
Justin Schiff <AmbientRevolution@gmail.com>
2023-02-21 14:10:37 -0600

    Remove cross-env and dotenv as vite has `loadEnv` built-in.

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,31 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    nodePolyfills({
-      protocolImports: true,
-    }),
-  ],
-  server: {
-    port: process.env.PORT || 4200,
-  },
-  define: {
-    'process.env': {},
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [esbuildCommonjs(['react-moment', 'lodash'])],
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      nodePolyfills({
+        protocolImports: true,
+      }),
+    ],
+    server: {
+      port: process.env.PORT || 4200,
     },
-  },
+    define: {
+      'process.env': env,
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        plugins: [esbuildCommonjs(['react-moment', 'lodash'])],
+      },
+    },
+  };
 });
