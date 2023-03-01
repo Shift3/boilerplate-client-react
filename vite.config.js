b509e297459ba121cbf0c7c833d67ad782610ae1
Justin Schiff <AmbientRevolution@gmail.com>
2023-03-01 15:59:47 -0600

    Ensure vite shows localhost instead of 127.0.0.1 as the url when starting.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import dns from 'dns';
+
+dns.setDefaultResultOrder('verbatim');
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
