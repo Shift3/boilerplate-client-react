22930a7b2be5fac9275067e3fe1577a0981a3419
Joshwin Greene <joshwin.greene@gmail.com>
2023-03-07 11:17:07 -0800

    Resolved a merge conflict.
    Rebase. This commit includes the following commits:
    
    feat(Routes): Started to migrate the routes to their declarative counterparts
    
    feat(various): Migrated more routes
    
    feat(AppRoutes): Finished migrating the rest of the routes and removed
    the commented out code
    
    fix(AppRoutes): Fixed the paths for the CreateUserView and the
    UpdateUserView
    
    refactor(various): Moved the Route definitions in NotificationListView
    into AppRoutes
    
    refactor(various): Removed the old route files and references; Renamed
    AppRoutes to Routes
    
    fix(Routes): Fixed an issue that occurred when the user logged out

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,17 @@
-import { Layout } from 'common/components/Layout';
-import { NotFoundView } from 'common/components/NotFoundView';
-import { Role } from 'common/models';
 import { BannerContentWrapper } from 'common/styles/utilities';
 import { environment } from 'environment';
-import { FarmRoutes } from 'features/farm-dashboard';
-import { AuthRoutes, RequireAuth } from 'features/auth';
 import { AppErrorBoundary } from 'features/error-boundary/components/AppErrorBoundary';
 import { NetworkDetector } from 'features/network-detector/components/NetworkDetector';
 import { NotificationsProvider } from 'features/notifications/context';
-import { NotificationRoutes } from 'features/notifications/Routes';
-import { UserRoutes } from 'features/user-dashboard';
-import { HomeRoutes } from 'features/user-dashboard/HomeRoutes';
-import { UserProfilePage } from 'features/user-profile/pages/UserProfilePage';
 import { createContext, FC, useMemo, useState } from 'react';
 import { ModalProvider } from 'react-modal-hook';
-import { Route, Routes } from 'react-router-dom';
 import { Slide, toast, ToastContainer } from 'react-toastify';
 import { TransitionGroup } from 'react-transition-group';
 import { ThemeProvider } from 'styled-components';
 import dark from 'themes/dark';
 import light from 'themes/light';
 import { GlobalStyle } from '../GlobalStyle';
+import { Routes } from './Routes';
 
 export const ThemeContext = createContext({
   theme: 'light',
@@ -70,53 +61,7 @@ export const App: FC = () => {
                 />
 
                 <BannerContentWrapper bannerShowing={environment.environment === 'staging'}>
-                  <Routes>
-                    <Route path='/auth/*' element={<AuthRoutes />} />
-                    <Route
-                      path='/user/profile/:id'
-                      element={
-                        <RequireAuth>
-                          <Layout>
-                            <UserProfilePage />
-                          </Layout>
-                        </RequireAuth>
-                      }
-                    />
-                    <Route
-                      path='/farms/*'
-                      element={
-                        <RequireAuth>
-                          <FarmRoutes />
-                        </RequireAuth>
-                      }
-                    />
-                    <Route
-                      path='/users/*'
-                      element={
-                        <RequireAuth allowedRoles={[Role.ADMIN]}>
-                          <UserRoutes />
-                        </RequireAuth>
-                      }
-                    />
-                    <Route
-                      path='/notifications/*'
-                      element={
-                        <RequireAuth>
-                          <NotificationRoutes />
-                        </RequireAuth>
-                      }
-                    />
-
-                    <Route
-                      path='/'
-                      element={
-                        <RequireAuth allowedRoles={[Role.ADMIN, Role.EDITOR, Role.USER]}>
-                          <HomeRoutes />
-                        </RequireAuth>
-                      }
-                    />
-                    <Route path='*' element={<NotFoundView />} />
-                  </Routes>
+                  <Routes />
                 </BannerContentWrapper>
               </NotificationsProvider>
             </ModalProvider>
