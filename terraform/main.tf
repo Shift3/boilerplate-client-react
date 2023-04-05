1191f800253c75f96d2c6ea1d630342c2b23a653
alexrdrgz <alejandro.xsc@gmail.com>
2023-04-05 15:49:27 -0700

    feat(.gitigon): removed tf lock file, updated some documnetaion

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,11 +5,11 @@ provider "aws" {
 
 terraform {
   backend "s3" {}
-  
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.33"
+      version = "~> 4.0"
     }
   }
 }
