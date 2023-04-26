596d951307f422c7b4292fe5f5127f1ac3e5b925
Corey Shuman <ctshuman89@gmail.com>
2023-04-26 13:30:16 -0700

    brought in service worker code from create react app pwa template

--- src/index.tsx
+++ src/index.tsx
@@ -43,6 +43,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.min.css';
 import { App } from './app/App';
 import i18n from './i18n/config';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 // Font Awesome recommends importing icons via a “library” in the initializing module of the app
@@ -107,4 +108,12 @@ root.render(
   </StrictMode>,
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register(null);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
